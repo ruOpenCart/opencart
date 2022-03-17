268beb8928ab84ceb0f2613f852e4a21ff096b66
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-17 18:38:31 +0300

    New translations footer.php (Russian)

--- upload/install/language/ru-ru/common/footer.php
+++ upload/install/language/ru-ru/common/footer.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_project']       = 'Project Homepage';
-$_['text_documentation'] = 'Documentation';
-$_['text_support']       = 'Support Forums';
-$_['text_footer']        = '<a href="https://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_project']       = 'Домашняя страница проекта';
+$_['text_documentation'] = 'Документация</a> | <a href="https://docs.opencart.name/" target="_blank"><b>Перевод документации</b>';
+$_['text_support']       = 'Форум техподдержки</a> | <a href="https://forum.opencart.name" target="_blank"><b>Русский Форум</b>';
+$_['text_footer']        = '<a href="https://www.opencart.com">OpenCart</a> &copy; 2009-' . date('Y') . ' Все права защищены.';
