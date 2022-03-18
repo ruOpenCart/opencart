47ed5a43851f588549bab7df5f9db699a2c790ab
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:34:19 +0300

    New translations cart.php (Russian)

--- upload/catalog/language/ru-ru/api/sale/cart.php
+++ upload/catalog/language/ru-ru/api/sale/cart.php
@@ -2,20 +2,20 @@
 // Text
 $_['text_success']               = 'Success: You have modified your shopping cart!';
 $_['text_subscription']          = 'Subscription';
-$_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
-$_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
-$_['text_subscription_cancel']   = '%s every %d %s(s) until canceled';
+$_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
+$_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
+$_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
 $_['text_day']                   = 'day';
 $_['text_week']                  = 'week';
 $_['text_semi_month']            = 'half-month';
 $_['text_month']                 = 'month';
 $_['text_year']                  = 'year';
-$_['text_for']                   = '%s Gift certificate for %s';
+$_['text_for']                   = '%s подарочный сертификат для %s';
 
 // Error
 $_['error_stock']                = 'Products marked with *** are not available in the desired quantity or not in stock!';
 $_['error_minimum']              = 'Minimum order amount for %s is %s!';
 $_['error_store']                = 'Product can not be bought from the store you have chosen!';
-$_['error_required']             = '%s required!';
+$_['error_required']             = '%s обязательно к заполнению!';
 $_['error_product']              = 'Warning: Product could not be found!';
 $_['error_subscription']         = 'Please select a subscription plan!';
