f5568ff083a2638bb4833b414a781b5f29638251
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:36:25 +0300

    New translations customer_order.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/customer_order.php
+++ upload/extension/opencart/admin/language/ru-ru/report/customer_order.php
@@ -13,18 +13,18 @@ $_['text_all_status']       = 'All Statuses';
 $_['column_customer']       = 'Customer Name';
 $_['column_email']          = 'E-Mail';
 $_['column_customer_group'] = 'Customer Group';
-$_['column_status']         = 'Status';
+$_['column_status']         = 'Статус';
 $_['column_orders']         = 'No. Orders';
 $_['column_products']       = 'No. Products';
 $_['column_total']          = 'Total';
-$_['column_action']         = 'Action';
+$_['column_action']         = 'Действие';
 
 // Entry
 $_['entry_date_start']      = 'Date Start';
 $_['entry_date_end']        = 'Date End';
 $_['entry_customer']        = 'Customer';
 $_['entry_order_status']    = 'Order Status';
-$_['entry_status']          = 'Status';
+$_['entry_status']          = 'Статус';
 $_['entry_sort_order']      = 'Sort Order';
 
 // Error
