24cc2518e007ae62b514cf4f51e2b12c6ddca25f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:33:53 +0300

    New translations return_status.php (Russian)

--- upload/admin/language/ru-ru/localisation/return_status.php
+++ upload/admin/language/ru-ru/localisation/return_status.php
@@ -10,7 +10,7 @@ $_['text_edit']        = 'Edit Return Status';
 
 // Column
 $_['column_name']      = 'Return Status Name';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Действие';
 
 // Entry
 $_['entry_name']       = 'Return Status Name';
