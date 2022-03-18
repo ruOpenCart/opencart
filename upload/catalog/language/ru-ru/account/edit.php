cf79f4bcd3d525316010a0d86f209c0be634004c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:34:45 +0300

    New translations edit.php (Russian)

--- upload/catalog/language/ru-ru/account/edit.php
+++ upload/catalog/language/ru-ru/account/edit.php
@@ -21,5 +21,5 @@ $_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
 $_['error_email']        = 'E-Mail Address does not appear to be valid!';
 $_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
-$_['error_custom_field'] = '%s required!';
-$_['error_regex']        = '%s not a valid input!';
+$_['error_custom_field'] = '%s обязательно к заполнению!';
+$_['error_regex']        = '%s не является допустимым вводом!';
