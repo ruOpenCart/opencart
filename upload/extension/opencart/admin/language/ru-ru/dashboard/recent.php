87244fad1d2abef7a380e42a275f6f6b036f0ca8
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:35:40 +0300

    New translations recent.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/dashboard/recent.php
+++ upload/extension/opencart/admin/language/ru-ru/dashboard/recent.php
@@ -10,13 +10,13 @@ $_['text_edit']         = 'Edit Dashboard Recent Orders';
 // Column
 $_['column_order_id']   = 'Order ID';
 $_['column_customer']   = 'Customer';
-$_['column_status']     = 'Status';
+$_['column_status']     = 'Статус';
 $_['column_total']      = 'Total';
 $_['column_date_added'] = 'Date Added';
-$_['column_action']     = 'Action';
+$_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_status']      = 'Status';
+$_['entry_status']      = 'Статус';
 $_['entry_sort_order']  = 'Sort Order';
 $_['entry_width']       = 'Width';
 
