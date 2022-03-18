f97a31a77dcc1545232764942d77bb3ef1db9f57
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:33:10 +0300

    New translations total.php (Russian)

--- upload/admin/language/ru-ru/extension/total.php
+++ upload/admin/language/ru-ru/extension/total.php
@@ -7,9 +7,9 @@ $_['text_success']      = 'Success: You have modified totals!';
 
 // Column
 $_['column_name']       = 'Order Totals';
-$_['column_status']     = 'Status';
+$_['column_status']     = 'Статус';
 $_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_action']     = 'Действие';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify totals!';
