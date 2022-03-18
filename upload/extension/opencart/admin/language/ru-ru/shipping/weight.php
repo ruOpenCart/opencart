f27fe745ffeb0bab717d8c3200553338b11286b1
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:35:49 +0300

    New translations weight.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/shipping/weight.php
+++ upload/extension/opencart/admin/language/ru-ru/shipping/weight.php
@@ -11,7 +11,7 @@ $_['text_edit']        = 'Edit Weight Based Shipping';
 $_['entry_rate']       = 'Rates';
 $_['entry_tax_class']  = 'Tax Class';
 $_['entry_geo_zone']   = 'Geo Zone';
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Статус';
 $_['entry_sort_order'] = 'Sort Order';
 
 // Help
