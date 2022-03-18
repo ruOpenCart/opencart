ec7ed5e314e0b07d906dd5c84551872141db42be
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:34:09 +0300

    New translations gdpr_export.php (Russian)

--- upload/admin/language/ru-ru/mail/gdpr_export.php
+++ upload/admin/language/ru-ru/mail/gdpr_export.php
@@ -1,21 +1,21 @@
 <?php
 // Text
-$_['text_subject']    = '%s - GDPR export request completed!';
+$_['text_subject']    = '%s - запрос на экспорт GDPR выполнен!';
 $_['text_request']    = 'Export Personal Data';
 $_['text_hello']      = 'Hello <strong>%s</strong>,';
 $_['text_user']       = 'User';
 $_['text_gdpr']       = 'Your GDPR data request has now been complete. Below you will find your GDPR data.';
 $_['text_account']    = 'Account';
 $_['text_customer']   = 'Personal Information';
-$_['text_address']    = 'Address';
+$_['text_address']    = 'Адрес';
 $_['text_addresses']  = 'Addresses';
 $_['text_name']       = 'Customer Name';
 $_['text_recipient']  = 'Recipient';
 $_['text_email']      = 'E-Mail';
 $_['text_telephone']  = 'Telephone';
 $_['text_company']    = 'Company';
-$_['text_address_1']  = 'Address 1';
-$_['text_address_2']  = 'Address 2';
+$_['text_address_1']  = 'Адрес 1';
+$_['text_address_2']  = 'Адрес 2';
 $_['text_postcode']   = 'Post Code';
 $_['text_city']       = 'City';
 $_['text_country']    = 'Country';
