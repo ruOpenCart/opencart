d0d049d5a1a88cfa2760f825e26df1b32a5a1b72
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:33:50 +0300

    New translations order_status.php (Russian)

--- upload/admin/language/ru-ru/localisation/order_status.php
+++ upload/admin/language/ru-ru/localisation/order_status.php
@@ -10,7 +10,7 @@ $_['text_edit']        = 'Edit Order Status';
 
 // Column
 $_['column_name']      = 'Order Status Name';
-$_['column_action']    = 'Action';
+$_['column_action']    = 'Действие';
 
 // Entry
 $_['entry_name']       = 'Order Status Name';
