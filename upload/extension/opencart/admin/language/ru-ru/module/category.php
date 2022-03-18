8ca987454ac1bd5dc060868a7171eba830ce89ef
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:36:10 +0300

    New translations category.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/category.php
+++ upload/extension/opencart/admin/language/ru-ru/module/category.php
@@ -8,7 +8,7 @@ $_['text_success']     = 'Success: You have modified category module!';
 $_['text_edit']        = 'Edit Category Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Статус';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify category module!';
