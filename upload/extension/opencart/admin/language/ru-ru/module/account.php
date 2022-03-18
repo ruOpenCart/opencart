bc0252aabb64e12d4fa25dd452f79b5d3e6398ad
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:36:07 +0300

    New translations account.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/account.php
+++ upload/extension/opencart/admin/language/ru-ru/module/account.php
@@ -8,7 +8,7 @@ $_['text_success']     = 'Success: You have modified account module!';
 $_['text_edit']        = 'Edit Account Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Статус';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify account module!';
