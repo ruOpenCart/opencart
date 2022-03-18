6c815221b61c9cc51c7e06d2f86c89fc259a5a40
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:34:36 +0300

    New translations api.php (Russian)

--- upload/admin/language/ru-ru/user/api.php
+++ upload/admin/language/ru-ru/user/api.php
@@ -11,17 +11,17 @@ $_['text_ip']              = 'Below you can create a list of IP\'s allowed to ac
 
 // Column
 $_['column_username']      = 'API Username';
-$_['column_status']        = 'Status';
+$_['column_status']        = 'Статус';
 $_['column_token']         = 'Token';
 $_['column_ip']            = 'IP';
 $_['column_date_added']    = 'Date Added';
 $_['column_date_modified'] = 'Date Modified';
-$_['column_action']        = 'Action';
+$_['column_action']        = 'Действие';
 
 // Entry
 $_['entry_username']       = 'API Username';
 $_['entry_key']            = 'API Key';
-$_['entry_status']         = 'Status';
+$_['entry_status']         = 'Статус';
 $_['entry_ip']             = 'IP';
 
 // Error
