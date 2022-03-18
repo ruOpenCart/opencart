06b846bd3be01a79696a71eabc09ef4bb834e4a4
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:33:12 +0300

    New translations report.php (Russian)

--- upload/admin/language/ru-ru/extension/report.php
+++ upload/admin/language/ru-ru/extension/report.php
@@ -8,9 +8,9 @@ $_['text_list']         = 'Reports List';
 
 // Column
 $_['column_name']       = 'Report Name';
-$_['column_status']     = 'Status';
+$_['column_status']     = 'Статус';
 $_['column_sort_order'] = 'Sort Order';
-$_['column_action']     = 'Action';
+$_['column_action']     = 'Действие';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify reports!';
