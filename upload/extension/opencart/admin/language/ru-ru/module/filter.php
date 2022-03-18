2cd01915eccd389d982bca94716f38c144eb363f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-18 22:36:12 +0300

    New translations filter.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/filter.php
+++ upload/extension/opencart/admin/language/ru-ru/module/filter.php
@@ -8,7 +8,7 @@ $_['text_success']     = 'Success: You have modified filter module!';
 $_['text_edit']        = 'Edit Filter Module';
 
 // Entry
-$_['entry_status']     = 'Status';
+$_['entry_status']     = 'Статус';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify filter module!';
