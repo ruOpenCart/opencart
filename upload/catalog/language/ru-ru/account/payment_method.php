8d714562ccf66848af0bfe023c447d88b9ecff0b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-19 23:29:19 +0300

    New translations payment_method.php (Russian)

--- upload/catalog/language/ru-ru/account/payment_method.php
+++ upload/catalog/language/ru-ru/account/payment_method.php
@@ -3,7 +3,7 @@
 $_['heading_title']         = 'Payment Methods';
 
 // Text
-$_['text_account']          = 'Account';
+$_['text_account']          = 'Аккаунт';
 $_['text_payment_method']   = 'Payment Method Entries';
 $_['text_success']          = 'Your payment method has been successfully deleted';
 $_['text_no_results']       = 'You have no payment methods in your account.';
