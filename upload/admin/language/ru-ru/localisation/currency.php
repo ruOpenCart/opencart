f98901db0cdf8301a72a746d38bfc56fc880e43c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-19 23:27:39 +0300

    New translations currency.php (Russian)

--- upload/admin/language/ru-ru/localisation/currency.php
+++ upload/admin/language/ru-ru/localisation/currency.php
@@ -5,7 +5,7 @@ $_['heading_title']        = 'Currencies';
 // Text
 $_['text_success']         = 'Success: You have modified currencies!';
 $_['text_list']            = 'Currency List';
-$_['text_add']             = 'Add Currency';
+$_['text_add']             = 'Добавить валюту';
 $_['text_edit']            = 'Edit Currency';
 $_['text_iso']             = 'You can find a full list of ISO currency codes and settings <a href="http://www.xe.com/iso4217.php" target="_blank" class="alert-link">here</a>.';
 
