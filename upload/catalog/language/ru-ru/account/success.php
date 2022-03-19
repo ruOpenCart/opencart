5bc2a0d097600992e84b80afcfa414dac870ad15
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-19 23:29:00 +0300

    New translations success.php (Russian)

--- upload/catalog/language/ru-ru/account/success.php
+++ upload/catalog/language/ru-ru/account/success.php
@@ -3,6 +3,6 @@
 $_['heading_title'] = 'Your Account Has Been Created!';
 
 // Text
-$_['text_success']  = '<p>Congratulations! Your new account has been successfully created!</p> <p>You can now take advantage of member privileges to enhance your online shopping experience with us.</p> <p>If you have ANY questions about the operation of this online shop, please e-mail the store owner.</p> <p>A confirmation has been sent to the provided e-mail address. If you have not received it within the hour, please <a href="%s">contact us</a>.</p>';
-$_['text_approval'] = '<p>Thank you for registering with %s!</p><p>You will be notified by e-mail once your account has been activated by the store owner.</p><p>If you have ANY questions about the operation of this online shop, please <a href="%s">contact the store owner</a>.</p>';
-$_['text_account']  = 'Account';
+$_['text_success']  = '<p>Поздравляем! Ваша учётная запись была успешно создана.</p><p>теперь вы можете воспользоваться дополнительными возможностями: просмотр истории заказов, печать счета, изменение своей контактной информации и адресов доставки, следить за накопленными баллами и многое другое...</p><p>Если у вас есть какие-либо вопросы, напишите нам.</p><p>Письмо с данными о регистрации было отправлено на указанный E-mail. Если вы не получили письмо, пожалуйста <a href="%s">свяжитесь с нами</a>.</p>';
+$_['text_approval'] = '<p>Спасибо за регистрацию в %s!</p><p>Вы будете уведомлены по электронной почте, как только Ваш Личный Кабинет будет активирован администрацией магазина.</p><p>Если у Вас есть какие-то вопросы, пожалуйста <a href="%s">напишите нам</a>.</p>';
+$_['text_account']  = 'Аккаунт';
