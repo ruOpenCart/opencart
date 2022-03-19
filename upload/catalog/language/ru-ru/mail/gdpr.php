c621a9c51739467dd4825de5263b85c9b0d50b92
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-19 23:29:58 +0300

    New translations gdpr.php (Russian)

--- upload/catalog/language/ru-ru/mail/gdpr.php
+++ upload/catalog/language/ru-ru/mail/gdpr.php
@@ -2,8 +2,8 @@
 // Text
 $_['text_subject']  = '%s - GDPR запрос экспорта/удаления!';
 $_['text_export']   = 'Export Data Request';
-$_['text_remove']   = 'Account Deletion Request';
-$_['text_gdpr']     = 'A GDPR request from this email address, To confirm this action please click on the link below:';
+$_['text_remove']   = 'Запрос на удаление учетной записи';
+$_['text_gdpr']     = 'Запрос GDPR с этого адреса электронной почты, чтобы подтвердить это действие, пожалуйста, нажмите на ссылку ниже:';
 $_['text_ip']       = 'The IP used to make this request was:';
 $_['text_contact']  = 'If you did not make this request please contact the store owner here:';
 $_['text_thanks']   = 'Thanks,';
