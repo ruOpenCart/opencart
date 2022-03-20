f8aee87ebba3785763edb5e3146fc4d8c595a4d0
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:28:49 +0300

    New translations fraud.php (Russian)

--- upload/admin/language/ru-ru/extension/fraud.php
+++ upload/admin/language/ru-ru/extension/fraud.php
@@ -1,13 +1,13 @@
 <?php
 // Heading
-$_['heading_title']    = 'Anti-Fraud';
+$_['heading_title']    = 'Защита от мошенничества';
 
 // Text
 $_['text_success']     = 'Success: You have modified anti-fraud!';
-$_['text_list']        = 'Anti-Fraud List';
+$_['text_list']        = 'Список защит от мошенничества';
 
 // Column
-$_['column_name']      = 'Anti-Fraud Name';
+$_['column_name']      = 'Наименование защиты от мошенничества';
 $_['column_status']    = 'Статус';
 $_['column_action']    = 'Действие';
 
