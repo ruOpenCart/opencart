58fc041a332fd2c2855896dcdbdc17e6f2fb5537
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:38 +0300

    New translations order.php (Russian)

--- upload/catalog/language/ru-ru/account/order.php
+++ upload/catalog/language/ru-ru/account/order.php
@@ -3,12 +3,12 @@
 $_['heading_title']         = 'Order History';
 
 // Text
-$_['text_account']          = 'Account';
+$_['text_account']          = 'Аккаунт';
 $_['text_order']            = 'Order Information';
 $_['text_order_detail']     = 'Order Details';
 $_['text_invoice_no']       = 'Invoice No.:';
 $_['text_order_id']         = 'Order ID:';
-$_['text_date_added']       = 'Date Added:';
+$_['text_date_added']       = 'Дата добавления:';
 $_['text_shipping_address'] = 'Shipping Address';
 $_['text_shipping_method']  = 'Shipping Method:';
 $_['text_payment_address']  = 'Payment Address';
@@ -21,7 +21,7 @@ $_['text_error']            = 'The order you requested could not be found!';
 
 // Column
 $_['column_order_id']       = 'Order ID';
-$_['column_customer']       = 'Customer';
+$_['column_customer']       = 'Покупатель';
 $_['column_product']        = 'No. of Products';
 $_['column_name']           = 'Product Name';
 $_['column_model']          = 'Model';
@@ -29,9 +29,9 @@ $_['column_quantity']       = 'Quantity';
 $_['column_price']          = 'Price';
 $_['column_total']          = 'Total';
 $_['column_action']         = 'Действие';
-$_['column_date_added']     = 'Date Added';
+$_['column_date_added']     = 'Дата добавления';
 $_['column_status']         = 'Статус';
-$_['column_comment']        = 'Comment';
+$_['column_comment']        = 'Комментарий';
 
 // Error
 $_['error_reorder']         = '%s в настоящее время недоступен для повторного заказа.';
