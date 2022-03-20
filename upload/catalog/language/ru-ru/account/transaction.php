241548d2b32715790188cd261b99e668c57003a8
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:44 +0300

    New translations transaction.php (Russian)

--- upload/catalog/language/ru-ru/account/transaction.php
+++ upload/catalog/language/ru-ru/account/transaction.php
@@ -3,12 +3,12 @@
 $_['heading_title']      = 'Your Transactions';
 
 // Column
-$_['column_date_added']  = 'Date Added';
+$_['column_date_added']  = 'Дата добавления';
 $_['column_description'] = 'Description';
-$_['column_amount']      = 'Amount (%s)';
+$_['column_amount']      = 'Сумма (%s)';
 
 // Text
-$_['text_account']       = 'Account';
+$_['text_account']       = 'Аккаунт';
 $_['text_transaction']   = 'Your Transactions';
 $_['text_total']         = 'Your current balance is:';
 $_['text_no_results']    = 'You do not have any transactions!';
