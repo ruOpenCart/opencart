5e5ba15ee5eb0cedcbbfedb66abcf73efb4d484e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:28 +0300

    New translations gdpr.php (Russian)

--- upload/admin/language/ru-ru/customer/gdpr.php
+++ upload/admin/language/ru-ru/customer/gdpr.php
@@ -5,31 +5,31 @@ $_['heading_title']     = 'GDPR Approvals';
 // Text
 $_['text_success']      = 'Success: You have modified GDPR approvals!';
 $_['text_list']         = 'GDPR Approval List';
-$_['text_info']         = '<strong>GDPR</strong> account deletion requests will process after <strong>%s days</strong> so any fraud detection, chargebacks or refunds can be processed.';
-$_['text_approve']      = 'Approve';
-$_['text_deny']         = 'Deny';
-$_['text_delete']       = 'Delete';
+$_['text_info']         = '<strong>GDPR</strong> запросы на удаление аккаунта будут обрабатываться через <strong>%s дней</strong>, так что любые обнаружения мошенничества, выплаты или возвраты средств могут быть обработаны.';
+$_['text_approve']      = 'Одобрить';
+$_['text_deny']         = 'Отклонить';
+$_['text_delete']       = 'Удалить';
 $_['text_unverified']   = 'Unverified';
 $_['text_pending']      = 'Pending';
 $_['text_processing']   = 'Processing';
-$_['text_complete']     = 'Complete';
-$_['text_denied']       = 'Denied';
+$_['text_complete']     = 'Завершено';
+$_['text_denied']       = 'Отказано';
 $_['text_export']       = 'Export';
 $_['text_remove']       = 'Remove';
 $_['text_filter']       = 'Filter';
 
 // Column
-$_['column_email']      = 'E-Mail';
+$_['column_email']      = 'Эл. почта';
 $_['column_request']    = 'Request';
 $_['column_status']     = 'Статус';
-$_['column_date_added'] = 'Date Added';
+$_['column_date_added'] = 'Дата добавления';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_email']       = 'E-Mail';
+$_['entry_email']       = 'Эл. почта';
 $_['entry_action']      = 'Действие';
 $_['entry_status']      = 'Статус';
-$_['entry_date_added']  = 'Date Added';
+$_['entry_date_added']  = 'Дата добавления';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify GDPR approvals!';
