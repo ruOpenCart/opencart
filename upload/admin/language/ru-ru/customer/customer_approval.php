1512e3e359907f6ed0901ae5f4e52485c15f0845
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:26 +0300

    New translations customer_approval.php (Russian)

--- upload/admin/language/ru-ru/customer/customer_approval.php
+++ upload/admin/language/ru-ru/customer/customer_approval.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Approvals';
+$_['heading_title']         = 'Одобрение клиентов';
 
 // Text
 $_['text_success']          = 'Success: You have modified customer approvals!';
-$_['text_list']             = 'Customer Approval List';
-$_['text_default']          = 'Default';
-$_['text_customer']         = 'Customer';
-$_['text_affiliate']        = 'Affiliate';
+$_['text_list']             = 'Список одобрения покупателей';
+$_['text_default']          = 'По умолчанию';
+$_['text_customer']         = 'Покупатель';
+$_['text_affiliate']        = 'Партнёр';
 $_['text_filter']           = 'Filter';
 
 // Column
-$_['column_customer']       = 'Customer';
-$_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
+$_['column_customer']       = 'Покупатель';
+$_['column_email']          = 'Эл. почта';
+$_['column_customer_group'] = 'Группа покупателя';
 $_['column_type']           = 'Type';
-$_['column_date_added']     = 'Date Added';
+$_['column_date_added']     = 'Дата добавления';
 $_['column_action']         = 'Действие';
 
 // Entry
-$_['entry_customer']        = 'Customer';
-$_['entry_email']           = 'E-Mail';
-$_['entry_customer_group']  = 'Customer Group';
+$_['entry_customer']        = 'Покупатель';
+$_['entry_email']           = 'Эл. почта';
+$_['entry_customer_group']  = 'Группа покупателя';
 $_['entry_type']            = 'Type';
-$_['entry_date_added']      = 'Date Added';
+$_['entry_date_added']      = 'Дата добавления';
 
 // Error
 $_['error_permission']      = 'Warning: You do not have permission to modify customer approvals!';
