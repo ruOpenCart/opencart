726db5cb48b27fd7b6071c009b667449a52ccbe4
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:30:15 +0300

    New translations upgrade.php (Russian)

--- upload/admin/language/ru-ru/tool/upgrade.php
+++ upload/admin/language/ru-ru/tool/upgrade.php
@@ -4,27 +4,27 @@ $_['heading_title']        = 'Upgrade';
 
 // Text
 $_['text_success']         = 'Success: You are using the latest version %s!';
-$_['text_upgrade']         = 'Check Latest Version';
+$_['text_upgrade']         = 'Проверить последнюю версию';
 $_['text_information']     = 'Version Information';
-$_['text_current_version'] = 'Current Version';
+$_['text_current_version'] = 'Текущая версия';
 $_['text_latest_version']  = 'Latest Version';
 $_['text_date_added']      = 'Release Date';
-$_['text_change']          = 'Change Log';
+$_['text_change']          = 'История изменений';
 $_['text_status']          = 'Upgrade Status';
-$_['text_ready']           = 'Click the upgrade button to upgrade to the latest version...';
-$_['text_download']        = 'Downloading latest...';
-$_['text_install']         = 'Copying files...';
-$_['text_patch']           = 'Applying patches...';
+$_['text_ready']           = 'Нажмите кнопку обновить для обновления до последней версии...';
+$_['text_download']        = 'Загружается последняя...';
+$_['text_install']         = 'Копирование файлов...';
+$_['text_patch']           = 'Применение исправлений...';
 
 // Button
 $_['button_upgrade']       = 'Upgrade';
 
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify upgrades!';
-$_['error_connection']     = 'Could not connect to the upgrade server!';
+$_['error_connection']     = 'Не удалось подключиться к серверу обновлений!';
 $_['error_version']        = 'Version is lower than the current version!';
 $_['error_download']       = 'Upgrade could not be downloaded!';
 $_['error_file']           = 'Upgrade file could not be found!';
-$_['error_directory']      = 'Could not create directory %s!';
-$_['error_copy']           = 'Could not copy file %s to %s!';
+$_['error_directory']      = 'Не удалось создать директорию %s!';
+$_['error_copy']           = 'Не удалось скопировать файл %s в %s!';
 $_['error_unzip']          = 'Zip file could not be opened!';
