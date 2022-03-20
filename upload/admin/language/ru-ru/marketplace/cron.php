b2e29a31ba4909f518896db41c56180dc58399d5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:57 +0300

    New translations cron.php (Russian)

--- upload/admin/language/ru-ru/marketplace/cron.php
+++ upload/admin/language/ru-ru/marketplace/cron.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']        = 'Cron Jobs';
+$_['heading_title']        = 'Задания Cron';
 
 // Text
 $_['text_success']         = 'Success: You have modified cron jobs!';
-$_['text_instruction']     = 'CRON Instructions';
-$_['text_list']            = 'Cron List';
-$_['text_cron_1']          = 'Cron Job\'s are scheduled tasks that are run periodically. To setup your servers to use cron job you can read the <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">opencart documentation</a> page.';
+$_['text_instruction']     = 'Инструкции по Крону';
+$_['text_list']            = 'Список задач';
+$_['text_cron_1']          = 'Cron Job — это запланированные задачи, которые запускаются периодически. Чтобы настроить серверы для использования задания cron, вы можете прочитать страницу <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">документации опенкарт</a>.';
 $_['text_cron_2']          = 'You need to set your Cron task to run every hour.';
-$_['text_info']            = 'CRON Information';
+$_['text_info']            = 'Информация по Крону';
 $_['text_hour']            = 'Hour';
 $_['text_day']             = 'Day';
 $_['text_month']           = 'Month';
 
 // Column
-$_['column_code']          = 'Cron Code';
-$_['column_cycle']         = 'Cycle';
-$_['column_date_added']    = 'Date Added';
+$_['column_code']          = 'Код крона';
+$_['column_cycle']         = 'Цикл';
+$_['column_date_added']    = 'Дата добавления';
 $_['column_date_modified'] = 'Date Modified';
 $_['column_action']        = 'Действие';
 
 // entry
-$_['entry_cron']           = 'Cron URL';
+$_['entry_cron']           = 'URL-адрес крона';
 $_['entry_description']    = 'Description';
 
 // Error
