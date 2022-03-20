7f2dde81c46a7b95db820f56ee54cd25e2687a2b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:49 +0300

    New translations voucher.php (Russian)

--- upload/catalog/language/ru-ru/api/sale/voucher.php
+++ upload/catalog/language/ru-ru/api/sale/voucher.php
@@ -11,4 +11,4 @@ $_['error_to_name']   = 'Recipient\'s Name must be between 1 and 64 characters!'
 $_['error_from_name'] = 'Your Name must be between 1 and 64 characters!';
 $_['error_email']     = 'E-Mail Address does not appear to be valid!';
 $_['error_theme']     = 'You must select a theme!';
-$_['error_amount']    = 'Amount must be between %s and %s!';
+$_['error_amount']    = 'Сумма должна быть больше %s и меньше %s!';
