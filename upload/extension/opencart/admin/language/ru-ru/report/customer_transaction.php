f670e8b79afe4dd32da00fac2c46835812046256
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:30:35 +0300

    New translations customer_transaction.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/customer_transaction.php
+++ upload/extension/opencart/admin/language/ru-ru/report/customer_transaction.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Transaction Report';
+$_['heading_title']         = 'Отчет по транзакциям покупателей';
 
 // Text
 $_['text_extension']        = 'Extensions';
@@ -9,17 +9,17 @@ $_['text_success']          = 'Success: You have modified customer credit report
 $_['text_filter']           = 'Filter';
 
 // Column
-$_['column_customer']       = 'Customer Name';
-$_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
+$_['column_customer']       = 'Имя покупателя';
+$_['column_email']          = 'Эл. почта';
+$_['column_customer_group'] = 'Группа покупателя';
 $_['column_status']         = 'Статус';
 $_['column_total']          = 'Total';
 $_['column_action']         = 'Действие';
 
 // Entry
 $_['entry_date_start']      = 'Date Start';
-$_['entry_date_end']        = 'Date End';
-$_['entry_customer']        = 'Customer';
+$_['entry_date_end']        = 'Дата окончания';
+$_['entry_customer']        = 'Покупатель';
 $_['entry_status']          = 'Статус';
 $_['entry_sort_order']      = 'Sort Order';
 
