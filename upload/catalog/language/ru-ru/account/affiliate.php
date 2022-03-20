05ecf93cbd85bb3bcaf987d1c361be5fd3eaa3f7
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:54 +0300

    New translations affiliate.php (Russian)

--- upload/catalog/language/ru-ru/account/affiliate.php
+++ upload/catalog/language/ru-ru/account/affiliate.php
@@ -3,35 +3,35 @@
 $_['heading_title']             = 'Your Affiliate Information';
 
 // Text
-$_['text_account']              = 'Account';
-$_['text_affiliate']            = 'Affiliate';
+$_['text_account']              = 'Аккаунт';
+$_['text_affiliate']            = 'Партнёр';
 $_['text_my_affiliate']         = 'My Affiliate Account';
 $_['text_payment']              = 'Payment Information';
-$_['text_cheque']               = 'Cheque';
+$_['text_cheque']               = 'Чек';
 $_['text_paypal']               = 'PayPal';
-$_['text_bank']                 = 'Bank Transfer';
+$_['text_bank']                 = 'Банковский перевод';
 $_['text_success']              = 'Success: Your affiliate account has been successfully updated.';
 $_['text_agree']                = 'I have read and agree to the <a href="%s" class="modal-link"><b>%s</b></a>';
 
 // Entry
-$_['entry_company']             = 'Company';
+$_['entry_company']             = 'Компания';
 $_['entry_website']             = 'Web Site';
 $_['entry_tax']                 = 'Tax ID';
 $_['entry_payment']             = 'Payment Method';
-$_['entry_cheque']              = 'Cheque Payee Name';
+$_['entry_cheque']              = 'Имя получателя чека';
 $_['entry_paypal']              = 'PayPal Email Account';
-$_['entry_bank_name']           = 'Bank Name';
-$_['entry_bank_branch_number']  = 'ABA/BSB number (Branch Number)';
+$_['entry_bank_name']           = 'Наименование банка';
+$_['entry_bank_branch_number']  = 'ABA/BSB номер (номер отделения)';
 $_['entry_bank_swift_code']     = 'SWIFT Code';
-$_['entry_bank_account_name']   = 'Account Name';
-$_['entry_bank_account_number'] = 'Account Number';
+$_['entry_bank_account_name']   = 'Название счёта';
+$_['entry_bank_account_number'] = 'Номер счета';
 
 // Error
 $_['error_token']               = 'Warning: Affiliate token invalid!';
 $_['error_agree']               = 'Warning: You must agree to the %s!';
-$_['error_cheque']              = 'Cheque Payee Name required!';
+$_['error_cheque']              = 'Необходимо указать имя получателя чека!';
 $_['error_paypal']              = 'PayPal Email Address does not appear to be valid!';
-$_['error_bank_account_name']   = 'Account Name required!';
-$_['error_bank_account_number'] = 'Account Number required!';
+$_['error_bank_account_name']   = 'Имя счета обязательно!';
+$_['error_bank_account_number'] = 'Номер счета обязателен!';
 $_['error_custom_field']        = '%s обязательно к заполнению!';
 $_['error_regex']               = '%s не является допустимым вводом!';
