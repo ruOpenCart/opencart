3d88be57804fb8e0bc8c245f64d0e952a1e0e323
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:28:50 +0300

    New translations address_format.php (Russian)

--- upload/admin/language/ru-ru/localisation/address_format.php
+++ upload/admin/language/ru-ru/localisation/address_format.php
@@ -1,27 +1,27 @@
 <?php
 // Heading
-$_['heading_title']           = 'Address Format';
+$_['heading_title']           = 'Формат адреса';
 
 // Text
 $_['text_success']            = 'Success: You have modified address formats!';
-$_['text_list']               = 'Address Format List';
-$_['text_add']                = 'Add Address Format';
+$_['text_list']               = 'Список форматов адресов';
+$_['text_add']                = 'Добавить формат адреса';
 $_['text_edit']               = 'Edit Address Format';
 
 // Column
-$_['column_name']             = 'Address Format Name';
-$_['column_address_format']   = 'Address Format';
+$_['column_name']             = 'Название формата адреса';
+$_['column_address_format']   = 'Формат адреса';
 $_['column_action']           = 'Действие';
 
 // Entry
-$_['entry_name']              = 'Address Format Name';
-$_['entry_address_format']    = 'Address Format';
+$_['entry_name']              = 'Название формата адреса';
+$_['entry_address_format']    = 'Формат адреса';
 
 // Help
 $_['help_address_format']     = 'First Name = {firstname}<br />Last Name = {lastname}<br />Company = {company}<br />Address 1 = {address_1}<br />Address 2 = {address_2}<br />City = {city}<br />Postcode = {postcode}<br />Zone = {zone}<br />Zone Code = {zone_code}<br />Country = {country}';
 
 // Error
 $_['error_permission']        = 'Warning: You do not have permission to modify address formats!';
-$_['error_name']              = 'Address Format Name must be between 1 and 128 characters!';
+$_['error_name']              = 'Название формата адреса должно быть от 1 до 128 символов!';
 $_['error_default']           = 'Warning: This address format cannot be deleted as it is currently assigned as the default address format!';
 $_['error_country']           = 'Warning: This address format cannot be deleted as it is currently assigned to %s countries!';
