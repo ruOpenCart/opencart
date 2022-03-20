04b232e13633e3bbc0fe188c41313e87684230f0
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:30:37 +0300

    New translations marketing.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/marketing.php
+++ upload/extension/opencart/admin/language/ru-ru/report/marketing.php
@@ -7,18 +7,18 @@ $_['text_extension']     = 'Extensions';
 $_['text_edit']          = 'Edit Marketing Report';
 $_['text_success']       = 'Success: You have modified marketing report!';
 $_['text_filter']        = 'Filter';
-$_['text_all_status']    = 'All Statuses';
+$_['text_all_status']    = 'Все статусы';
 
 // Column
-$_['column_campaign']    = 'Campaign Name';
-$_['column_code']        = 'Code';
-$_['column_clicks']      = 'Clicks';
+$_['column_campaign']    = 'Название кампании';
+$_['column_code']        = 'Код';
+$_['column_clicks']      = 'Клики';
 $_['column_orders']      = 'No. Orders';
 $_['column_total']       = 'Total';
 
 // Entry
 $_['entry_date_start']   = 'Date Start';
-$_['entry_date_end']     = 'Date End';
+$_['entry_date_end']     = 'Дата окончания';
 $_['entry_order_status'] = 'Order Status';
 $_['entry_status']       = 'Статус';
 $_['entry_sort_order']   = 'Sort Order';
