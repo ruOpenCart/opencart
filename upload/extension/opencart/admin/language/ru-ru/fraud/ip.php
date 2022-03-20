2c1fbc21ac061a681cbe49a93edd3b8b2f458a88
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:30:23 +0300

    New translations ip.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/fraud/ip.php
+++ upload/extension/opencart/admin/language/ru-ru/fraud/ip.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']      = 'Anti-Fraud IP';
+$_['heading_title']      = 'Защита от мошенничества по IP';
 
 // Text
 $_['text_extension']     = 'Extensions';
 $_['text_success']       = 'Success: You have modified Anti-Fraud IP!';
 $_['text_edit']          = 'Edit Anti-Fraud IP';
-$_['text_ip_add']        = 'Add IP Address';
+$_['text_ip_add']        = 'Добавить IP-адрес';
 $_['text_ip_list']       = 'Fraud IP Address List';
 
 // Column
 $_['column_ip']          = 'IP';
 $_['column_total']       = 'Total Accounts';
-$_['column_date_added']  = 'Date Added';
+$_['column_date_added']  = 'Дата добавления';
 $_['column_action']      = 'Действие';
 
 // Entry
@@ -21,7 +21,7 @@ $_['entry_status']       = 'Статус';
 $_['entry_order_status'] = 'Order Status';
 
 // Help
-$_['help_order_status']  = 'Customers that have a banned IP on their accounts will be assigned this order status and will not be allowed to reach the complete status automatically.';
+$_['help_order_status']  = 'Клиентам, у которых заблокирован IP-адрес в своих учетных записях, будет присвоен этот статус заказа, и они не смогут автоматически получить статус выполнено.';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify Anti-Fraud IP!';
