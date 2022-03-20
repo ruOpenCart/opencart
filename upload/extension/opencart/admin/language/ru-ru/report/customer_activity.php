212739edfe5e3465c62478a5493b95b25e1cdbaa
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:30:22 +0300

    New translations customer_activity.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/customer_activity.php
+++ upload/extension/opencart/admin/language/ru-ru/report/customer_activity.php
@@ -1,40 +1,40 @@
 <?php
 // Heading
-$_['heading_title']                = 'Customer Activity Report';
+$_['heading_title']                = 'Отчет об активности покупателей';
 
 // Text
 $_['text_extension']               = 'Extensions';
 $_['text_edit']                    = 'Edit Customer Activity Report';
 $_['text_success']                 = 'Success: You have modified customer activity report!';
 $_['text_filter']                  = 'Filter';
-$_['text_activity_register']       = '<a href="customer_id=%d">%s</a> registered for an account.';
-$_['text_activity_edit']           = '<a href="customer_id=%d">%s</a> updated their account details.';
-$_['text_activity_password']       = '<a href="customer_id=%d">%s</a> updated their account password.';
-$_['text_activity_reset']          = '<a href="customer_id=%d">%s</a> reset their account password.';
-$_['text_activity_login']          = '<a href="customer_id=%d">%s</a> logged in.';
-$_['text_activity_forgotten']      = '<a href="customer_id=%d">%s</a> requested a reset password.';
-$_['text_activity_address_add']    = '<a href="customer_id=%d">%s</a> added a new address.';
-$_['text_activity_address_edit']   = '<a href="customer_id=%d">%s</a> updated their address.';
-$_['text_activity_address_delete'] = '<a href="customer_id=%d">%s</a> deleted one of their addresses.';
-$_['text_activity_return_account'] = '<a href="customer_id=%d">%s</a> submitted a product return.';
+$_['text_activity_register']       = '<a href="customer_id=%d">%s</a> зарегистрировал новый аккаунт.';
+$_['text_activity_edit']           = '<a href="customer_id=%d">%s</a> обновил данные учетной записи.';
+$_['text_activity_password']       = '<a href="customer_id=%d">%s</a> обновил пароль учетной записи.';
+$_['text_activity_reset']          = '<a href="customer_id=%d">%s</a> сбросил пароль учетной записи.';
+$_['text_activity_login']          = '<a href="customer_id=%d">%s</a> авторизовался.';
+$_['text_activity_forgotten']      = '<a href="customer_id=%d">%s</a> запросил сброс пароля.';
+$_['text_activity_address_add']    = '<a href="customer_id=%d">%s</a> добавил новый адрес.';
+$_['text_activity_address_edit']   = '<a href="customer_id=%d">%s</a> обновил свой адрес.';
+$_['text_activity_address_delete'] = '<a href="customer_id=%d">%s</a> удалил один из своих адресов.';
+$_['text_activity_return_account'] = '<a href="customer_id=%d">%s</a> запросил возврата товара.';
 $_['text_activity_return_guest']   = '%s запросил возврата товара.';
-$_['text_activity_order_account']  = '<a href="customer_id=%d">%s</a> created a <a href="order_id=%d">new order</a>.';
+$_['text_activity_order_account']  = '<a href="customer_id=%d">%s</a> создал <a href="order_id=%d">новый заказ</a>.';
 $_['text_activity_order_guest']    = '%s создал(а) <a href="order_id=%d"> новый заказ</a>.';
-$_['text_activity_affiliate_add']  = '<a href="customer_id=%d">%s</a> registered for a affiliate account.';
-$_['text_activity_affiliate_edit'] = '<a href="customer_id=%d">%s</a> updated their affiliate details.';
-$_['text_activity_transaction']    = '<a href="customer_id=%d">%s</a> received commission from an new <a href="order_id=%d">order</a>.';
+$_['text_activity_affiliate_add']  = '<a href="customer_id=%d">%s</a> зарегистрирован новый партнер.';
+$_['text_activity_affiliate_edit'] = '<a href="customer_id=%d">%s</a> обновил партнерские данные.';
+$_['text_activity_transaction']    = '<a href="customer_id=%d">%s</a> получил комиссию от нового <a href="order_id=%d">заказа</a>.';
 
 // Column
-$_['column_customer']              = 'Customer';
-$_['column_comment']               = 'Comment';
+$_['column_customer']              = 'Покупатель';
+$_['column_comment']               = 'Комментарий';
 $_['column_ip']                    = 'IP';
-$_['column_date_added']            = 'Date Added';
+$_['column_date_added']            = 'Дата добавления';
 
 // Entry
-$_['entry_customer']               = 'Customer';
+$_['entry_customer']               = 'Покупатель';
 $_['entry_ip']                     = 'IP';
 $_['entry_date_start']             = 'Date Start';
-$_['entry_date_end']               = 'Date End';
+$_['entry_date_end']               = 'Дата окончания';
 $_['entry_status']                 = 'Статус';
 $_['entry_sort_order']             = 'Sort Order';
 
