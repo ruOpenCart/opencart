9dfd02a06a1c8f3d6956e56e7c93e20da249f125
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:30:04 +0300

    New translations report.php (Russian)

--- upload/admin/language/ru-ru/report/report.php
+++ upload/admin/language/ru-ru/report/report.php
@@ -4,5 +4,5 @@ $_['heading_title'] = 'Reports';
 
 // Text
 $_['text_success']  = 'Success: You have modified reports!';
-$_['text_type']     = 'Choose the report type';
+$_['text_type']     = 'Выберите тип отчёта';
 $_['text_filter']   = 'Filter';
