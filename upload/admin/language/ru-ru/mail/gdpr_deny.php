0635b2bd5c6244d0256252c18cba2c8953823b63
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:04 +0300

    New translations gdpr_deny.php (Russian)

--- upload/admin/language/ru-ru/mail/gdpr_deny.php
+++ upload/admin/language/ru-ru/mail/gdpr_deny.php
@@ -1,12 +1,12 @@
 <?php
 // Text
 $_['text_subject']   = '%s - GDPR запрос отклонен!';
-$_['text_export']    = 'Account Export Data Request';
-$_['text_remove']    = 'Account Deletion Request';
+$_['text_export']    = 'Запрос экспорта данных учетной записи';
+$_['text_remove']    = 'Запрос на удаление учетной записи';
 $_['text_hello']     = 'Hello <strong>%s</strong>,';
 $_['text_user']      = 'User';
 $_['text_contact']   = 'Unfortunately your request has been denied. For more information you can contact the store here:';
 $_['text_thanks']    = 'Thanks,';
 
 // Button
-$_['button_contact'] = 'Contact Us';
+$_['button_contact'] = 'Связаться с нами';
