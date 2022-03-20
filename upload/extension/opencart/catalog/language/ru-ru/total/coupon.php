a760e8db5ce798199b3abf408184291c15fcaefa
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:31:22 +0300

    New translations coupon.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/total/coupon.php
+++ upload/extension/opencart/catalog/language/ru-ru/total/coupon.php
@@ -3,7 +3,7 @@
 $_['heading_title'] = 'Use Coupon Code';
 
 // Text
-$_['text_coupon']   = 'Coupon (%s)';
+$_['text_coupon']   = 'Купон (%s)';
 $_['text_success']  = 'Success: Your coupon discount has been applied!';
 $_['text_remove']   = 'Success: Your coupon discount has been removed!';
 
