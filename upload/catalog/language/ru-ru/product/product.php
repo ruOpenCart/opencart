fe17eec7325d9e2335bae6eb1905acfdc60c87c3
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:31:07 +0300

    New translations product.php (Russian)

--- upload/catalog/language/ru-ru/product/product.php
+++ upload/catalog/language/ru-ru/product/product.php
@@ -1,22 +1,22 @@
 <?php
 // Text
 $_['text_search']                = 'Search';
-$_['text_brand']                 = 'Brand';
-$_['text_manufacturer']          = 'Brand:';
+$_['text_brand']                 = 'Бренд';
+$_['text_manufacturer']          = 'Бренд:';
 $_['text_model']                 = 'Product Code:';
 $_['text_reward']                = 'Reward Points:';
 $_['text_points']                = 'Price in reward points:';
-$_['text_stock']                 = 'Availability:';
+$_['text_stock']                 = 'Доступность:';
 $_['text_instock']               = 'In Stock';
 $_['text_tax']                   = 'Ex Tax:';
 $_['text_discount']              = ' или более ';
-$_['text_option']                = 'Available Options';
+$_['text_option']                = 'Доступные опции';
 $_['text_minimum']               = 'This product has a minimum quantity of %s';
 $_['text_reviews']               = '%s отзыв(ов)';
 $_['text_write']                 = 'Write a review';
 $_['text_login']                 = 'Please <a href="%s">login</a> or <a href="%s">register</a> to review';
 $_['text_no_reviews']            = 'There are no reviews for this product.';
-$_['text_note']                  = '<span class="text-danger">Note:</span> HTML is not translated!';
+$_['text_note']                  = '<span style="color: #FF0000;">Примечание:</span> HTML разметка не поддерживается! Используйте обычный текст!';
 $_['text_success']               = 'Thank you for your review. It has been submitted to the webmaster for approval.';
 $_['text_related']               = 'Related Products';
 $_['text_tags']                  = 'Tags:';
@@ -25,7 +25,7 @@ $_['text_subscription']          = 'Subscription';
 $_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда';
 $_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
 $_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
-$_['text_day']                   = 'day';
+$_['text_day']                   = 'день';
 $_['text_week']                  = 'week';
 $_['text_semi_month']            = 'half-month';
 $_['text_month']                 = 'month';
@@ -37,7 +37,7 @@ $_['entry_name']                 = 'Your Name';
 $_['entry_review']               = 'Your Review';
 $_['entry_rating']               = 'Rating';
 $_['entry_good']                 = 'Good';
-$_['entry_bad']                  = 'Bad';
+$_['entry_bad']                  = 'Плохо';
 
 // Tabs
 $_['tab_description']            = 'Description';
