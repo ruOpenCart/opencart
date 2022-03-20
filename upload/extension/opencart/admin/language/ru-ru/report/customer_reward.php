d9c87faa67e07ba93b5485382d8b52865a9d5cd3
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:30:33 +0300

    New translations customer_reward.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/customer_reward.php
+++ upload/extension/opencart/admin/language/ru-ru/report/customer_reward.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']         = 'Customer Reward Points Report';
+$_['heading_title']         = 'Отчет по бонусным баллам покупателей';
 
 // Text
 $_['text_extension']        = 'Extensions';
@@ -9,9 +9,9 @@ $_['text_success']          = 'Success: You have modified customer reward points
 $_['text_filter']           = 'Filter';
 
 // Column
-$_['column_customer']       = 'Customer Name';
-$_['column_email']          = 'E-Mail';
-$_['column_customer_group'] = 'Customer Group';
+$_['column_customer']       = 'Имя покупателя';
+$_['column_email']          = 'Эл. почта';
+$_['column_customer_group'] = 'Группа покупателя';
 $_['column_status']         = 'Статус';
 $_['column_points']         = 'Reward Points';
 $_['column_orders']         = 'No. Orders';
@@ -20,8 +20,8 @@ $_['column_action']         = 'Действие';
 
 // Entry
 $_['entry_date_start']      = 'Date Start';
-$_['entry_date_end']        = 'Date End';
-$_['entry_customer']        = 'Customer';
+$_['entry_date_end']        = 'Дата окончания';
+$_['entry_customer']        = 'Покупатель';
 $_['entry_status']          = 'Статус';
 $_['entry_sort_order']      = 'Sort Order';
 
