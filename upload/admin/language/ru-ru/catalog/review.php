801feb575770250b0c7c75eb55c7377f104e8e7e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:17 +0300

    New translations review.php (Russian)

--- upload/admin/language/ru-ru/catalog/review.php
+++ upload/admin/language/ru-ru/catalog/review.php
@@ -5,25 +5,25 @@ $_['heading_title']     = 'Reviews';
 // Text
 $_['text_success']      = 'Success: You have modified reviews!';
 $_['text_list']         = 'Review List';
-$_['text_add']          = 'Add Review';
+$_['text_add']          = 'Добавить отзыв';
 $_['text_edit']         = 'Edit Review';
 $_['text_filter']       = 'Filter';
 
 // Column
 $_['column_product']    = 'Product';
-$_['column_author']     = 'Author';
+$_['column_author']     = 'Автор';
 $_['column_rating']     = 'Rating';
 $_['column_status']     = 'Статус';
-$_['column_date_added'] = 'Date Added';
+$_['column_date_added'] = 'Дата добавления';
 $_['column_action']     = 'Действие';
 
 // Entry
 $_['entry_product']     = 'Product';
-$_['entry_author']      = 'Author';
+$_['entry_author']      = 'Автор';
 $_['entry_rating']      = 'Rating';
 $_['entry_status']      = 'Статус';
 $_['entry_text']        = 'Text';
-$_['entry_date_added']  = 'Date Added';
+$_['entry_date_added']  = 'Дата добавления';
 
 // Help
 $_['help_product']      = '(Автодополнение)';
@@ -32,6 +32,6 @@ $_['help_product']      = '(Автодополнение)';
 $_['error_warning']     = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']  = 'Warning: You do not have permission to modify reviews!';
 $_['error_product']     = 'Product required!';
-$_['error_author']      = 'Author must be between 3 and 64 characters!';
+$_['error_author']      = 'Автор должен содержать от 3 до 64 символов!';
 $_['error_text']        = 'Review Text must be at least 1 character!';
 $_['error_rating']      = 'Review rating required!';
