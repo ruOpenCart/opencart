32238a9489c9895b2d805cdb674f0ce962646e74
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:12 +0300

    New translations download.php (Russian)

--- upload/admin/language/ru-ru/catalog/download.php
+++ upload/admin/language/ru-ru/catalog/download.php
@@ -1,26 +1,26 @@
 <?php
 // Heading
-$_['heading_title']     = 'Downloads';
+$_['heading_title']     = 'Загрузки';
 
 // Text
 $_['text_success']      = 'Success: You have modified downloads!';
-$_['text_list']         = 'Download List';
-$_['text_add']          = 'Add Download';
+$_['text_list']         = 'Список загрузок';
+$_['text_add']          = 'Добавить загрузку';
 $_['text_edit']         = 'Edit Download';
 $_['text_upload']       = 'Your file was successfully uploaded!';
 $_['text_report']       = 'Report';
 
 // Column
-$_['column_name']       = 'Download Name';
+$_['column_name']       = 'Название загрузки';
 $_['column_ip']         = 'IP';
-$_['column_account']    = 'Accounts';
+$_['column_account']    = 'Учётные записи';
 $_['column_store']      = 'Store';
-$_['column_country']    = 'Country';
-$_['column_date_added'] = 'Date Added';
+$_['column_country']    = 'Страна';
+$_['column_date_added'] = 'Дата добавления';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_name']        = 'Download Name';
+$_['entry_name']        = 'Название загрузки';
 $_['entry_filename']    = 'Filename';
 $_['entry_mask']        = 'Mask';
 
@@ -31,9 +31,9 @@ $_['help_mask']         = 'It is recommended that the filename and the mask are 
 // Error
 $_['error_warning']     = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']  = 'Warning: You do not have permission to modify downloads!';
-$_['error_name']        = 'Download Name must be between 3 and 64 characters!';
+$_['error_name']        = 'Имя загрузки должно быть от 3 до 64 символов!';
 $_['error_filename']    = 'Filename must be between 3 and 128 characters!';
-$_['error_directory']   = 'Downloads need to be within the storage/download directory!';
+$_['error_directory']   = 'Загрузки должны находиться в папке storage/download!';
 $_['error_exists']      = 'File does not exist!';
 $_['error_mask']        = 'Mask must be between 3 and 128 characters!';
 $_['error_filetype']    = 'Invalid file type!';
