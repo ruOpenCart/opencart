f67a60a5abf472570f1313d515efb61694b2f6b5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-20 23:29:27 +0300

    New translations customer_group.php (Russian)

--- upload/admin/language/ru-ru/customer/customer_group.php
+++ upload/admin/language/ru-ru/customer/customer_group.php
@@ -1,30 +1,30 @@
 <?php
 // Heading
-$_['heading_title']     = 'Customer Groups';
+$_['heading_title']     = 'Группы покупателя';
 
 // Text
 $_['text_success']      = 'Success: You have modified customer groups!';
-$_['text_list']         = 'Customer Group List';
-$_['text_add']          = 'Add Customer Group';
+$_['text_list']         = 'Список групп покупателей';
+$_['text_add']          = 'Добавить группу покупателей';
 $_['text_edit']         = 'Edit Customer Group';
 
 // Column
-$_['column_name']       = 'Customer Group Name';
+$_['column_name']       = 'Название группы покупателей';
 $_['column_sort_order'] = 'Sort Order';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_name']        = 'Customer Group Name';
+$_['entry_name']        = 'Название группы покупателей';
 $_['entry_description'] = 'Description';
-$_['entry_approval']    = 'Approve New Customers';
+$_['entry_approval']    = 'Одобрять новых покупателей';
 $_['entry_sort_order']  = 'Sort Order';
 
 // Help
-$_['help_approval']     = 'Customers must be approved by an administrator before they can login.';
+$_['help_approval']     = 'Клиенты должны быть одобрены администратором, прежде чем они смогут войти.';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify customer groups!';
-$_['error_name']        = 'Customer Group Name must be between 3 and 32 characters!';
+$_['error_name']        = 'Название группы покупателей должно быть от 3 до 32 символов!';
 $_['error_default']     = 'Warning: This customer group cannot be deleted as it is currently assigned as the default store customer group!';
 $_['error_store']       = 'Warning: This customer group cannot be deleted as it is currently assigned to %s stores!';
 $_['error_customer']    = 'Warning: This customer group cannot be deleted as it is currently assigned to %s customers!';
