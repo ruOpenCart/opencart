8751c6f90fdb6bc24ea03daa06e477ab14dd29d7
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:35:15 +0300

    New translations edit.php (Russian)

--- upload/catalog/language/ru-ru/account/edit.php
+++ upload/catalog/language/ru-ru/account/edit.php
@@ -10,7 +10,7 @@ $_['text_success']       = 'Success: Your account has been successfully updated.
 
 // Entry
 $_['entry_firstname']    = 'First Name';
-$_['entry_lastname']     = 'Last Name';
+$_['entry_lastname']     = 'Фамилия';
 $_['entry_email']        = 'Эл. почта';
 $_['entry_telephone']    = 'Telephone';
 
@@ -19,7 +19,7 @@ $_['error_token']        = 'Warning: Edit token invalid!';
 $_['error_exists']       = 'Warning: E-Mail address is already registered!';
 $_['error_firstname']    = 'First Name must be between 1 and 32 characters!';
 $_['error_lastname']     = 'Last Name must be between 1 and 32 characters!';
-$_['error_email']        = 'E-Mail Address does not appear to be valid!';
+$_['error_email']        = 'Адрес электронной почты недействителен!';
 $_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
 $_['error_custom_field'] = '%s обязательно к заполнению!';
 $_['error_regex']        = '%s не является допустимым вводом!';
