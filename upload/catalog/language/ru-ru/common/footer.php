bfae5d8c1b5e97031b0dc1a80da62eb8e2925de6
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:34:38 +0300

    New translations footer.php (Russian)

--- upload/catalog/language/ru-ru/common/footer.php
+++ upload/catalog/language/ru-ru/common/footer.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_information']  = 'Information';
+$_['text_information']  = 'Информация';
 $_['text_service']      = 'Служба поддержки';
 $_['text_extra']        = 'Extras';
 $_['text_contact']      = 'Связаться с нами';
