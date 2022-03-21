10126bd1ed972cb4fbae52e7a66c6323cd0a367a
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:36:25 +0300

    New translations free.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/shipping/free.php
+++ upload/extension/opencart/catalog/language/ru-ru/shipping/free.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']    = 'Free Shipping';
+$_['heading_title']    = 'Бесплатная доставка';
 
 // Text
-$_['text_description'] = 'Free Shipping';
+$_['text_description'] = 'Бесплатная доставка';
