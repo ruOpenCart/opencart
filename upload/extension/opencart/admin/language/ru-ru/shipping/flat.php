3b488d06c7ea2ca66558efef0af0a1a8040cc903
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:35:24 +0300

    New translations flat.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/shipping/flat.php
+++ upload/extension/opencart/admin/language/ru-ru/shipping/flat.php
@@ -3,14 +3,14 @@
 $_['heading_title']    = 'Flat Rate';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified flat rate shipping!';
 $_['text_edit']        = 'Edit Flat Rate Shipping';
 
 // Entry
 $_['entry_cost']       = 'Стоимость';
 $_['entry_tax_class']  = 'Tax Class';
-$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_geo_zone']   = 'Гео-зона';
 $_['entry_status']     = 'Статус';
 $_['entry_sort_order'] = 'Sort Order';
 
