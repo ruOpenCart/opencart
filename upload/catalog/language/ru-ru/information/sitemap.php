a5836a463832b1684c84a4e6bea782f9721a4bcb
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:34:44 +0300

    New translations sitemap.php (Russian)

--- upload/catalog/language/ru-ru/information/sitemap.php
+++ upload/catalog/language/ru-ru/information/sitemap.php
@@ -13,5 +13,5 @@ $_['text_download']    = 'Загрузки';
 $_['text_cart']        = 'Shopping Cart';
 $_['text_checkout']    = 'Оформить заказ';
 $_['text_search']      = 'Search';
-$_['text_information'] = 'Information';
+$_['text_information'] = 'Информация';
 $_['text_contact']     = 'Связаться с нами';
