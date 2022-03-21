9029a885ece0b25c756d646d1d44fd64ad7ba948
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:35:30 +0300

    New translations sale_order.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/sale_order.php
+++ upload/extension/opencart/admin/language/ru-ru/report/sale_order.php
@@ -3,10 +3,10 @@
 $_['heading_title']     = 'Sales Report';
 
 // Text
-$_['text_extension']     = 'Extensions';
+$_['text_extension']     = 'Расширения';
 $_['text_edit']          = 'Edit Sales Report';
 $_['text_success']       = 'Success: You have modified sales report!';
-$_['text_filter']        = 'Filter';
+$_['text_filter']        = 'Фильтр';
 $_['text_year']          = 'Years';
 $_['text_month']         = 'Months';
 $_['text_week']          = 'Weeks';
@@ -14,7 +14,7 @@ $_['text_day']           = 'Дней';
 $_['text_all_status']    = 'Все статусы';
 
 // Column
-$_['column_date_start']  = 'Date Start';
+$_['column_date_start']  = 'Дата начала';
 $_['column_date_end']    = 'Дата окончания';
 $_['column_orders']      = 'No. Orders';
 $_['column_products']    = 'No. Products';
@@ -22,7 +22,7 @@ $_['column_tax']         = 'Tax';
 $_['column_total']       = 'Total';
 
 // Entry
-$_['entry_date_start']   = 'Date Start';
+$_['entry_date_start']   = 'Дата начала';
 $_['entry_date_end']     = 'Дата окончания';
 $_['entry_group']        = 'Group By';
 $_['entry_order_status'] = 'Order Status';
