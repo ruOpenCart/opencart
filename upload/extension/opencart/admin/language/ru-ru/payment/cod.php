4f309210c8bac42be5813351373d1f6452b26904
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:36:14 +0300

    New translations cod.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/payment/cod.php
+++ upload/extension/opencart/admin/language/ru-ru/payment/cod.php
@@ -3,13 +3,13 @@
 $_['heading_title']      = 'Оплата при доставке';
 
 // Text
-$_['text_extension']     = 'Extensions';
+$_['text_extension']     = 'Расширения';
 $_['text_success']       = 'Success: You have modified cash on delivery payment module!';
-$_['text_edit']          = 'Edit Cash On Delivery';
+$_['text_edit']          = 'Редактирование Наличных при получении';
 
 // Entry
 $_['entry_order_status'] = 'Order Status';
-$_['entry_geo_zone']     = 'Geo Zone';
+$_['entry_geo_zone']     = 'Гео-зона';
 $_['entry_status']       = 'Статус';
 $_['entry_sort_order']   = 'Sort Order';
 
