34a19897ae7d3030b916fe6f3c1374c835432b71
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:36:02 +0300

    New translations bestseller.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/bestseller.php
+++ upload/extension/opencart/admin/language/ru-ru/module/bestseller.php
@@ -3,22 +3,22 @@
 $_['heading_title']    = 'Хиты продаж';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified bestsellers module!';
-$_['text_edit']        = 'Edit Bestsellers Module';
+$_['text_edit']        = 'Редактирование модуля хиты продаж';
 $_['text_horizontal']  = 'Horizontal';
 $_['text_vertical']    = 'Vertical';
 
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_axis']       = 'Axis';
-$_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
+$_['entry_limit']      = 'Лимит';
+$_['entry_width']      = 'Ширина изображения';
+$_['entry_height']     = 'Высота изображения';
 $_['entry_status']     = 'Статус';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify bestsellers module!';
 $_['error_name']       = 'Module Name must be between 3 and 64 characters!';
 $_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_height']     = 'Необходимо указать высоту!';
