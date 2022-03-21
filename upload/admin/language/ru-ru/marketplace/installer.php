70e9f77bee9f042ac78ed587c9eff747ce441366
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:34:01 +0300

    New translations installer.php (Russian)

--- upload/admin/language/ru-ru/marketplace/installer.php
+++ upload/admin/language/ru-ru/marketplace/installer.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']     = 'Extension Installer';
+$_['heading_title']     = 'Установщик расширений';
 
 // Text
 $_['text_upload']       = 'Success: The extension has been uploaded!';
 $_['text_install']      = 'Success: The extension has been installed!';
 $_['text_uninstall']    = 'Success: The extension has been uninstalled!';
 $_['text_delete']       = 'Success: The extension has been deleted!';
-$_['text_progress']     = 'Install Progress';
-$_['text_installed']    = 'Installed Extensions';
+$_['text_progress']     = 'Прогресс установки';
+$_['text_installed']    = 'Установленные расширения';
 
 // Column
 $_['column_image']      = 'Image';
-$_['column_name']       = 'Extension Name';
+$_['column_name']       = 'Название расширения';
 $_['column_version']    = 'Version';
 $_['column_date_added'] = 'Дата добавления';
 $_['column_action']     = 'Действие';
@@ -22,9 +22,9 @@ $_['entry_progress']    = 'Progress';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify extensions!';
-$_['error_install']     = 'Install record could not be found!';
-$_['error_file']        = 'Install file %s could not be found!';
-$_['error_directory']   = 'Install directory %s could not be found!';
+$_['error_install']     = 'Запись установки не найдена!';
+$_['error_file']        = 'Файл установки %s не найден!';
+$_['error_directory']   = 'Каталог установки %s не найден!';
 $_['error_exists']      = 'Path %s already exists!';
 $_['error_unzip']       = 'Zip file could not be opened!';
 $_['error_upload']      = 'File could not be uploaded!';
