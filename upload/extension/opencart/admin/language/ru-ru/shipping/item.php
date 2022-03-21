0f0ad7ece853f93b6101dee0083950022f8acabe
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:35:36 +0300

    New translations item.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/shipping/item.php
+++ upload/extension/opencart/admin/language/ru-ru/shipping/item.php
@@ -3,14 +3,14 @@
 $_['heading_title']    = 'Per Item';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified shipping per item rates!';
 $_['text_edit']        = 'Edit Per Item Shipping';
 
 // Entry
 $_['entry_cost']       = 'Стоимость';
 $_['entry_tax_class']  = 'Tax Class';
-$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_geo_zone']   = 'Гео-зона';
 $_['entry_status']     = 'Статус';
 $_['entry_sort_order'] = 'Sort Order';
 
