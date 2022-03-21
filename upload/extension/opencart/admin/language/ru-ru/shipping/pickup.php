58bdb3763f8f95a8acfdc1394ed03d942818c592
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:35:37 +0300

    New translations pickup.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/shipping/pickup.php
+++ upload/extension/opencart/admin/language/ru-ru/shipping/pickup.php
@@ -3,12 +3,12 @@
 $_['heading_title']    = 'Pickup From Store';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified pickup from store!';
 $_['text_edit']        = 'Edit Pickup From Store Shipping';
 
 // Entry
-$_['entry_geo_zone']   = 'Geo Zone';
+$_['entry_geo_zone']   = 'Гео-зона';
 $_['entry_status']     = 'Статус';
 $_['entry_sort_order'] = 'Sort Order';
 
