46cdfa7dabd34a5cb0b40f41c0184c14fbeaf3a3
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:36:17 +0300

    New translations sub_total.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/total/sub_total.php
+++ upload/extension/opencart/admin/language/ru-ru/total/sub_total.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Sub-Total';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified sub-total total!';
 $_['text_edit']        = 'Edit Sub-Total Total';
 
