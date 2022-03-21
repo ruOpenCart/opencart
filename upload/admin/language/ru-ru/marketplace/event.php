10455aa5754cfb849eecf94dbb1aa085bc86cc74
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:33:58 +0300

    New translations event.php (Russian)

--- upload/admin/language/ru-ru/marketplace/event.php
+++ upload/admin/language/ru-ru/marketplace/event.php
@@ -1,20 +1,20 @@
 <?php
 // Heading
-$_['heading_title']     = 'Events';
+$_['heading_title']     = 'События';
 
 // Text
 $_['text_success']      = 'Success: You have modified events!';
-$_['text_list']         = 'Event List';
+$_['text_list']         = 'Список событий';
 $_['text_event']        = 'Events are used by extensions to override the default functionality of your store. If you have issues you can disable or enable the events here.';
-$_['text_info']         = 'Event Information';
+$_['text_info']         = 'Информация о событии';
 
 // Column
-$_['column_code']       = 'Event Code';
+$_['column_code']       = 'Код события';
 $_['column_sort_order'] = 'Sort Order';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['text_description']  = 'Description';
+$_['text_description']  = 'Описание';
 $_['text_trigger']      = 'Trigger';
 $_['text_action']       = 'Действие';
 
