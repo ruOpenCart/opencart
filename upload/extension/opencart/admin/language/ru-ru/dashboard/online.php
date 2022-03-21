118e0306e6513df22592feb4d3e313651b29d264
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:35:51 +0300

    New translations online.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/dashboard/online.php
+++ upload/extension/opencart/admin/language/ru-ru/dashboard/online.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'People Online';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified dashboard online!';
 $_['text_edit']        = 'Edit Dashboard Online';
 $_['text_view']        = 'View more...';
