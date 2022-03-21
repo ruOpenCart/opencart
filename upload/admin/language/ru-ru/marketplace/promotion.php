7db95e5bc74392c656681a58331b6750a8f20dc3
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:34:03 +0300

    New translations promotion.php (Russian)

--- upload/admin/language/ru-ru/marketplace/promotion.php
+++ upload/admin/language/ru-ru/marketplace/promotion.php
@@ -1,6 +1,6 @@
 <?php
 // Text
 $_['text_recommended'] = 'Recommended';
-$_['text_install']     = 'Install';
+$_['text_install']     = 'Установить';
 $_['text_uninstall']   = 'Uninstall';
 $_['text_delete']      = 'Удалить';
