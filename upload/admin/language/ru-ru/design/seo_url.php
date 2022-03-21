ee2a6f9b95cb804bf0a24097a4e96e4297e9f36b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:34:29 +0300

    New translations seo_url.php (Russian)

--- upload/admin/language/ru-ru/design/seo_url.php
+++ upload/admin/language/ru-ru/design/seo_url.php
@@ -7,23 +7,23 @@ $_['text_success']         = 'Success: You have modified SEO URL!';
 $_['text_list']            = 'SEO URL List';
 $_['text_add']             = 'Добавить SEO URL-адрес';
 $_['text_edit']            = 'Edit SEO URL';
-$_['text_filter']          = 'Filter';
+$_['text_filter']          = 'Фильтр';
 $_['text_default']         = 'По умолчанию';
 
 // Column
-$_['column_key']           = 'Key';
+$_['column_key']           = 'Ключ';
 $_['column_value']         = 'Value';
-$_['column_keyword']       = 'Keyword';
+$_['column_keyword']       = 'Ключевое слово';
 $_['column_store']         = 'Store';
-$_['column_language']      = 'Language';
+$_['column_language']      = 'Язык';
 $_['column_action']        = 'Действие';
 
 // Entry
 $_['entry_store']          = 'Store';
-$_['entry_language']       = 'Language';
-$_['entry_key']            = 'Key';
+$_['entry_language']       = 'Язык';
+$_['entry_key']            = 'Ключ';
 $_['entry_value']          = 'Value';
-$_['entry_keyword']        = 'Keyword';
+$_['entry_keyword']        = 'Ключевое слово';
 $_['entry_sort_order']     = 'Sort Order';
 
 // Help
@@ -33,8 +33,8 @@ $_['help_sort_order']      = 'The sort order of the keywords in the URL.';
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify SEO URL!';
 $_['error_exists']         = 'Warning: Combination of store, language key, value, keyword already exists!';
-$_['error_key']            = 'Key must be between 1 and 64 characters!';
+$_['error_key']            = 'Ключ должен быть от 1 до 64 символов!';
 $_['error_value']          = 'Value must be between 1 and 255 characters!';
 $_['error_value_exists']   = 'Value is already in use!';
-$_['error_keyword']        = 'Keyword must be between 3 and 64 characters!';
-$_['error_keyword_exists'] = 'Keyword is already in use!';
+$_['error_keyword']        = 'Ключевое слово должно быть от 3 до 64 символов!';
+$_['error_keyword_exists'] = 'Ключевое слово уже используется!';
