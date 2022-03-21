e2cab54981209f512bdb88a3048419e97741d6e9
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:36:10 +0300

    New translations special.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/special.php
+++ upload/extension/opencart/admin/language/ru-ru/module/special.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Specials';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified module specials!';
 $_['text_edit']        = 'Edit Specials Module';
 $_['text_horizontal']  = 'Horizontal';
@@ -12,13 +12,13 @@ $_['text_vertical']    = 'Vertical';
 // Entry
 $_['entry_name']       = 'Module Name';
 $_['entry_axis']       = 'Axis';
-$_['entry_limit']      = 'Limit';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
+$_['entry_limit']      = 'Лимит';
+$_['entry_width']      = 'Ширина изображения';
+$_['entry_height']     = 'Высота изображения';
 $_['entry_status']     = 'Статус';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify specials module!';
 $_['error_name']       = 'Module Name must be between 3 and 64 characters!';
 $_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_height']     = 'Необходимо указать высоту!';
