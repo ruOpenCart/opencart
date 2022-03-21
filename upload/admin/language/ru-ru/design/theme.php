bb849fa1da1bc6006bc933874bc0f432f2a02e75
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:34:20 +0300

    New translations theme.php (Russian)

--- upload/admin/language/ru-ru/design/theme.php
+++ upload/admin/language/ru-ru/design/theme.php
@@ -8,7 +8,7 @@ $_['text_edit']         = 'Edit Theme';
 $_['text_store']        = 'Выберите магазин';
 $_['text_template']     = 'Выберите шаблон';
 $_['text_default']      = 'По умолчанию';
-$_['text_extension']    = 'Extension';
+$_['text_extension']    = 'Расширение';
 $_['text_history']      = 'Theme History';
 $_['text_twig']         = 'The theme editor uses the template language Twig. You can read about <a href="https://twig.symfony.com/doc/" target="_blank" class="alert-link">Twig syntax here</a>.';
 
