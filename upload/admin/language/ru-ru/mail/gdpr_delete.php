fc54d6f669fa12055ef4088adb4c519602b2eca6
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:33:50 +0300

    New translations gdpr_delete.php (Russian)

--- upload/admin/language/ru-ru/mail/gdpr_delete.php
+++ upload/admin/language/ru-ru/mail/gdpr_delete.php
@@ -2,7 +2,7 @@
 // Text
 $_['text_subject']   = '%s - GDPR запрос был обработан!';
 $_['text_request']   = 'Запрос на удаление учетной записи';
-$_['text_hello']     = 'Hello <strong>%s</strong>,';
+$_['text_hello']     = 'Привет <strong>%s</strong>,';
 $_['text_user']      = 'User';
 $_['text_delete']    = 'Your GDPR data deletion request has now been completed.';
 $_['text_contact']   = 'For more information you can contact the store owner here:';
