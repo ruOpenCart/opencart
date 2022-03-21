56aad464d3491c8159df8382c3c20005dc1c37a2
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-21 23:35:49 +0300

    New translations featured.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/featured.php
+++ upload/extension/opencart/admin/language/ru-ru/module/featured.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Featured';
 
 // Text
-$_['text_extension']   = 'Extensions';
+$_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified featured module!';
 $_['text_edit']        = 'Edit Featured Module';
 $_['text_horizontal']  = 'Horizontal';
@@ -13,8 +13,8 @@ $_['text_vertical']    = 'Vertical';
 $_['entry_name']       = 'Module Name';
 $_['entry_product']    = 'Products';
 $_['entry_axis']       = 'Axis';
-$_['entry_width']      = 'Image Width';
-$_['entry_height']     = 'Image Height';
+$_['entry_width']      = 'Ширина изображения';
+$_['entry_height']     = 'Высота изображения';
 $_['entry_status']     = 'Статус';
 
 // Help
@@ -24,4 +24,4 @@ $_['help_product']     = '(Автодополнение)';
 $_['error_permission'] = 'Warning: You do not have permission to modify featured module!';
 $_['error_name']       = 'Module Name must be between 3 and 64 characters!';
 $_['error_width']      = 'Width required!';
-$_['error_height']     = 'Height required!';
+$_['error_height']     = 'Необходимо указать высоту!';
