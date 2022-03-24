e6139ae6cfd49eb07c5e6460537511dc1d14c42e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-24 21:48:09 +0300

    New translations ecb.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/currency/ecb.php
+++ upload/extension/opencart/admin/language/ru-ru/currency/ecb.php
@@ -5,7 +5,7 @@ $_['heading_title']    = 'European Central Bank Currency Converter';
 // Text
 $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified European Central Bank Currency Converter!';
-$_['text_edit']        = 'Edit European Central Bank';
+$_['text_edit']        = 'Редактирование Европейского Центрального Банка';
 $_['text_support']     = 'This extension requires at EUR currency to be available currency option.';
 
 // Entry
