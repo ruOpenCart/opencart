4c71012e1e94ccbf46ed8e7cabe30c056ecc0594
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-24 21:47:30 +0300

    New translations recent.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/dashboard/recent.php
+++ upload/extension/opencart/admin/language/ru-ru/dashboard/recent.php
@@ -5,7 +5,7 @@ $_['heading_title']     = 'Latest Orders';
 // Text
 $_['text_extension']    = 'Расширения';
 $_['text_success']      = 'Success: You have modified dashboard recent orders!';
-$_['text_edit']         = 'Edit Dashboard Recent Orders';
+$_['text_edit']         = 'Редактирование последних заказов на панели';
 
 // Column
 $_['column_order_id']   = 'Order ID';
