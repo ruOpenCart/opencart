9038b4b92200d2f9be6a87b522f239b9662f6062
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-24 21:48:10 +0300

    New translations fixer.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/currency/fixer.php
+++ upload/extension/opencart/admin/language/ru-ru/currency/fixer.php
@@ -5,7 +5,7 @@ $_['heading_title']    = 'Fixer';
 // Text
 $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified fixer currency rates!';
-$_['text_edit']        = 'Edit Fixer';
+$_['text_edit']        = 'Редактирование Fixer';
 $_['text_signup']      = 'Fixer.io is a currency conversion service <a href="https://fixer.io/" target="_blank" class="alert-link">signup here</a>.';
 $_['text_support']     = 'This extension Requires at EUR currency to be available currency option.';
 
