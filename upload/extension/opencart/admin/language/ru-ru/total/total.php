8b90a03faf9a82baab95e7fa3daa490aaae545d3
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:16:18 +0300

    New translations total.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/total/total.php
+++ upload/extension/opencart/admin/language/ru-ru/total/total.php
@@ -5,7 +5,7 @@ $_['heading_title']    = 'Total';
 // Text
 $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified total totals!';
-$_['text_edit']        = 'Edit Total Total';
+$_['text_edit']        = 'Редактирование общей суммы';
 
 // Entry
 $_['entry_status']     = 'Статус';
