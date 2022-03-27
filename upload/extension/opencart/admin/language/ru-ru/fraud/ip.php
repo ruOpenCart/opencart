33113c61e7848db14d29ca81c2aa4aeccb67a557
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:16:35 +0300

    New translations ip.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/fraud/ip.php
+++ upload/extension/opencart/admin/language/ru-ru/fraud/ip.php
@@ -7,16 +7,16 @@ $_['text_extension']     = 'Расширения';
 $_['text_success']       = 'Success: You have modified Anti-Fraud IP!';
 $_['text_edit']          = 'Редактирование IP-адреса анти-мошенничества';
 $_['text_ip_add']        = 'Добавить IP-адрес';
-$_['text_ip_list']       = 'Fraud IP Address List';
+$_['text_ip_list']       = 'Список мошеннических IP-адресов';
 
 // Column
-$_['column_ip']          = 'IP';
+$_['column_ip']          = 'IP-адрес';
 $_['column_total']       = 'Total Accounts';
 $_['column_date_added']  = 'Дата добавления';
 $_['column_action']      = 'Действие';
 
 // Entry
-$_['entry_ip']           = 'IP';
+$_['entry_ip']           = 'IP-адрес';
 $_['entry_status']       = 'Статус';
 $_['entry_order_status'] = 'Order Status';
 
@@ -25,5 +25,5 @@ $_['help_order_status']  = 'Клиентам, у которых заблокир
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify Anti-Fraud IP!';
-$_['error_required']     = 'IP Address required!';
-$_['error_invalid']      = 'IP Address invalid!';
+$_['error_required']     = 'Требуется указать IP-адрес!';
+$_['error_invalid']      = 'Недопустимый IP-адрес!';
