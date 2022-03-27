99039e32de6a77558ce1f4757f0f583fbbb5fef9
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:15:24 +0300

    New translations confirm.php (Russian)

--- upload/catalog/language/ru-ru/checkout/confirm.php
+++ upload/catalog/language/ru-ru/checkout/confirm.php
@@ -6,13 +6,13 @@ $_['text_subscription_duration'] = '%s каждые %d %s для %d платеж
 $_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
 $_['text_day']                   = 'день';
 $_['text_week']                  = 'week';
-$_['text_semi_month']            = 'half-month';
+$_['text_semi_month']            = 'полмесяца';
 $_['text_month']                 = 'month';
 $_['text_year']                  = 'year';
 
 // Column
 $_['column_name']                = 'Product Name';
-$_['column_model']               = 'Model';
+$_['column_model']               = 'Модель';
 $_['column_quantity']            = 'Quantity';
 $_['column_price']               = 'Unit Price';
 $_['column_total']               = 'Total';
