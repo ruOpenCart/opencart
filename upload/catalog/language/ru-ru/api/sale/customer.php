3eab07f3f73900f2ba176c25acd9f64cdc3df0d0
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:15:44 +0300

    New translations customer.php (Russian)

--- upload/catalog/language/ru-ru/api/sale/customer.php
+++ upload/catalog/language/ru-ru/api/sale/customer.php
@@ -5,8 +5,8 @@ $_['text_success']         = 'You have successfully modified customers';
 // Error
 $_['error_customer']       = 'Warning: Customer could not be found!';
 $_['error_customer_group'] = 'Группа покупателей недействительна!';
-$_['error_firstname']      = 'First Name must be between 1 and 32 characters!';
-$_['error_lastname']       = 'Last Name must be between 1 and 32 characters!';
+$_['error_firstname']      = 'Имя должно содержать от 1 до 32 символов!';
+$_['error_lastname']       = 'Фамилия должна быть от 1 до 32 символов!';
 $_['error_email']          = 'Адрес электронной почты недействителен!';
 $_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';
 $_['error_custom_field']   = '%s обязательно к заполнению!';
