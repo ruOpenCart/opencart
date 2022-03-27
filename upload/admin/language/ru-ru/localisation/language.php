22c37025f9d712d0ab9733ffeced69872ea7a601
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:14:53 +0300

    New translations language.php (Russian)

--- upload/admin/language/ru-ru/localisation/language.php
+++ upload/admin/language/ru-ru/localisation/language.php
@@ -17,12 +17,12 @@ $_['column_action']     = 'Действие';
 // Entry
 $_['entry_name']        = 'Название языка';
 $_['entry_code']        = 'Код';
-$_['entry_locale']      = 'Locale';
+$_['entry_locale']      = 'Локаль';
 $_['entry_status']      = 'Статус';
 $_['entry_sort_order']  = 'Sort Order';
 
 // Help
-$_['help_locale']       = 'Example: en_US.UTF-8,en_US,en-gb,en_gb,english';
+$_['help_locale']       = 'Пример: ru_RU.UTF-8,ru_RU,ru-ru,ru_ru,russian';
 $_['help_status']       = 'Скрыть/показать его в раскрывающемся списке языков';
 
 // Error
@@ -30,7 +30,7 @@ $_['error_permission']  = 'Warning: You do not have permission to modify languag
 $_['error_exists']      = 'Warning: You have already added this language!';
 $_['error_name']        = 'Название языка должно быть от 1 до 32 символов!';
 $_['error_code']        = 'Код языка должен быть от 2 до 5 символов!';
-$_['error_locale']      = 'Locale required!';
+$_['error_locale']      = 'Локаль обязательна!';
 $_['error_default']     = 'Warning: This language cannot be deleted as it is currently assigned as the default store language!';
 $_['error_admin']       = 'Warning: This Language cannot be deleted as it is currently assigned as the administration language!';
 $_['error_store']       = 'Warning: This language cannot be deleted as it is currently assigned to %s stores!';
