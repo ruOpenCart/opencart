a1333834f1f821f7bcf2fc618972ca13b9f1e67c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:16:25 +0300

    New translations coupon.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/total/coupon.php
+++ upload/extension/opencart/catalog/language/ru-ru/total/coupon.php
@@ -8,7 +8,7 @@ $_['text_success']  = 'Success: Your coupon discount has been applied!';
 $_['text_remove']   = 'Success: Your coupon discount has been removed!';
 
 // Entry
-$_['entry_coupon']  = 'Enter your coupon here';
+$_['entry_coupon']  = 'Введите ваш купон здесь';
 
 // Error
 $_['error_coupon']  = 'Warning: Coupon is either invalid, expired or reached its usage limit!';
