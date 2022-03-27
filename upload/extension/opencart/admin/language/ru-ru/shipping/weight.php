82ae99a0705a59b3e285021f2eed629e7aab8653
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:16:20 +0300

    New translations weight.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/shipping/weight.php
+++ upload/extension/opencart/admin/language/ru-ru/shipping/weight.php
@@ -5,7 +5,7 @@ $_['heading_title']    = 'Weight Based Shipping';
 // Text
 $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified weight based shipping!';
-$_['text_edit']        = 'Edit Weight Based Shipping';
+$_['text_edit']        = 'Редактирование доставки на основе веса';
 
 // Entry
 $_['entry_rate']       = 'Rates';
@@ -15,7 +15,7 @@ $_['entry_status']     = 'Статус';
 $_['entry_sort_order'] = 'Sort Order';
 
 // Help
-$_['help_rate']        = 'Example: 5:10.00,7:12.00 Weight:Cost,Weight:Cost, etc..';
+$_['help_rate']        = 'Например: 5:10.00,7:12.00 Вес:Цена,Вес:Цена, и т.д.';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify weight based shipping!';
