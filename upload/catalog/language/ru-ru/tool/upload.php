ab0ce58f64c770a49ebfb4b748609a3d30565b98
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:16:31 +0300

    New translations upload.php (Russian)

--- upload/catalog/language/ru-ru/tool/upload.php
+++ upload/catalog/language/ru-ru/tool/upload.php
@@ -3,6 +3,6 @@
 $_['text_upload']    = 'Your file was successfully uploaded!';
 
 // Error
-$_['error_filename'] = 'Filename must be between 3 and 64 characters!';
-$_['error_filetype'] = 'Invalid file type!';
+$_['error_filename'] = 'Имя файла должно быть от 3 до 64 символов!';
+$_['error_filetype'] = 'Недопустимый тип файла!';
 $_['error_upload']   = 'Upload required!';
