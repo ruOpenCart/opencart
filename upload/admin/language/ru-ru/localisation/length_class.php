1e177ab1c4f439ebf37702f4a438aa1f4af4c521
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:14:32 +0300

    New translations length_class.php (Russian)

--- upload/admin/language/ru-ru/localisation/length_class.php
+++ upload/admin/language/ru-ru/localisation/length_class.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']    = 'Length Classes';
+$_['heading_title']    = 'Классы длины';
 
 // Text
 $_['text_success']     = 'Success: You have modified length classes!';
-$_['text_list']        = 'Length Class List';
+$_['text_list']        = 'Список классов длины';
 $_['text_add']         = 'Добавить класс длины';
 $_['text_edit']        = 'Редактирование класса длины';
 
 // Column
-$_['column_title']     = 'Length Title';
-$_['column_unit']      = 'Length Unit';
+$_['column_title']     = 'Заголовок длины';
+$_['column_unit']      = 'Единица длины';
 $_['column_value']     = 'Value';
 $_['column_action']    = 'Действие';
 
 // Entry
-$_['entry_title']      = 'Length Title';
-$_['entry_unit']       = 'Length Unit';
+$_['entry_title']      = 'Заголовок длины';
+$_['entry_unit']       = 'Единица длины';
 $_['entry_value']      = 'Value';
 
 // Help
@@ -24,7 +24,7 @@ $_['help_value']       = 'The value of your default length in the current length
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify length classes!';
-$_['error_title']      = 'Length Title must be between 3 and 32 characters!';
-$_['error_unit']       = 'Length Unit must be between 1 and 4 characters!';
+$_['error_title']      = 'Длина заголовка должна быть от 3 до 32 символов!';
+$_['error_unit']       = 'Единица длины должна быть от 1 до 4 символов!';
 $_['error_default']    = 'Warning: This length class cannot be deleted as it is currently assigned as the default store length class!';
 $_['error_product']    = 'Warning: This length class cannot be deleted as it is currently assigned to %s products!';
