c53a889c1c5b2185efe6f9249a1a79c2b2ee04ca
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-27 20:16:28 +0300

    New translations basic.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/theme/basic.php
+++ upload/extension/opencart/admin/language/ru-ru/theme/basic.php
@@ -12,7 +12,7 @@ $_['text_image']                       = 'Изображения';
 
 // Entry
 $_['entry_status']                     = 'Статус';
-$_['entry_product_description_length'] = 'List Description Limit';
+$_['entry_product_description_length'] = 'Ограничение описания списка';
 $_['entry_pagination']                 = 'Товаров на странице по умолчанию';
 $_['entry_image_category']             = 'Размер изображения категории (Ш х В)';
 $_['entry_image_thumb']                = 'Product Image Thumb Size (W x H)';
@@ -29,7 +29,7 @@ $_['entry_height']                     = 'Высота';
 
 // Help
 $_['help_pagination']                  = 'Определяет, сколько элементов отображается на странице (товаров, категорий и т. д.)';
-$_['help_product_description_length']  = 'In the list view, short description character limit (categories, special etc)';
+$_['help_product_description_length']  = 'Максимальное количество символов в кратком описании в виде списка (категории, специальные и т. д.)';
 
 // Error
 $_['error_permission']                 = 'Warning: You do not have permission to modify the default store theme!';
