09d338fc6e38080e2cdeee1291c3fbe1f19b7792
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-28 20:14:33 +0300

    New translations manufacturer.php (Russian)

--- upload/admin/language/ru-ru/catalog/manufacturer.php
+++ upload/admin/language/ru-ru/catalog/manufacturer.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']     = 'Manufacturers';
+$_['heading_title']     = 'Производители';
 
 // Text
 $_['text_success']      = 'Success: You have modified manufacturers!';
-$_['text_list']         = 'Manufacturer List';
+$_['text_list']         = 'Список производителей';
 $_['text_add']          = 'Добавить производителя';
 $_['text_edit']         = 'Редактирование производителя';
 $_['text_default']      = 'По умолчанию';
 $_['text_keyword']      = 'Не используйте пробелы, вместо этого заменяйте пробелы на - и убедитесь, что Семантический URL-адрес является глобально уникальным.';
 
 // Column
-$_['column_name']       = 'Manufacturer Name';
+$_['column_name']       = 'Имя производителя';
 $_['column_sort_order'] = 'Sort Order';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_name']        = 'Manufacturer Name';
+$_['entry_name']        = 'Имя производителя';
 $_['entry_store']       = 'Stores';
 $_['entry_keyword']     = 'Ключевое слово';
 $_['entry_image']       = 'Изображение';
@@ -26,7 +26,7 @@ $_['entry_layout']      = 'Переопределение макета';
 // Error
 $_['error_warning']     = 'Warning: Please check the form carefully for errors!';
 $_['error_permission']  = 'Warning: You do not have permission to modify manufacturers!';
-$_['error_name']        = 'Manufacturer Name must be between 1 and 64 characters!';
+$_['error_name']        = 'Имя производителя должно быть от 2 до 64 символов!';
 $_['error_seo']         = 'SEO URL keyword required!';
 $_['error_keyword']     = 'SEO URL must be unique!';
 $_['error_product']     = 'Warning: This manufacturer cannot be deleted as it is currently assigned to %s products!';
