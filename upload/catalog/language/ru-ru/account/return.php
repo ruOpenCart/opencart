6641d64467408f022451c318f4799b8f6bbaa2fe
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-29 20:13:40 +0300

    New translations return.php (Russian)

--- upload/catalog/language/ru-ru/account/return.php
+++ upload/catalog/language/ru-ru/account/return.php
@@ -7,13 +7,13 @@ $_['text_account']       = 'Аккаунт';
 $_['text_return']        = 'Return Information';
 $_['text_return_detail'] = 'Return Details';
 $_['text_description']   = 'Please complete the form below to request an RMA number.';
-$_['text_order']         = 'Order Information';
+$_['text_order']         = 'Информация о заказе';
 $_['text_product']       = 'Product Information';
 $_['text_reason']        = 'Reason for Return';
 $_['text_message']       = '<p>Вы отправили запрос на возврат.</p><p> Уведомления о статусе запроса вы получите на ваш e-mail. Спасибо!</p>';
 $_['text_return_id']     = 'Return ID:';
-$_['text_orders_id']     = 'Order ID:';
-$_['text_date_ordered']  = 'Order Date:';
+$_['text_orders_id']     = 'Номер заказа:';
+$_['text_date_ordered']  = 'Дата заказа:';
 $_['text_status']        = 'Status:';
 $_['text_date_added']    = 'Дата добавления:';
 $_['text_comment']       = 'Return Comments';
@@ -23,7 +23,7 @@ $_['text_agree']         = 'Я прочитал и согласен с <a href="
 
 // Column
 $_['column_return_id']   = 'Return ID';
-$_['column_order_id']    = 'Order ID';
+$_['column_order_id']    = 'Номер заказа';
 $_['column_status']      = 'Статус';
 $_['column_date_added']  = 'Дата добавления';
 $_['column_customer']    = 'Покупатель';
@@ -37,8 +37,8 @@ $_['column_reason']      = 'Reason';
 $_['column_action']      = 'Действие';
 
 // Entry
-$_['entry_order_id']     = 'Order ID';
-$_['entry_date_ordered'] = 'Order Date';
+$_['entry_order_id']     = 'Номер заказа';
+$_['entry_date_ordered'] = 'Дата заказа';
 $_['entry_firstname']    = 'Имя';
 $_['entry_lastname']     = 'Фамилия';
 $_['entry_email']        = 'Эл. почта';
@@ -53,7 +53,7 @@ $_['entry_fault_detail'] = 'Неисправный или другие данн�
 // Error
 $_['text_error']         = 'The returns you requested could not be found!';
 $_['error_token']        = 'Warning: Reset token invalid!';
-$_['error_order_id']     = 'Order ID required!';
+$_['error_order_id']     = 'Необходимо указать номер заказа!';
 $_['error_firstname']    = 'Имя должно содержать от 1 до 32 символов!';
 $_['error_lastname']     = 'Фамилия должна быть от 1 до 32 символов!';
 $_['error_email']        = 'Адрес электронной почты недействителен!';
