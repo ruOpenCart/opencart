a7a2d893adbac14cdfa4dc9269dda93107f5d0c3
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-29 20:14:48 +0300

    New translations cheque.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/payment/cheque.php
+++ upload/extension/opencart/catalog/language/ru-ru/payment/cheque.php
@@ -4,6 +4,6 @@ $_['heading_title']     = 'Чек / Денежный перевод';
 
 // Text
 $_['text_instruction'] = 'Инструкции по оплате чеком / денежным переводом';
-$_['text_payable']     = 'Make Payable To: ';
+$_['text_payable']     = 'Получатель платежа: ';
 $_['text_address']     = 'Send To: ';
 $_['text_payment']     = 'Your order will not ship until we receive payment.';
