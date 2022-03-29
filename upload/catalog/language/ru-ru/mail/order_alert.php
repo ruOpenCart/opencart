f72270fe6da475fc32a57c56639c1b8e3b482af6
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-29 20:14:02 +0300

    New translations order_alert.php (Russian)

--- upload/catalog/language/ru-ru/mail/order_alert.php
+++ upload/catalog/language/ru-ru/mail/order_alert.php
@@ -2,7 +2,7 @@
 // Text
 $_['text_subject']      = '%s - Заказ %s';
 $_['text_received']     = 'You have received an order.';
-$_['text_order_id']     = 'Order ID:';
+$_['text_order_id']     = 'Номер заказа:';
 $_['text_date_added']   = 'Дата добавления:';
 $_['text_order_status'] = 'Order Status:';
 $_['text_product']      = 'Products:';
