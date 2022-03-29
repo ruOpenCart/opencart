327f84565a52f8eb21c69ded79d1245c5a6a7bff
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-29 20:14:09 +0300

    New translations newsletter.php (Russian)

--- upload/catalog/language/ru-ru/account/newsletter.php
+++ upload/catalog/language/ru-ru/account/newsletter.php
@@ -1,10 +1,10 @@
 <?php
 // Heading
-$_['heading_title']    = 'Newsletter Subscription';
+$_['heading_title']    = 'Подписка на рассылку';
 
 // Text
 $_['text_account']     = 'Аккаунт';
-$_['text_newsletter']  = 'Newsletter';
+$_['text_newsletter']  = 'Рассылка';
 $_['text_success']     = 'Success: Your newsletter subscription has been successfully updated!';
 
 // Entry
