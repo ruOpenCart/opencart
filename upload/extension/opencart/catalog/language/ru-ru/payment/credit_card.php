1cab5dc9ac28fe7287f6d094bcb2c6ccb0fe748c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-30 21:56:22 +0300

    New translations credit_card.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/payment/credit_card.php
+++ upload/extension/opencart/catalog/language/ru-ru/payment/credit_card.php
@@ -3,7 +3,7 @@
 $_['heading_title']      = 'Оплата кредитной картой';
 
 // Text
-$_['text_title']         = 'Please enter your card details below';
+$_['text_title']         = 'Пожалуйста, введите данные вашей карты ниже';
 $_['text_description']   = 'This payment method is a dummy one and will not store credit card information nor should it be used on a live site.';
 $_['text_month']         = 'Месяцы';
 $_['text_year']          = 'Years';
