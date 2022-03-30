df043a6a485e0dee1f90fde318b55350a827a351
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-30 21:56:20 +0300

    New translations bank_transfer.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/payment/bank_transfer.php
+++ upload/extension/opencart/catalog/language/ru-ru/payment/bank_transfer.php
@@ -4,5 +4,5 @@ $_['heading_title']     = 'Банковский перевод';
 
 // Text
 $_['text_instruction'] = 'Инструкции по банковскому переводу';
-$_['text_description'] = 'Please transfer the total amount to the following bank account.';
+$_['text_description'] = 'Пожалуйста, переведите оплату на следующий банковский счет.';
 $_['text_payment']     = 'Your order will not ship until we receive payment.';
