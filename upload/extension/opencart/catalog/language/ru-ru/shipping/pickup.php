7397f4b59d33632a36a41129e7ab62f3c16d2409
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-30 21:56:24 +0300

    New translations pickup.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/shipping/pickup.php
+++ upload/extension/opencart/catalog/language/ru-ru/shipping/pickup.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']    = 'Pickup';
+$_['heading_title']    = 'Самовывоз';
 
 // Text
-$_['text_description'] = 'Pickup From Store';
+$_['text_description'] = 'Самовывоз из магазина';
