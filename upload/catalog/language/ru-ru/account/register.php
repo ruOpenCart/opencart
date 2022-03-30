2b6a757a2a8aeb60ef8e54da6afd878a98060b88
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-30 21:55:59 +0300

    New translations register.php (Russian)

--- upload/catalog/language/ru-ru/account/register.php
+++ upload/catalog/language/ru-ru/account/register.php
@@ -18,8 +18,8 @@ $_['entry_lastname']       = 'Фамилия';
 $_['entry_email']          = 'Эл. почта';
 $_['entry_telephone']      = 'Telephone';
 $_['entry_newsletter']     = 'Subscribe';
-$_['entry_password']       = 'Password';
-$_['entry_confirm']        = 'Password Confirm';
+$_['entry_password']       = 'Пароль';
+$_['entry_confirm']        = 'Подтверждение пароля';
 
 // Error
 $_['error_token']          = 'Warning: Register token invalid!';
@@ -31,6 +31,6 @@ $_['error_email']          = 'Адрес электронной почты не�
 $_['error_telephone']      = 'Telephone must be between 3 and 32 characters!';;
 $_['error_custom_field']   = '%s обязательно к заполнению!';
 $_['error_regex']          = '%s не является допустимым вводом!';
-$_['error_password']       = 'Password must be between 4 and 20 characters!';
-$_['error_confirm']        = 'Password confirmation does not match password!';
+$_['error_password']       = 'Пароль должен быть от 4 до 20 символов!';
+$_['error_confirm']        = 'Подтверждение пароля не совпадает с паролем!';
 $_['error_agree']          = 'Warning: You must agree to the %s!';
