28f7545f377a948671248751d3241e7b88cfe112
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-30 21:56:21 +0300

    New translations cod.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/payment/cod.php
+++ upload/extension/opencart/catalog/language/ru-ru/payment/cod.php
@@ -4,4 +4,4 @@ $_['heading_title']       = 'Оплата при доставке';
 
 // Error
 $_['error_order_id']       = 'Идентификатор заказа отсутствует в сессии!';
-$_['error_payment_method'] = 'Payment method is incorrect!';
+$_['error_payment_method'] = 'Неправильный способ оплаты!';
