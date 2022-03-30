c48f4e5d4196b79502ec1d79eebe1fca41052ba2
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-30 21:55:16 +0300

    New translations other.php (Russian)

--- upload/admin/language/ru-ru/extension/other.php
+++ upload/admin/language/ru-ru/extension/other.php
@@ -1,13 +1,13 @@
 <?php
 // Heading
-$_['heading_title']    = 'Other';
+$_['heading_title']    = 'Другие';
 
 // Text
 $_['text_success']     = 'Success: You have modified other extension!';
-$_['text_list']        = 'Other List';
+$_['text_list']        = 'Другой список';
 
 // Column
-$_['column_name']      = 'Other Name';
+$_['column_name']      = 'Другое имя';
 $_['column_status']    = 'Статус';
 $_['column_action']    = 'Действие';
 
