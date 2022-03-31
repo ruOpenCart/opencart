6313a553797b706edd1779412d29e6128c8edfaa
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-31 22:17:27 +0300

    New translations map.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/dashboard/map.php
+++ upload/extension/opencart/admin/language/ru-ru/dashboard/map.php
@@ -7,7 +7,7 @@ $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified dashboard map!';
 $_['text_edit']        = 'Редактирование карты на панели';
 $_['text_order']       = 'Заказы';
-$_['text_sale']        = 'Sales';
+$_['text_sale']        = 'Продажи';
 
 // Entry
 $_['entry_status']     = 'Статус';
