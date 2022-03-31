c8cc3f7a5173d8cc05218f69cef087e4f160e789
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-31 22:16:50 +0300

    New translations return.php (Russian)

--- upload/catalog/language/ru-ru/account/return.php
+++ upload/catalog/language/ru-ru/account/return.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']      = 'Product Returns';
+$_['heading_title']      = 'Возврат товара';
 
 // Text
 $_['text_account']       = 'Аккаунт';
@@ -8,15 +8,15 @@ $_['text_return']        = 'Return Information';
 $_['text_return_detail'] = 'Return Details';
 $_['text_description']   = 'Пожалуйста, заполните форму ниже, чтобы запросить номер RMA.';
 $_['text_order']         = 'Информация о заказе';
-$_['text_product']       = 'Product Information';
-$_['text_reason']        = 'Reason for Return';
+$_['text_product']       = 'Информация о товаре';
+$_['text_reason']        = 'Причина возврата';
 $_['text_message']       = '<p>Вы отправили запрос на возврат.</p><p> Уведомления о статусе запроса вы получите на ваш e-mail. Спасибо!</p>';
 $_['text_return_id']     = 'Return ID:';
 $_['text_orders_id']     = 'Номер заказа:';
 $_['text_date_ordered']  = 'Дата заказа:';
 $_['text_status']        = 'Status:';
 $_['text_date_added']    = 'Дата добавления:';
-$_['text_comment']       = 'Return Comments';
+$_['text_comment']       = 'Комментарий по возврату';
 $_['text_history']       = 'Return History';
 $_['text_no_results']    = 'You have not made any previous returns!';
 $_['text_agree']         = 'Я прочитал и согласен с <a href="%s" class="agree"><b>%s</b></a>';
@@ -27,13 +27,13 @@ $_['column_order_id']    = 'Номер заказа';
 $_['column_status']      = 'Статус';
 $_['column_date_added']  = 'Дата добавления';
 $_['column_customer']    = 'Покупатель';
-$_['column_product']     = 'Product Name';
+$_['column_product']     = 'Название товара';
 $_['column_model']       = 'Модель';
-$_['column_quantity']    = 'Quantity';
-$_['column_price']       = 'Price';
+$_['column_quantity']    = 'Количество';
+$_['column_price']       = 'Цена';
 $_['column_opened']      = 'Открыто';
 $_['column_comment']     = 'Комментарий';
-$_['column_reason']      = 'Reason';
+$_['column_reason']      = 'Причина';
 $_['column_action']      = 'Действие';
 
 // Entry
@@ -43,11 +43,11 @@ $_['entry_firstname']    = 'Имя';
 $_['entry_lastname']     = 'Фамилия';
 $_['entry_email']        = 'Эл. почта';
 $_['entry_telephone']    = 'Telephone';
-$_['entry_product']      = 'Product Name';
-$_['entry_model']        = 'Product Code';
-$_['entry_quantity']     = 'Quantity';
-$_['entry_reason']       = 'Reason for Return';
-$_['entry_opened']       = 'Product is opened';
+$_['entry_product']      = 'Название товара';
+$_['entry_model']        = 'Код товара';
+$_['entry_quantity']     = 'Количество';
+$_['entry_reason']       = 'Причина возврата';
+$_['entry_opened']       = 'Товар распакован';
 $_['entry_fault_detail'] = 'Неисправный или другие данные';
 
 // Error
@@ -58,7 +58,7 @@ $_['error_firstname']    = 'Имя должно содержать от 1 до 3
 $_['error_lastname']     = 'Фамилия должна быть от 1 до 32 символов!';
 $_['error_email']        = 'Адрес электронной почты недействителен!';
 $_['error_telephone']    = 'Telephone must be between 3 and 32 characters!';
-$_['error_product']      = 'Product Name must be greater than 3 and less than 255 characters!';
-$_['error_model']        = 'Product Model must be greater than 3 and less than 64 characters!';
+$_['error_product']      = 'Название товара должно содержать от 3 до 255 символов!';
+$_['error_model']        = 'Модель продукта должна быть больше 3 и меньше, чем 64 символов!';
 $_['error_reason']       = 'You must select a return product reason!';
 $_['error_agree']        = 'Warning: You must agree to the %s!';
