3caec7ce607bc6f79387486a227285b532b30ad0
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-31 22:16:51 +0300

    New translations reward.php (Russian)

--- upload/catalog/language/ru-ru/account/reward.php
+++ upload/catalog/language/ru-ru/account/reward.php
@@ -9,6 +9,6 @@ $_['column_points']      = 'Баллы';
 
 // Text
 $_['text_account']       = 'Аккаунт';
-$_['text_reward']        = 'Reward Points';
+$_['text_reward']        = 'Бонусные баллы';
 $_['text_total']         = 'Your total number of reward points is:';
 $_['text_no_results']    = 'You do not have any reward points!';
