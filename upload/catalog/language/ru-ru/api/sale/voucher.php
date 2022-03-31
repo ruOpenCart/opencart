32b5d73cb640266ccde244fd70541277a4fb4e14
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-03-31 22:17:43 +0300

    New translations voucher.php (Russian)

--- upload/catalog/language/ru-ru/api/sale/voucher.php
+++ upload/catalog/language/ru-ru/api/sale/voucher.php
@@ -7,7 +7,7 @@ $_['text_remove']     = 'Success: Your gift voucher discount has been removed!';
 
 // Error
 $_['error_voucher']   = 'Warning: Gift voucher is either invalid or the balance has been used up!';
-$_['error_to_name']   = 'Recipient\'s Name must be between 1 and 64 characters!';
+$_['error_to_name']   = 'Имя получателя должно содержать от 1 до 64 символов!';
 $_['error_from_name'] = 'Your Name must be between 1 and 64 characters!';
 $_['error_email']     = 'Адрес электронной почты недействителен!';
 $_['error_theme']     = 'You must select a theme!';
