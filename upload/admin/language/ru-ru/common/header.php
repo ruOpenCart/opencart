4915f763f22afdaa709741ca3a736ade325fb998
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:40:29 +0300

    New translations header.php (Russian)

--- upload/admin/language/ru-ru/common/header.php
+++ upload/admin/language/ru-ru/common/header.php
@@ -4,12 +4,12 @@ $_['heading_title']         = 'OpenCart';
 
 // Text
 $_['text_notification']      = 'Уведомления';
-$_['text_notification_all']  = 'Show All';
-$_['text_notification_none'] = 'There are no notifications';
-$_['text_profile']           = 'Your Profile';
-$_['text_store']             = 'Stores';
+$_['text_notification_all']  = 'Показать все';
+$_['text_notification_none'] = 'Нет никаких уведомлений';
+$_['text_profile']           = 'Ваш профиль';
+$_['text_store']             = 'Магазины';
 $_['text_help']              = 'Помощь';
 $_['text_homepage']          = 'Домашняя страница OpenCart';
-$_['text_support']           = 'Support Forum';
+$_['text_support']           = 'Форум поддержки';
 $_['text_documentation']     = 'Документация';
 $_['text_logout']            = 'Выйти';
