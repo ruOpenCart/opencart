afccbb72cb5aef718ba2ccecbc16ec33eea37218
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:24 +0300

    New translations api.php (Russian)

--- upload/admin/language/ru-ru/marketplace/api.php
+++ upload/admin/language/ru-ru/marketplace/api.php
@@ -7,10 +7,10 @@ $_['text_success']     = 'Success: You have modified your API information!';
 $_['text_signup']      = 'Пожалуйста, введите вашу информацию OpenCart API, которую вы можете получить <a href="https://www.opencart.com/index.php?route=account/store" target="_blank" class="alert-link">здесь</a>.';
 
 // Entry
-$_['entry_username']   = 'Username';
-$_['entry_secret']     = 'Secret';
+$_['entry_username']   = 'Имя пользователя';
+$_['entry_secret']     = 'Секрет';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify marketplace API!';
-$_['error_username']   = 'Username required!';
-$_['error_secret']     = 'Secret required!';
+$_['error_username']   = 'Имя пользователя обязательно!';
+$_['error_secret']     = 'Требуется секрет!';
