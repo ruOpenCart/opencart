9080657beefc7f75b906cf92d4acf1eed1a438a7
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:06 +0300

    New translations free_checkout.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/payment/free_checkout.php
+++ upload/extension/opencart/admin/language/ru-ru/payment/free_checkout.php
@@ -10,7 +10,7 @@ $_['text_edit']          = 'Редактирование бесплатного 
 // Entry
 $_['entry_order_status'] = 'Статус заказа';
 $_['entry_status']       = 'Статус';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Порядок сортировки';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify payment free checkout!';
