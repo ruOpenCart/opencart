abc9bc45a80f3162765f4ad225f99124b0476963
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:23 +0300

    New translations affiliate.php (Russian)

--- upload/catalog/language/ru-ru/mail/affiliate.php
+++ upload/catalog/language/ru-ru/mail/affiliate.php
@@ -1,20 +1,20 @@
 <?php
 // Text
 $_['text_subject']        = '%s - Партнерская программа';
-$_['text_welcome']        = 'Thank you for joining the %s Affiliate Program!';
+$_['text_welcome']        = 'Спасибо за присоединение к партнерской программе %s!';
 $_['text_login']          = 'Your account has now been created and you can log in by using your e-mail address and password by visiting our website or at the following URL:';
 $_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your e-mail address and password by visiting our website or at the following URL:';
-$_['text_service']        = 'Upon logging in, you will be able to generate tracking codes, track commission payments and edit your account information.';
-$_['text_thanks']         = 'Thanks,';
+$_['text_service']        = 'После входа в систему вы сможете генерировать коды отслеживания, отслеживать комиссионные платежи и редактировать информацию о вашей учетной записи.';
+$_['text_thanks']         = 'Спасибо,';
 $_['text_new_affiliate']  = 'Новый партнер';
 $_['text_signup']         = 'Зарегистрирован новый партнер:';
-$_['text_website']        = 'Web Site:';
+$_['text_website']        = 'Веб-сайт:';
 $_['text_customer_group'] = 'Группа покупателя:';
 $_['text_firstname']      = 'Имя:';
 $_['text_lastname']       = 'Фамилия:';
 $_['text_company']        = 'Компания:';
 $_['text_email']          = 'Эл. почта:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_telephone']      = 'Телефон:';
 
 // Button
 $_['button_login']        = 'Войти';
