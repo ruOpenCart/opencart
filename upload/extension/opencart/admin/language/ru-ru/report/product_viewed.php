7637f127d7264db805d6c7a7913541d7da45c52f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:43:10 +0300

    New translations product_viewed.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/product_viewed.php
+++ upload/extension/opencart/admin/language/ru-ru/report/product_viewed.php
@@ -10,12 +10,12 @@ $_['text_success']     = 'Success: You have reset the products viewed report!';
 // Column
 $_['column_name']      = 'Название товара';
 $_['column_model']     = 'Модель';
-$_['column_viewed']    = 'Viewed';
+$_['column_viewed']    = 'Просмотров';
 $_['column_percent']   = 'Процент';
 
 // Entry
 $_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Порядок сортировки';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify products viewed report!';
