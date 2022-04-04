9b5d5b823e0d64344ab98ef1490e085ebe484562
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:40:47 +0300

    New translations subscription_status.php (Russian)

--- upload/admin/language/ru-ru/localisation/subscription_status.php
+++ upload/admin/language/ru-ru/localisation/subscription_status.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']      = 'Subscription Statuses';
+$_['heading_title']      = 'Статусы подписки';
 
 // Text
 $_['text_success']       = 'Success: You have modified subscription statuses!';
-$_['text_list']          = 'Subscription Status List';
+$_['text_list']          = 'Список статусов подписки';
 $_['text_add']           = 'Добавить статус подписки';
 $_['text_edit']          = 'Редактирование статуса подписки';
 
 // Column
-$_['column_name']        = 'Subscription Status Name';
+$_['column_name']        = 'Название статуса подписки';
 $_['column_action']      = 'Действие';
 
 // Entry
-$_['entry_name']         = 'Subscription Status Name';
+$_['entry_name']         = 'Название статуса подписки';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify subscription statuses!';
-$_['error_name']         = 'Subscription Status Name must be between 3 and 32 characters!';
+$_['error_name']         = 'Название статуса подписки должно быть от 3 до 32 символов!';
 $_['error_default']      = 'Warning: This subscription status cannot be deleted as it is currently assigned as the default store subscription status!';
 $_['error_subscription'] = 'Warning: This subscription status cannot be deleted as it is currently assigned to %s subscriptions!';
