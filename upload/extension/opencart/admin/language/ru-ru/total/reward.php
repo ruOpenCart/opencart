f57f7a40009b642645eb99594d0e11dd0b53a84d
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:43:27 +0300

    New translations reward.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/total/reward.php
+++ upload/extension/opencart/admin/language/ru-ru/total/reward.php
@@ -9,7 +9,7 @@ $_['text_edit']        = 'Редактирование общего количе
 
 // Entry
 $_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Порядок сортировки';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify reward points total!';
