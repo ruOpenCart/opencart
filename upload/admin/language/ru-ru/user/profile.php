bbfff47223664ca77d8eaa55ac1d41e76d760fc6
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:43 +0300

    New translations profile.php (Russian)

--- upload/admin/language/ru-ru/user/profile.php
+++ upload/admin/language/ru-ru/user/profile.php
@@ -7,13 +7,13 @@ $_['text_success']          = 'Success: You have modified your profile!';
 $_['text_edit']             = 'Редактирование вашего профиля';
 
 // Column
-$_['column_username']       = 'Username';
+$_['column_username']       = 'Имя пользователя';
 $_['column_status']         = 'Статус';
 $_['column_date_added']     = 'Дата добавления';
 $_['column_action']         = 'Действие';
 
 // Entry
-$_['entry_username']        = 'Username';
+$_['entry_username']        = 'Имя пользователя';
 $_['entry_password']        = 'Пароль';
 $_['entry_confirm']         = 'Подтвердить';
 $_['entry_firstname']       = 'Имя';
@@ -24,7 +24,7 @@ $_['entry_image']           = 'Изображение';
 // Error
 $_['error_permission']      = 'Warning: You do not have permission to modify your profile!';
 $_['error_username_exists'] = 'Warning: Username is already in use!';
-$_['error_username']        = 'Username must be between 3 and 20 characters!';
+$_['error_username']        = 'Имя пользователя должно содержать от 3 до 20 символов!';
 $_['error_password']        = 'Пароль должен быть от 4 до 20 символов!';
 $_['error_confirm']         = 'Пароль и подтверждение пароля не совпадают!';
 $_['error_firstname']       = 'Имя должно содержать от 1 до 32 символов!';
