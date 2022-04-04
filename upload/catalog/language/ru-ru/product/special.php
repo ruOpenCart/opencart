a13a78b07ceb805157eb169c935525c088e62780
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:38 +0300

    New translations special.php (Russian)

--- upload/catalog/language/ru-ru/product/special.php
+++ upload/catalog/language/ru-ru/product/special.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']    = 'Special Offers';
+$_['heading_title']    = 'Специальные предложения';
 
 // Text
 $_['text_no_results']  = 'There are no special offer products to list.';
 $_['text_compare']     = 'Сравнение товаров (%s)';
-$_['text_sort']        = 'Sort By';
+$_['text_sort']        = 'Сортировать по';
 $_['text_default']     = 'По умолчанию';
 $_['text_name_asc']    = 'Название (А - Я)';
 $_['text_name_desc']   = 'Название (Я - А)';
@@ -15,4 +15,4 @@ $_['text_rating_asc']  = 'Рейтинг (по убыванию)';
 $_['text_rating_desc'] = 'Рейтинг (по возрастанию)';
 $_['text_model_asc']   = 'Модель (А - Я)';
 $_['text_model_desc']  = 'Модель (Я - А)';
-$_['text_limit']       = 'Show';
+$_['text_limit']       = 'Показать';
