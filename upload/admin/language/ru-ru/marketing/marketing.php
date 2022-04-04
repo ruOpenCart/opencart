f85ab02203fd17bc4b4acce2e8380377b65d482d
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:23 +0300

    New translations marketing.php (Russian)

--- upload/admin/language/ru-ru/marketing/marketing.php
+++ upload/admin/language/ru-ru/marketing/marketing.php
@@ -19,7 +19,7 @@ $_['column_clicks']     = 'Клики';
 $_['column_orders']     = 'Заказы';
 $_['column_ip']         = 'IP-адрес';
 $_['column_account']    = 'Учётные записи';
-$_['column_store']      = 'Store';
+$_['column_store']      = 'Магазин';
 $_['column_country']    = 'Страна';
 $_['column_date_added'] = 'Дата добавления';
 $_['column_action']     = 'Действие';
@@ -27,16 +27,16 @@ $_['column_action']     = 'Действие';
 // Entry
 $_['entry_name']        = 'Название кампании';
 $_['entry_description'] = 'Описание кампании';
-$_['entry_code']        = 'Tracking Code';
+$_['entry_code']        = 'Код отслеживания';
 $_['entry_example']     = 'Примеры';
 $_['entry_date_added']  = 'Дата добавления';
 
 // Help
 $_['help_code']         = 'The tracking code that will be used to track marketing campaigns.';
-$_['help_example']      = 'So the system can track referrals you need to add the tracking code to the end of the URL linking to your site.';
+$_['help_example']      = 'Чтобы система могла отслеживать рефералов, необходимо добавить код отслеживания в конец URL-адреса, ссылающегося на ваш сайт.';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify marketing tracking!';
 $_['error_name']        = 'Кампания должна содержать от 1 до 32 символов!';
-$_['error_code']        = 'Tracking Code required!';
-$_['error_exists']      = 'Tracking code is being used by another campaign!';
+$_['error_code']        = 'Требуется код отслеживания!';
+$_['error_exists']      = 'Код отслеживания уже используется другой кампанией!';
