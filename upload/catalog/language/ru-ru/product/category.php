1fefb357f78219821767e8d6b307d4f187590051
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:33 +0300

    New translations category.php (Russian)

--- upload/catalog/language/ru-ru/product/category.php
+++ upload/catalog/language/ru-ru/product/category.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_refine']      = 'Refine Search';
+$_['text_refine']      = 'Уточнить поиск';
 $_['text_product']     = 'Товары';
 $_['text_error']       = 'Категория не найдена!';
 $_['text_no_results']  = 'There are no products to list in this category.';
 $_['text_compare']     = 'Сравнение товаров (%s)';
-$_['text_sort']        = 'Sort By';
+$_['text_sort']        = 'Сортировать по';
 $_['text_default']     = 'По умолчанию';
 $_['text_name_asc']    = 'Название (А - Я)';
 $_['text_name_desc']   = 'Название (Я - А)';
@@ -15,4 +15,4 @@ $_['text_rating_asc']  = 'Рейтинг (по убыванию)';
 $_['text_rating_desc'] = 'Рейтинг (по возрастанию)';
 $_['text_model_asc']   = 'Модель (А - Я)';
 $_['text_model_desc']  = 'Модель (Я - А)';
-$_['text_limit']       = 'Show';
+$_['text_limit']       = 'Показать';
