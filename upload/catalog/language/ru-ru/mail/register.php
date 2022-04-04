8180ffe2ef13cbdcbe1eee45b63ef8f29156a354
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:30 +0300

    New translations register.php (Russian)

--- upload/catalog/language/ru-ru/mail/register.php
+++ upload/catalog/language/ru-ru/mail/register.php
@@ -1,18 +1,18 @@
 <?php
 // Text
 $_['text_subject']        = '%s - Благодарим за регистрацию';
-$_['text_welcome']        = 'Welcome and thank you for registering at %s!';
+$_['text_welcome']        = 'Добро пожаловать и спасибо за регистрацию в %s!';
 $_['text_login']          = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
 $_['text_approval']       = 'Your account must be approved before you can login. Once approved you can log in by using your email address and password by visiting our website or at the following URL:';
-$_['text_service']        = 'Upon logging in, you will be able to access other services including reviewing past orders, printing invoices and editing your account information.';
-$_['text_thanks']         = 'Thanks,';
+$_['text_service']        = 'После входа в систему вы сможете получить доступ к другим услугам, включая просмотр прошлых заказов, печать счетов-фактур и редактирование информации о вашей учетной записи.';
+$_['text_thanks']         = 'Спасибо,';
 $_['text_new_customer']   = 'Новый покупатель';
 $_['text_signup']         = 'Зарегистрирован новый клиент:';
 $_['text_customer_group'] = 'Группа покупателя:';
 $_['text_firstname']      = 'Имя:';
 $_['text_lastname']       = 'Фамилия:';
 $_['text_email']          = 'Эл. почта:';
-$_['text_telephone']      = 'Telephone:';
+$_['text_telephone']      = 'Телефон:';
 
 // Button
 $_['button_login']        = 'Войти';
