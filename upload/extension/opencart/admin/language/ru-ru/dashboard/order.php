2a3a958c0feef441de5753d269e13b9fad1097a7
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:51 +0300

    New translations order.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/dashboard/order.php
+++ upload/extension/opencart/admin/language/ru-ru/dashboard/order.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'Total Orders';
+$_['heading_title']    = 'Всего заказов';
 
 // Text
 $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified dashboard orders!';
 $_['text_edit']        = 'Редактирование заказов на панели';
-$_['text_view']        = 'View more...';
+$_['text_view']        = 'Смотреть подробнее...';
 
 // Entry
 $_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Порядок сортировки';
+$_['entry_width']      = 'Ширина';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify dashboard orders!';
