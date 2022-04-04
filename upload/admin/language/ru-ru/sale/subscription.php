99bc712876939117510184f336c02a4ab2c40a8e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:32 +0300

    New translations subscription.php (Russian)

--- upload/admin/language/ru-ru/sale/subscription.php
+++ upload/admin/language/ru-ru/sale/subscription.php
@@ -1,13 +1,13 @@
 <?php
 // Heading
-$_['heading_title']              = 'Subscriptions';
+$_['heading_title']              = 'Подписки';
 
 // Text
 $_['text_success']               = 'Success: You have modified subscriptions!';
-$_['text_list']                  = 'Subscription List';
+$_['text_list']                  = 'Список подписок';
 $_['text_filter']                = 'Фильтр';
-$_['text_subscription']          = 'Subscription Details';
-$_['text_subscription_id']       = 'Subscription ID';
+$_['text_subscription']          = 'Детали подписки';
+$_['text_subscription_id']       = 'Идентификатор подписки';
 $_['text_order_id']              = 'Номер заказа';
 $_['text_customer']              = 'Покупатель';
 $_['text_date_added']            = 'Дата добавления';
@@ -17,7 +17,7 @@ $_['text_order_status']          = 'Статус заказа';
 
 $_['text_payment_method']        = 'Метод оплаты';
 
-$_['text_cancel']                = 'Until Canceled';
+$_['text_cancel']                = 'До отмены';
 
 $_['text_product']               = 'Детали товара';
 $_['text_quantity']              = 'Количество';
@@ -25,13 +25,13 @@ $_['text_amount']                = 'Сумма';
 
 $_['text_history']               = 'История';
 $_['text_history_add']           = 'Добавить историю';
-$_['text_transaction']           = 'Transactions';
+$_['text_transaction']           = 'Транзакции';
 $_['text_transaction_add']       = 'Добавить транзакцию';
 
 // Column
-$_['column_subscription_id']     = 'Subscription ID';
+$_['column_subscription_id']     = 'Идентификатор подписки';
 $_['column_order_id']            = 'Номер заказа';
-$_['column_reference']           = 'Reference';
+$_['column_reference']           = 'Ссылка';
 $_['column_customer']            = 'Покупатель';
 $_['column_comment']             = 'Комментарий';
 $_['column_description']         = 'Описание';
@@ -45,11 +45,11 @@ $_['column_action']              = 'Действие';
 
 // Entry
 $_['entry_order_id']             = 'Номер заказа';
-$_['entry_subscription_id']      = 'Subscription ID';
+$_['entry_subscription_id']      = 'Идентификатор подписки';
 $_['entry_customer']             = 'Покупатель';
-$_['entry_subscription_plan']    = 'Subscription Plan';
+$_['entry_subscription_plan']    = 'План подписки';
 $_['entry_payment_method']       = 'Метод оплаты';
-$_['entry_subscription_status']  = 'Subscription Status';
+$_['entry_subscription_status']  = 'Статус подписки';
 $_['entry_comment']              = 'Комментарий';
 $_['entry_description']          = 'Описание';
 $_['entry_amount']               = 'Сумма';
