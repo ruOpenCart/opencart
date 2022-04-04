8e8e303bf257bd5e1d54c42c79af5c88c3fb1ebe
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:43:32 +0300

    New translations tax.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/total/tax.php
+++ upload/extension/opencart/admin/language/ru-ru/total/tax.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']    = 'Taxes';
+$_['heading_title']    = 'Налоги';
 
 // Text
 $_['text_extension']   = 'Расширения';
@@ -9,7 +9,7 @@ $_['text_edit']        = 'Редактирование общей суммы н�
 
 // Entry
 $_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Порядок сортировки';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify taxes total!';
