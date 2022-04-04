9ec94464ea255e05a88c95ca0807fcfa543d9b2b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:40:46 +0300

    New translations tax_class.php (Russian)

--- upload/admin/language/ru-ru/localisation/tax_class.php
+++ upload/admin/language/ru-ru/localisation/tax_class.php
@@ -1,32 +1,32 @@
 <?php
 // Heading
-$_['heading_title']     = 'Tax Classes';
+$_['heading_title']     = 'Налоговые классы';
 
 // Text
 $_['text_success']      = 'Success: You have modified tax classes!';
-$_['text_list']         = 'Tax Class List';
+$_['text_list']         = 'Список налоговых классов';
 $_['text_add']          = 'Добавить налоговый класс';
 $_['text_edit']         = 'Редактирование налогового класса';
-$_['text_tax_class']    = 'Tax Class';
-$_['text_tax_rate']     = 'Tax Rates';
-$_['text_shipping']     = 'Shipping Address';
+$_['text_tax_class']    = 'Класс налога';
+$_['text_tax_rate']     = 'Налоговые ставки';
+$_['text_shipping']     = 'Адрес доставки';
 $_['text_payment']      = 'Платёжный адрес';
-$_['text_store']        = 'Store Address';
+$_['text_store']        = 'Адрес магазина';
 
 // Column
-$_['column_title']      = 'Tax Class Title';
+$_['column_title']      = 'Заголовок налогового класса';
 $_['column_action']     = 'Действие';
 
 // Entry
-$_['entry_title']       = 'Tax Class Title';
+$_['entry_title']       = 'Заголовок налогового класса';
 $_['entry_description'] = 'Описание';
-$_['entry_rate']        = 'Tax Rate';
+$_['entry_rate']        = 'Налоговая ставка';
 $_['entry_based']       = 'На основе';
 $_['entry_geo_zone']    = 'Гео-зона';
 $_['entry_priority']    = 'Приоритет';
 
 // Error
 $_['error_permission']  = 'Warning: You do not have permission to modify tax classes!';
-$_['error_title']       = 'Tax Class Title must be between 3 and 32 characters!';
+$_['error_title']       = 'Заголовок налогового класса должен быть от 3 до 32 символов!';
 $_['error_description'] = 'Описание должно быть от 3 до 255 символов!';
 $_['error_product']     = 'Warning: This tax class cannot be deleted as it is currently assigned to %s products!';
