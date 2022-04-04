fc61f3ee4b8b67ac19426fb9c3fc3e86cb25d642
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:17 +0300

    New translations cookie.php (Russian)

--- upload/catalog/language/ru-ru/common/cookie.php
+++ upload/catalog/language/ru-ru/common/cookie.php
@@ -1,8 +1,8 @@
 <?php
 // Text
-$_['text_success'] = 'Thank you for letting us know your choice!';
+$_['text_success'] = 'Спасибо, что сообщили нам о вашем выборе!';
 $_['text_cookie']  = 'This website uses cookies. For more information <a href="%s" class="alert-link modal-link">click here</a>.';
 
 // Button
-$_['button_agree']    = 'Yes, that\'s fine!';
+$_['button_agree']    = 'Да, это прекрасно!';
 $_['button_disagree'] = 'Нет, спасибо!';
