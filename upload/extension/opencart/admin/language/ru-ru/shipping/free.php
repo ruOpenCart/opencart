f435992d7834f62a66fe2e31ce94816996701c3d
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:43:17 +0300

    New translations free.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/shipping/free.php
+++ upload/extension/opencart/admin/language/ru-ru/shipping/free.php
@@ -8,13 +8,13 @@ $_['text_success']     = 'Success: You have modified free shipping!';
 $_['text_edit']        = 'Редактирование бесплатной доставки';
 
 // Entry
-$_['entry_total']      = 'Total';
+$_['entry_total']      = 'Всего';
 $_['entry_geo_zone']   = 'Гео-зона';
 $_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Порядок сортировки';
 
 // Help
-$_['help_total']       = 'Sub-Total amount needed before the free shipping module becomes available.';
+$_['help_total']       = 'Минимальная сумма, необходимая для того, чтобы бесплатная доставка стала доступна.';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify free shipping!';
