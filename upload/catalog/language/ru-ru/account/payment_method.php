d27f404f9e5ebb8cebd637f231ca894183334b83
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:01 +0300

    New translations payment_method.php (Russian)

--- upload/catalog/language/ru-ru/account/payment_method.php
+++ upload/catalog/language/ru-ru/account/payment_method.php
@@ -5,12 +5,12 @@ $_['heading_title']         = 'Методы оплаты';
 // Text
 $_['text_account']          = 'Аккаунт';
 $_['text_payment_method']   = 'Записи метода оплаты';
-$_['text_success']          = 'Your payment method has been successfully deleted';
+$_['text_success']          = 'Ваш способ оплаты был успешно удален';
 $_['text_no_results']       = 'You have no payment methods in your account.';
 
 // Column
 $_['column_payment_method'] = 'Метод оплаты';
-$_['column_type']           = 'Type';
+$_['column_type']           = 'Тип';
 $_['column_date_expire']    = 'Дата истечения';
 $_['column_action']         = 'Действие';
 
