cb7de4bf5ebe299c6e47113ddf96a2376566a70a
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:12 +0300

    New translations customer.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/dashboard/customer.php
+++ upload/extension/opencart/admin/language/ru-ru/dashboard/customer.php
@@ -1,17 +1,17 @@
 <?php
 // Heading
-$_['heading_title']    = 'Total Customers';
+$_['heading_title']    = 'Всего покупателей';
 
 // Text
 $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified dashboard customer!';
 $_['text_edit']        = 'Редактирование покупателя для панели';
-$_['text_view']        = 'View more...';
+$_['text_view']        = 'Смотреть подробнее...';
 
 // Entry
 $_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Sort Order';
-$_['entry_width']      = 'Width';
+$_['entry_sort_order'] = 'Порядок сортировки';
+$_['entry_width']      = 'Ширина';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify dashboard customer!';
