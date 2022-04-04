950d672a231f3bdbe8b530bace34481fa7f1567e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:43:12 +0300

    New translations sale_order.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/sale_order.php
+++ upload/extension/opencart/admin/language/ru-ru/report/sale_order.php
@@ -7,9 +7,9 @@ $_['text_extension']     = 'Расширения';
 $_['text_edit']          = 'Редактирование отчета о продажах';
 $_['text_success']       = 'Success: You have modified sales report!';
 $_['text_filter']        = 'Фильтр';
-$_['text_year']          = 'Years';
+$_['text_year']          = 'Годы';
 $_['text_month']         = 'Месяцы';
-$_['text_week']          = 'Weeks';
+$_['text_week']          = 'Недели';
 $_['text_day']           = 'Дней';
 $_['text_all_status']    = 'Все статусы';
 
@@ -18,8 +18,8 @@ $_['column_date_start']  = 'Дата начала';
 $_['column_date_end']    = 'Дата окончания';
 $_['column_orders']      = 'Кол-во заказов';
 $_['column_products']    = 'Кол-во товаров';
-$_['column_tax']         = 'Tax';
-$_['column_total']       = 'Total';
+$_['column_tax']         = 'Налог';
+$_['column_total']       = 'Всего';
 
 // Entry
 $_['entry_date_start']   = 'Дата начала';
@@ -27,7 +27,7 @@ $_['entry_date_end']     = 'Дата окончания';
 $_['entry_group']        = 'Группировать по';
 $_['entry_order_status'] = 'Статус заказа';
 $_['entry_status']       = 'Статус';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Порядок сортировки';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify sales report!';
