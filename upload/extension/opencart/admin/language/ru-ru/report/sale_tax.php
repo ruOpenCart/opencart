5c46894d747c9eeae99e8b6b1ec6aef53eb38287
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:43:15 +0300

    New translations sale_tax.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/sale_tax.php
+++ upload/extension/opencart/admin/language/ru-ru/report/sale_tax.php
@@ -1,24 +1,24 @@
 <?php
 // Heading
-$_['heading_title']      = 'Tax Report';
+$_['heading_title']      = 'Отчет по налогам';
 
 // Text
 $_['text_extension']     = 'Расширения';
 $_['text_edit']          = 'Редактирование отчета по налогам';
 $_['text_success']       = 'Success: You have modified tax report!';
 $_['text_filter']        = 'Фильтр';
-$_['text_year']          = 'Years';
+$_['text_year']          = 'Годы';
 $_['text_month']         = 'Месяцы';
-$_['text_week']          = 'Weeks';
+$_['text_week']          = 'Недели';
 $_['text_day']           = 'Дней';
 $_['text_all_status']    = 'Все статусы';
 
 // Column
 $_['column_date_start']  = 'Дата начала';
 $_['column_date_end']    = 'Дата окончания';
-$_['column_title']       = 'Tax Title';
+$_['column_title']       = 'Название налога';
 $_['column_orders']      = 'Кол-во заказов';
-$_['column_total']       = 'Total';
+$_['column_total']       = 'Всего';
 
 // Entry
 $_['entry_date_start']   = 'Дата начала';
@@ -26,7 +26,7 @@ $_['entry_date_end']     = 'Дата окончания';
 $_['entry_group']        = 'Группировать по';
 $_['entry_order_status'] = 'Статус заказа';
 $_['entry_status']       = 'Статус';
-$_['entry_sort_order']   = 'Sort Order';
+$_['entry_sort_order']   = 'Порядок сортировки';
 
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify tax report!';
