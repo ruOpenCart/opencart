9e81b1955872e859ffe239cf918544e4100a6251
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:13 +0300

    New translations cart.php (Russian)

--- upload/catalog/language/ru-ru/checkout/cart.php
+++ upload/catalog/language/ru-ru/checkout/cart.php
@@ -1,33 +1,33 @@
 <?php
 // Heading
-$_['heading_title']              = 'Shopping Cart';
+$_['heading_title']              = 'Корзина';
 
 // Text
 $_['text_success']               = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">shopping cart</a>!';
 $_['text_edit']                  = 'Success: You have modified your shopping cart!';
 $_['text_remove']                = 'Success: You have removed an item from your shopping cart!';
 $_['text_login']                 = 'Внимание: Вы должны <a href="%s">войти</a> или <a href="%s">создать учетную запись</a> для просмотра цен!';
-$_['text_no_results']            = 'Your shopping cart is empty!';
-$_['text_next']                  = 'What would you like to do next?';
+$_['text_no_results']            = 'Ваша корзина пуста!';
+$_['text_next']                  = 'Что бы вы хотели сделать дальше?';
 $_['text_next_choice']           = 'Выберите, если у вас есть код скидки или бонусные баллы, которые вы хотите использовать, или хотите оценить стоимость доставки.';
 $_['text_points']                = 'Бонусные баллы';
-$_['text_subscription']          = 'Subscription';
+$_['text_subscription']          = 'Подписка';
 $_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
 $_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
 $_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
 $_['text_day']                   = 'день';
-$_['text_week']                  = 'week';
+$_['text_week']                  = 'неделя';
 $_['text_semi_month']            = 'полмесяца';
 $_['text_month']                 = 'месяц';
-$_['text_year']                  = 'year';
+$_['text_year']                  = 'год';
 
 // Column
 $_['column_image']               = 'Изображение';
 $_['column_name']                = 'Название товара';
 $_['column_model']               = 'Модель';
 $_['column_quantity']            = 'Количество';
-$_['column_price']               = 'Unit Price';
-$_['column_total']               = 'Total';
+$_['column_price']               = 'Цена за единицу';
+$_['column_total']               = 'Всего';
 
 // Error
 $_['error_stock']                = 'Товары отмеченные *** отсутствуют в нужном количестве или их нет на складе!';
