6e2ad79140668ed1b121c94df4e84dfe8eedd0cb
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:58 +0300

    New translations special.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/special.php
+++ upload/extension/opencart/admin/language/ru-ru/module/special.php
@@ -1,13 +1,13 @@
 <?php
 // Heading
-$_['heading_title']    = 'Specials';
+$_['heading_title']    = 'Скидки';
 
 // Text
 $_['text_extension']   = 'Расширения';
 $_['text_success']     = 'Success: You have modified module specials!';
 $_['text_edit']        = 'Редактирование модуля специальных предложений';
 $_['text_horizontal']  = 'Горизонтально';
-$_['text_vertical']    = 'Vertical';
+$_['text_vertical']    = 'Вертикально';
 
 // Entry
 $_['entry_name']       = 'Название модуля';
@@ -20,5 +20,5 @@ $_['entry_status']     = 'Статус';
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify specials module!';
 $_['error_name']       = 'Название модуля должно быть от 3 до 64 символов!';
-$_['error_width']      = 'Width required!';
+$_['error_width']      = 'Необходимо указать ширину!';
 $_['error_height']     = 'Необходимо указать высоту!';
