fa6ff5840645eb58c8c7b45ddd4ac4cf6195fd2b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:40:35 +0300

    New translations gdpr_approve.php (Russian)

--- upload/admin/language/ru-ru/mail/gdpr_approve.php
+++ upload/admin/language/ru-ru/mail/gdpr_approve.php
@@ -3,9 +3,9 @@
 $_['text_subject'] = '%s - GDPR запрос одобрен!';
 $_['text_request'] = 'Запрос на удаление учетной записи';
 $_['text_hello']   = 'Привет <strong>%s</strong>,';
-$_['text_user']    = 'User';
+$_['text_user']    = 'Пользователь';
 $_['text_gdpr']    = 'Your GDPR data deletion request has been approved and will be deleted in <strong>%s Days</strong>.';
 $_['text_q']       = 'В. Почему мы не удаляем ваши данные прямо сейчас?';
 $_['text_a']       = 'Запросы на удаление учетной записи будут обрабатываться через <strong>%s дней</strong>, так что любые возвраты денег или обнаружение мошенничества могут быть обработаны.';
 $_['text_delete']  = 'You will receive a email informing you when your account has been deleted.';
-$_['text_thanks']  = 'Thanks,';
+$_['text_thanks']  = 'Спасибо,';
