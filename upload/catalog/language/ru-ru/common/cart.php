3ca4245f1ba12befce0813b32032370d6d174151
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:16 +0300

    New translations cart.php (Russian)

--- upload/catalog/language/ru-ru/common/cart.php
+++ upload/catalog/language/ru-ru/common/cart.php
@@ -1,15 +1,15 @@
 <?php
 // Text
 $_['text_items']                 = '%s товар(ов) - %s';
-$_['text_subscription']          = 'Subscription';
+$_['text_subscription']          = 'Подписка';
 $_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
 $_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
 $_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
 $_['text_day']                   = 'день';
-$_['text_week']                  = 'week';
+$_['text_week']                  = 'неделя';
 $_['text_semi_month']            = 'полмесяца';
 $_['text_month']                 = 'месяц';
-$_['text_year']                  = 'year';
-$_['text_no_results']            = 'Your shopping cart is empty!';
-$_['text_cart']                  = 'View Cart';
+$_['text_year']                  = 'год';
+$_['text_no_results']            = 'Ваша корзина пуста!';
+$_['text_cart']                  = 'Посмотреть корзину';
 $_['text_checkout']              = 'Оформить заказ';
