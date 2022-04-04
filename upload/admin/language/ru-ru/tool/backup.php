953612ac4e47e1e47ec3782a08d2a80969f9c1c4
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:39 +0300

    New translations backup.php (Russian)

--- upload/admin/language/ru-ru/tool/backup.php
+++ upload/admin/language/ru-ru/tool/backup.php
@@ -14,7 +14,7 @@ $_['text_import']        = 'Для больших резервных копий 
 
 // Column
 $_['column_filename']    = 'Имя файла';
-$_['column_size']        = 'Size';
+$_['column_size']        = 'Размер';
 $_['column_date_added']  = 'Дата добавления';
 $_['column_action']      = 'Действие';
 
@@ -25,7 +25,7 @@ $_['entry_export']       = 'Экспорт';
 // Error
 $_['error_permission']   = 'Warning: You do not have permission to modify Backup &amp; Restore!';
 $_['error_export']       = 'Warning: You must select at least one table to export!';
-$_['error_table']        = 'Table %s is not in the allowed list!';
+$_['error_table']        = 'Таблица %s не находится в списке разрешенных!';
 $_['error_file']         = 'Файл не найден!';
 $_['error_directory']    = 'Каталог не найден!';
 $_['error_not_found']    = 'Ошибка: Не удалось найти файл %s!';
