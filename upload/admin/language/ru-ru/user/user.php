a98a1c20c6eda534bdd9b051bf3faa40f3e63eec
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:44 +0300

    New translations user.php (Russian)

--- upload/admin/language/ru-ru/user/user.php
+++ upload/admin/language/ru-ru/user/user.php
@@ -1,22 +1,22 @@
 <?php
 // Heading
-$_['heading_title']         = 'Users';
+$_['heading_title']         = 'Пользователи';
 
 // Text
 $_['text_success']          = 'Success: You have modified users!';
-$_['text_list']             = 'User List';
+$_['text_list']             = 'Список пользователей';
 $_['text_add']              = 'Добавить пользователя';
 $_['text_edit']             = 'Редактирование пользователя';
 
 // Column
-$_['column_username']       = 'Username';
+$_['column_username']       = 'Имя пользователя';
 $_['column_status']         = 'Статус';
 $_['column_date_added']     = 'Дата добавления';
 $_['column_action']         = 'Действие';
 
 // Entry
-$_['entry_username']        = 'Username';
-$_['entry_user_group']      = 'User Group';
+$_['entry_username']        = 'Имя пользователя';
+$_['entry_user_group']      = 'Группа пользователя';
 $_['entry_password']        = 'Пароль';
 $_['entry_confirm']         = 'Подтвердить';
 $_['entry_firstname']       = 'Имя';
@@ -28,7 +28,7 @@ $_['entry_status']          = 'Статус';
 // Error
 $_['error_permission']      = 'Warning: You do not have permission to modify users!';
 $_['error_account']         = 'Warning: You can not delete your own account!';
-$_['error_username']        = 'Username must be between 3 and 20 characters!';
+$_['error_username']        = 'Имя пользователя должно содержать от 3 до 20 символов!';
 $_['error_username_exists'] = 'Warning: Username is already in use!';
 $_['error_password']        = 'Пароль должен быть от 4 до 20 символов!';
 $_['error_confirm']         = 'Пароль и подтверждение пароля не совпадают!';
