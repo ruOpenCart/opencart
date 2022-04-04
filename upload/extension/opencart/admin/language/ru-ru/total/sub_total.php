da18096f358472154cd1d9e44ded050c4e3d76da
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:43:30 +0300

    New translations sub_total.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/total/sub_total.php
+++ upload/extension/opencart/admin/language/ru-ru/total/sub_total.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']    = 'Sub-Total';
+$_['heading_title']    = 'Подитог';
 
 // Text
 $_['text_extension']   = 'Расширения';
@@ -9,7 +9,7 @@ $_['text_edit']        = 'Редактирование общей суммы п�
 
 // Entry
 $_['entry_status']     = 'Статус';
-$_['entry_sort_order'] = 'Sort Order';
+$_['entry_sort_order'] = 'Порядок сортировки';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify sub-total total!';
