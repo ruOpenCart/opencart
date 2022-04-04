43576194b6d5f1162012f75434aef1ed9ff3656e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:41:40 +0300

    New translations upgrade.php (Russian)

--- upload/admin/language/ru-ru/tool/upgrade.php
+++ upload/admin/language/ru-ru/tool/upgrade.php
@@ -1,30 +1,30 @@
 <?php
 // Heading
-$_['heading_title']        = 'Upgrade';
+$_['heading_title']        = 'Обновить';
 
 // Text
 $_['text_success']         = 'Success: You are using the latest version %s!';
 $_['text_upgrade']         = 'Проверить последнюю версию';
-$_['text_information']     = 'Version Information';
+$_['text_information']     = 'Информация о версии';
 $_['text_current_version'] = 'Текущая версия';
 $_['text_latest_version']  = 'Последняя версия';
 $_['text_date_added']      = 'Дата выпуска';
 $_['text_change']          = 'История изменений';
-$_['text_status']          = 'Upgrade Status';
+$_['text_status']          = 'Статус обновления';
 $_['text_ready']           = 'Нажмите кнопку обновить для обновления до последней версии...';
 $_['text_download']        = 'Загружается последняя...';
 $_['text_install']         = 'Копирование файлов...';
 $_['text_patch']           = 'Применение исправлений...';
 
 // Button
-$_['button_upgrade']       = 'Upgrade';
+$_['button_upgrade']       = 'Обновить';
 
 // Error
 $_['error_permission']     = 'Warning: You do not have permission to modify upgrades!';
 $_['error_connection']     = 'Не удалось подключиться к серверу обновлений!';
-$_['error_version']        = 'Version is lower than the current version!';
-$_['error_download']       = 'Upgrade could not be downloaded!';
-$_['error_file']           = 'Upgrade file could not be found!';
+$_['error_version']        = 'Версия ниже текущей версии!';
+$_['error_download']       = 'Обновление не может быть загружено!';
+$_['error_file']           = 'Файл обновления не найден!';
 $_['error_directory']      = 'Не удалось создать директорию %s!';
 $_['error_copy']           = 'Не удалось скопировать файл %s в %s!';
-$_['error_unzip']          = 'Zip file could not be opened!';
+$_['error_unzip']          = 'Zip-файл не может быть открыт!';
