b6a1baf662b0603188ffcff7b69792cc9aadc398
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:39:59 +0300

    New translations translation.php (Russian)

--- upload/admin/language/ru-ru/design/translation.php
+++ upload/admin/language/ru-ru/design/translation.php
@@ -4,28 +4,28 @@ $_['heading_title']    = 'Редактор языка';
 
 // Text
 $_['text_success']     = 'Success: You have modified language editor!';
-$_['text_list']        = 'Translation List';
+$_['text_list']        = 'Список переводов';
 $_['text_add']         = 'Добавить перевод';
 $_['text_edit']        = 'Редактирование перевода';
 $_['text_default']     = 'По умолчанию';
-$_['text_store']       = 'Store';
+$_['text_store']       = 'Магазин';
 $_['text_language']    = 'Язык';
 
 // Column
-$_['column_store']     = 'Store';
+$_['column_store']     = 'Магазин';
 $_['column_language']  = 'Язык';
 $_['column_route']     = 'Маршрут';
 $_['column_key']       = 'Ключ';
-$_['column_value']     = 'Value';
+$_['column_value']     = 'Значение';
 $_['column_action']    = 'Действие';
 
 // Entry
-$_['entry_store']      = 'Store';
+$_['entry_store']      = 'Магазин';
 $_['entry_language']   = 'Язык';
 $_['entry_route']      = 'Маршрут';
 $_['entry_key']        = 'Ключ';
 $_['entry_default']    = 'По умолчанию';
-$_['entry_value']      = 'Value';
+$_['entry_value']      = 'Значение';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify language editor!';
