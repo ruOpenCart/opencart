9f8aa5effdc53507f89911caa08758d8101cb134
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:40:10 +0300

    New translations return_reason.php (Russian)

--- upload/admin/language/ru-ru/localisation/return_reason.php
+++ upload/admin/language/ru-ru/localisation/return_reason.php
@@ -1,21 +1,21 @@
 <?php
 // Heading
-$_['heading_title']    = 'Return Reasons';
+$_['heading_title']    = 'Причины возврата';
 
 // Text
 $_['text_success']     = 'Success: You have modified return reasons!';
-$_['text_list']        = 'Return Reason List';
+$_['text_list']        = 'Список причин возврата';
 $_['text_add']         = 'Добавить причину возврата';
 $_['text_edit']        = 'Редактирование причины возврата';
 
 // Column
-$_['column_name']      = 'Return Reason Name';
+$_['column_name']      = 'Название причины возврата';
 $_['column_action']    = 'Действие';
 
 // Entry
-$_['entry_name']       = 'Return Reason Name';
+$_['entry_name']       = 'Название причины возврата';
 
 // Error
 $_['error_permission'] = 'Warning: You do not have permission to modify return reasons!';
-$_['error_name']       = 'Return Reason Name must be between 3 and 128 characters!';
+$_['error_name']       = 'Название причины возврата должно быть от 3 до 128 символов!';
 $_['error_return']     = 'Warning: This return reason cannot be deleted as it is currently assigned to %s returned products!';
