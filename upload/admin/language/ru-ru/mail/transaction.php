0e8425d1d7451defdca810ed48e8b085d2bfd015
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-04 18:42:58 +0300

    New translations transaction.php (Russian)

--- upload/admin/language/ru-ru/mail/transaction.php
+++ upload/admin/language/ru-ru/mail/transaction.php
@@ -2,5 +2,5 @@
 // Text
 $_['text_subject']  = '%s - Партнерское вознаграждение.';
 $_['text_received'] = 'You have received %s credit!';
-$_['text_total']    = 'Your total amount of credit is now %s.';
+$_['text_total']    = 'Ваша общая сумма в настоящее время %s.';
 $_['text_credit']   = 'Your account credit can deducted from your next purchase.';
