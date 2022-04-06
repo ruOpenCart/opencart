7232d6eef9db85f8086b8533489665842fb1a7dd
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:50:12 +0300

    New translations feed.php (Russian)

--- upload/admin/language/ru-ru/extension/feed.php
+++ upload/admin/language/ru-ru/extension/feed.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Каналы';
 
 // Text
-$_['text_success']     = 'Success: You have modified feeds!';
+$_['text_success']     = 'Успех: Вы изменили каналы!';
 $_['text_list']        = 'Список каналов';
 
 // Column
