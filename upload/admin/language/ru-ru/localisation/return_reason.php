d1dd3bf834a55c9d750dedbd9b596e2495135b72
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:50:05 +0300

    New translations return_reason.php (Russian)

--- upload/admin/language/ru-ru/localisation/return_reason.php
+++ upload/admin/language/ru-ru/localisation/return_reason.php
@@ -3,7 +3,7 @@
 $_['heading_title']    = 'Причины возврата';
 
 // Text
-$_['text_success']     = 'Success: You have modified return reasons!';
+$_['text_success']     = 'Успех: Вы изменили причины возврата!';
 $_['text_list']        = 'Список причин возврата';
 $_['text_add']         = 'Добавить причину возврата';
 $_['text_edit']        = 'Редактирование причины возврата';
