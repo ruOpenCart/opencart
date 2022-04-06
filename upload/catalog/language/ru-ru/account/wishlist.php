bfc35f1a4e71c8dbcb7ca80fe4cea3bdf50a3136
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:50:52 +0300

    New translations wishlist.php (Russian)

--- upload/catalog/language/ru-ru/account/wishlist.php
+++ upload/catalog/language/ru-ru/account/wishlist.php
@@ -7,8 +7,8 @@ $_['text_account']    = 'Аккаунт';
 $_['text_instock']    = 'В наличии';
 $_['text_wishlist']   = 'Список желаний (%s)';
 $_['text_login']      = 'You must <a href="%s">login</a> or <a href="%s">create an account</a> to save <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
-$_['text_success']    = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">wish list</a>!';
-$_['text_remove']     = 'Success: You have removed an item from your wishlist';
+$_['text_success']    = 'Успех: Вы добавили <a href="%s">%s</a> в ваш <a href="%s">список пожеланий</a>!';
+$_['text_remove']     = 'Успех: Вы удалили товар из списка желаний!';
 $_['text_no_results'] = 'Ваш список желаний пуст.';
 
 // Column
