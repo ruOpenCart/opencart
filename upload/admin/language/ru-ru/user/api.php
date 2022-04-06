88819b6b697bd71da93213278f13ae809d272bc5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:51:05 +0300

    New translations api.php (Russian)

--- upload/admin/language/ru-ru/user/api.php
+++ upload/admin/language/ru-ru/user/api.php
@@ -3,7 +3,7 @@
 $_['heading_title']        = 'API';
 
 // Text
-$_['text_success']         = 'Success: You have modified APIs!';
+$_['text_success']         = 'Успех: Вы изменили API!';
 $_['text_list']            = 'Список API';
 $_['text_add']             = 'Добавить API';
 $_['text_edit']            = 'Редактирование API';
