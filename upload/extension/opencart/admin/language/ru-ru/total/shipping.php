3fc1283784a5003f936dedabf069fa3b156d402f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:53:04 +0300

    New translations shipping.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/total/shipping.php
+++ upload/extension/opencart/admin/language/ru-ru/total/shipping.php
@@ -4,7 +4,7 @@ $_['heading_title']    = 'Доставка';
 
 // Text
 $_['text_extension']   = 'Расширения';
-$_['text_success']     = 'Success: You have modified shipping total!';
+$_['text_success']     = 'Успех: Вы изменили общую сумму доставки!';
 $_['text_edit']        = 'Редактирование общей суммы доставки';
 
 // Entry
