2089563bf3f675814c3876fd35e68a681c62ea2b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:50:41 +0300

    New translations returns.php (Russian)

--- upload/admin/language/ru-ru/mail/returns.php
+++ upload/admin/language/ru-ru/mail/returns.php
@@ -4,5 +4,5 @@ $_['text_subject']       = '%s - Возврат обновлен %s';
 $_['text_return_id']     = 'Идентификатор возврата:';
 $_['text_date_added']    = 'Дата возврата:';
 $_['text_return_status'] = 'Your return has been updated to the following status:';
-$_['text_comment']       = 'The comments for your return are:';
+$_['text_comment']       = 'Комментарии к вашему возврату:';
 $_['text_footer']        = 'Пожалуйста, ответьте на это письмо, если у вас есть какие-либо вопросы.';
