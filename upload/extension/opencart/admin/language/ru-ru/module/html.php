258a3a89796e8e5e3da9a41b83327cfb1b40d304
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:51:35 +0300

    New translations html.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/html.php
+++ upload/extension/opencart/admin/language/ru-ru/module/html.php
@@ -4,7 +4,7 @@ $_['heading_title']     = 'HTML-контент';
 
 // Text
 $_['text_extension']    = 'Расширения';
-$_['text_success']      = 'Success: You have modified HTML Content module!';
+$_['text_success']      = 'Успех: Вы изменили модуль HTML-контента!';
 $_['text_edit']         = 'Редактирование модуля HTML-содержимого';
 
 // Entry
