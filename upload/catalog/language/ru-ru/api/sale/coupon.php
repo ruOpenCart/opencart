9bdc175bbe48ae1e06435e1cf50419e7a0d98f68
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:52:41 +0300

    New translations coupon.php (Russian)

--- upload/catalog/language/ru-ru/api/sale/coupon.php
+++ upload/catalog/language/ru-ru/api/sale/coupon.php
@@ -1,7 +1,7 @@
 <?php
 // Text
-$_['text_success'] = 'Success: Your coupon discount has been applied!';
-$_['text_remove']  = 'Success: Your coupon discount has been removed!';
+$_['text_success'] = 'Успех: Ваш купон на скидку был применен!';
+$_['text_remove']  = 'Успех: Ваш купон на скидку был удален!';
 
 // Error
-$_['error_coupon'] = 'Warning: Coupon is either invalid, expired or reached it\'s usage limit!';
+$_['error_coupon'] = 'Внимание: Купон недействителен, истек или достигнут лимит использования!';
