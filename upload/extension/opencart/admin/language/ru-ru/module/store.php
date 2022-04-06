ee9b1b116718c063bc295bb2eae903c38f27e981
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:51:39 +0300

    New translations store.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/store.php
+++ upload/extension/opencart/admin/language/ru-ru/module/store.php
@@ -4,7 +4,7 @@ $_['heading_title']    = 'Магазин';
 
 // Text
 $_['text_extension']   = 'Расширения';
-$_['text_success']     = 'Success: You have modified store module!';
+$_['text_success']     = 'Успех: Вы изменили модуль магазин!';
 $_['text_edit']        = 'Редактирование модуля магазина';
 
 // Entry
