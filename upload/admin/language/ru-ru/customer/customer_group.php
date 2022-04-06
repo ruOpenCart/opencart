a36a105e08ee3f55dbb70f6a1d7bb2a8c9df887c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:50:35 +0300

    New translations customer_group.php (Russian)

--- upload/admin/language/ru-ru/customer/customer_group.php
+++ upload/admin/language/ru-ru/customer/customer_group.php
@@ -3,7 +3,7 @@
 $_['heading_title']     = 'Группы покупателя';
 
 // Text
-$_['text_success']      = 'Success: You have modified customer groups!';
+$_['text_success']      = 'Успех: Вы изменили группы покупателя!';
 $_['text_list']         = 'Список групп покупателей';
 $_['text_add']          = 'Добавить группу покупателей';
 $_['text_edit']         = 'Редактирование группы покупателей';
