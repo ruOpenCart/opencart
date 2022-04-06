f5b3ccf34ee9d2819a97856f0533e20b76c94e82
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-06 18:50:33 +0300

    New translations customer.php (Russian)

--- upload/admin/language/ru-ru/customer/customer.php
+++ upload/admin/language/ru-ru/customer/customer.php
@@ -3,7 +3,7 @@
 $_['heading_title']         = 'Покупатели';
 
 // Text
-$_['text_success']          = 'Success: You have modified customers!';
+$_['text_success']          = 'Успех: Вы изменили покупателей!';
 $_['text_list']             = 'Список покупателей';
 $_['text_add']              = 'Добавить клиента';
 $_['text_edit']             = 'Редактирование покупателя';
