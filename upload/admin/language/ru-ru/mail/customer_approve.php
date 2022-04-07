564fb1f81e209b0c5d857dea3b72b0a9e5e7cfba
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-07 18:56:07 +0300

    New translations customer_approve.php (Russian)

--- upload/admin/language/ru-ru/mail/customer_approve.php
+++ upload/admin/language/ru-ru/mail/customer_approve.php
@@ -2,7 +2,7 @@
 // Text
 $_['text_subject'] = '%s - Ваш аккаунт активирован!';
 $_['text_welcome'] = 'Добро пожаловать и спасибо за регистрацию в %s!';
-$_['text_login']   = 'Your account has now been created and you can log in by using your email address and password by visiting our website or at the following URL:';
+$_['text_login']   = 'Ваша учетная запись была создана, и вы можете войти, используя свой адрес электронной почты и пароль, посетив наш веб-сайт по следующему URL:';
 $_['text_service'] = 'После входа в систему вы сможете получить доступ к другим услугам, включая просмотр прошлых заказов, печать счетов-фактур и редактирование информации о вашей учетной записи.';
 $_['text_thanks']  = 'Спасибо,';
 
