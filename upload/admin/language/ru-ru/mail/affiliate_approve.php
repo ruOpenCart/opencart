5154184dcad3303ee941f5faf7b29baf200ca552
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-07 18:55:54 +0300

    New translations affiliate_approve.php (Russian)

--- upload/admin/language/ru-ru/mail/affiliate_approve.php
+++ upload/admin/language/ru-ru/mail/affiliate_approve.php
@@ -2,6 +2,6 @@
 // Text
 $_['text_subject']  = '%s - Ваш партнерский аккаунт активирован!';
 $_['text_welcome']  = 'Добро пожаловать и спасибо за регистрацию в %s!';
-$_['text_login']    = 'Your account has now been approved and you can log in by using your email address and password by visiting our website or at the following URL:';
+$_['text_login']    = 'Ваша учетная запись была одобрена, и вы можете войти, используя свой адрес электронной почты и пароль, посетив наш веб-сайт или по следующему URL:';
 $_['text_service']  = 'После входа в систему вы сможете генерировать коды отслеживания, отслеживать комиссионные платежи и редактировать информацию о вашей учетной записи.';
 $_['text_thanks']   = 'Спасибо,';
