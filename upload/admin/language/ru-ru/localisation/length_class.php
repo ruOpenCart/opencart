8125841a165ec8fe6788e94822563c78b3988b1e
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-08 18:56:18 +0300

    New translations length_class.php (Russian)

--- upload/admin/language/ru-ru/localisation/length_class.php
+++ upload/admin/language/ru-ru/localisation/length_class.php
@@ -26,5 +26,5 @@ $_['help_value']       = 'Значение длины по умолчанию в
 $_['error_permission'] = 'Warning: You do not have permission to modify length classes!';
 $_['error_title']      = 'Длина заголовка должна быть от 3 до 32 символов!';
 $_['error_unit']       = 'Единица длины должна быть от 1 до 4 символов!';
-$_['error_default']    = 'Warning: This length class cannot be deleted as it is currently assigned as the default store length class!';
-$_['error_product']    = 'Warning: This length class cannot be deleted as it is currently assigned to %s products!';
+$_['error_default']    = 'Внимание: Этот класс длины не может быть удален, поскольку он в настоящее время назначен классом длины магазина по умолчанию!';
+$_['error_product']    = 'Внимание: Этот класс длины не может быть удален, поскольку он в настоящее время назначен %s товарам!';
