12644053b409de1729e95a0e0f99b0f800e0e22c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-08 18:56:28 +0300

    New translations shipping_method.php (Russian)

--- upload/catalog/language/ru-ru/checkout/shipping_method.php
+++ upload/catalog/language/ru-ru/checkout/shipping_method.php
@@ -6,6 +6,6 @@ $_['heading_title']          = 'Способ доставки';
 $_['text_success']           = 'Успех: Вы изменили способ доставки!';
 
 // Error
-$_['error_shipping_address'] = 'Warning: Shipping address required!';
-$_['error_shipping_method']  = 'Warning: Shipping method required!';
-$_['error_no_shipping']      = 'Warning: No Shipping options are available. Please <a href="%s">contact us</a> for assistance!';
+$_['error_shipping_address'] = 'Внимание: Адрес доставки обязателен!';
+$_['error_shipping_method']  = 'Внимание: Способ доставки обязателен!';
+$_['error_no_shipping']      = 'Внимание: Нет доступных способов доставки. Пожалуйста, <a href="%s"> свяжитесь с нами</a> для помощи!';
