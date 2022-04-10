a9cae44148534ac1ec4bfc72c4d41716e3289730
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 13:28:07 +0300

    New translations shipping.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/total/shipping.php
+++ upload/extension/opencart/admin/language/ru-ru/total/shipping.php
@@ -13,4 +13,4 @@ $_['entry_status']     = 'Статус';
 $_['entry_sort_order'] = 'Порядок сортировки';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify shipping total!';
+$_['error_permission'] = 'Внимание: У вас нет прав для изменения итоговой стоимости доставки!';
