7e9589030afcc8b35e750e0c9a61ef82663e07aa
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 13:27:56 +0300

    New translations sale_tax.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/sale_tax.php
+++ upload/extension/opencart/admin/language/ru-ru/report/sale_tax.php
@@ -29,4 +29,4 @@ $_['entry_status']       = 'Статус';
 $_['entry_sort_order']   = 'Порядок сортировки';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify tax report!';
+$_['error_permission']   = 'Внимание: У вас нет прав на изменение налогового отчета!';
