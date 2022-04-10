4b3518efd9cba2e831943f5171f03c8d3bc7dd27
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 13:26:01 +0300

    New translations feed.php (Russian)

--- upload/admin/language/ru-ru/extension/feed.php
+++ upload/admin/language/ru-ru/extension/feed.php
@@ -12,4 +12,4 @@ $_['column_status']    = 'Статус';
 $_['column_action']    = 'Действие';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify feeds!';
+$_['error_permission'] = 'Внимание: У вас нет прав на изменение лент новостей!';
