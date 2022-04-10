6cbb583ccb4661ff3e978da2641e7a2cce87915d
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 12:54:27 +0300

    New translations customer_search.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/customer_search.php
+++ upload/extension/opencart/admin/language/ru-ru/report/customer_search.php
@@ -28,4 +28,4 @@ $_['entry_status']      = 'Статус';
 $_['entry_sort_order']  = 'Порядок сортировки';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customer searches report!';
+$_['error_permission']  = 'Внимание: У вас нет прав для изменения отчета о поисковых запросах покупателей!';
