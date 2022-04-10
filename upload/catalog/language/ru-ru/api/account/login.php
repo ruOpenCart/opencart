912a2865dff9244a23c06eb5c321dafa8c4a7098
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 12:54:35 +0300

    New translations login.php (Russian)

--- upload/catalog/language/ru-ru/api/account/login.php
+++ upload/catalog/language/ru-ru/api/account/login.php
@@ -3,6 +3,6 @@
 $_['text_success']     = 'Успех: API сессия успешно начата!';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to access the API!';
+$_['error_permission'] = 'Внимание: У вас нет прав доступа к API!';
 $_['error_key']        = 'Внимание: Неверный ключ API!';
 $_['error_ip']         = 'Внимание: Ваш IP адрес %s не разрешен для доступа к этому API!';
