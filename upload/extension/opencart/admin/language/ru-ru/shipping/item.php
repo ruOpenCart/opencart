466e2d2d868ec16d6a7f701982ed966f0f58be07
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 13:27:59 +0300

    New translations item.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/shipping/item.php
+++ upload/extension/opencart/admin/language/ru-ru/shipping/item.php
@@ -15,4 +15,4 @@ $_['entry_status']     = 'Статус';
 $_['entry_sort_order'] = 'Порядок сортировки';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify shipping per item rates!';
+$_['error_permission'] = 'Внимание: У вас нет прав для изменения стоимости доставки за единицу!';
