011c83be7a8791d14480a6e29af9deea901c0fa9
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 13:27:51 +0300

    New translations product_purchased.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/product_purchased.php
+++ upload/extension/opencart/admin/language/ru-ru/report/product_purchased.php
@@ -25,4 +25,4 @@ $_['entry_status']       = 'Статус';
 $_['entry_sort_order']   = 'Порядок сортировки';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify products purchased report!';
+$_['error_permission']   = 'Внимание: У вас нет прав на изменение отчета о купленных товарах!';
