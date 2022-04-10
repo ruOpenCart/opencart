2216b5e09d2f5bee84334dad626c9f9aabfb6182
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 12:54:31 +0300

    New translations captcha.php (Russian)

--- upload/admin/language/ru-ru/extension/captcha.php
+++ upload/admin/language/ru-ru/extension/captcha.php
@@ -12,4 +12,4 @@ $_['column_status']    = 'Статус';
 $_['column_action']    = 'Действие';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify captchas!';
+$_['error_permission'] = 'Внимание: У вас нет прав для изменения капч!';
