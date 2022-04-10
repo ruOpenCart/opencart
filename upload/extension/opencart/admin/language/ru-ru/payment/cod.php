d6b5271caeb18af6f54c3a9c11d500025dd36997
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 13:28:22 +0300

    New translations cod.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/payment/cod.php
+++ upload/extension/opencart/admin/language/ru-ru/payment/cod.php
@@ -14,4 +14,4 @@ $_['entry_status']       = 'Статус';
 $_['entry_sort_order']   = 'Порядок сортировки';
 
 // Error
-$_['error_permission']   = 'Warning: You do not have permission to modify payment cash on delivery!';
+$_['error_permission']   = 'Внимание: У вас нет прав на изменение оплаты наличными при доставке!';
