deb67cdbb59ea97ff4ee8983c2a97a2e135feff1
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-04-10 13:28:33 +0300

    New translations startup.php (Russian)

--- upload/admin/language/ru-ru/marketplace/startup.php
+++ upload/admin/language/ru-ru/marketplace/startup.php
@@ -12,4 +12,4 @@ $_['column_sort_order'] = 'Порядок сортировки';
 $_['column_action']     = 'Действие';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify startup!';
+$_['error_permission']  = 'Внимание: У вас нет прав для изменения автозагрузки!';
