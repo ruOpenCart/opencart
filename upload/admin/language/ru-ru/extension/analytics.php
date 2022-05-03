8c908bb32fa3f641960f249622d177b35ebbd5b1
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-05-03 20:13:32 +0300

    New translations analytics.php (Russian)

--- upload/admin/language/ru-ru/extension/analytics.php
+++ upload/admin/language/ru-ru/extension/analytics.php
@@ -13,3 +13,4 @@ $_['column_action']    = 'Действие';
 
 // Error
 $_['error_permission'] = 'Внимание: У вас нет прав для изменения аналитики!';
+$_['error_directory']  = 'Warning: Extension directory does not exist!';
