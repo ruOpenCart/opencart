dffa0ea59ef05d879eb8f3241b5eb6a8469d9d47
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-05-04 20:12:19 +0300

    New translations theme.php (Russian)

--- upload/admin/language/ru-ru/extension/theme.php
+++ upload/admin/language/ru-ru/extension/theme.php
@@ -12,4 +12,4 @@ $_['column_action']    = 'Действие';
 
 // Error
 $_['error_permission'] = 'Внимание: У вас нет прав на изменение тем!';
-$_['error_directory']  = 'Warning: Extension directory does not exist!';
+$_['error_directory']  = 'Внимание: Каталог расширения не существует!';
