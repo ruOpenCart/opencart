daf648013e418ad2aff48ea953b48af712310ecd
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-05-04 20:12:29 +0300

    New translations other.php (Russian)

--- upload/admin/language/ru-ru/extension/other.php
+++ upload/admin/language/ru-ru/extension/other.php
@@ -13,4 +13,4 @@ $_['column_action']    = 'Действие';
 
 // Error
 $_['error_permission'] = 'Внимание: У вас нет прав на изменение другого расширения!';
-$_['error_directory']  = 'Warning: Extension directory does not exist!';
+$_['error_directory']  = 'Внимание: Каталог расширения не существует!';
