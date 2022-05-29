41ce843d3360572cfecb49268a8c998ecd6c3ec5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-05-29 12:03:31 +0300

    New translations affiliate.php (Russian)

--- upload/catalog/language/ru-ru/api/sale/affiliate.php
+++ upload/catalog/language/ru-ru/api/sale/affiliate.php
@@ -1,7 +1,7 @@
 <?php
 // Text
 $_['text_success']    = 'Успех: Партнерская комиссия будет применяться к этому заказу!';
-$_['text_remove']     = 'Успех: Ваш купон на скидку был удален!';
+$_['text_remove']     = 'Success: Your affiliate commission has been removed!';
 
 // Error
 $_['error_affiliate'] = 'Внимание: Партнер не найден!';
