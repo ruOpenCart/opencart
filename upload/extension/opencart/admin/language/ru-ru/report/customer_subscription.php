92eb8277ed022294c88af6f7c252bc709d607bfb
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-05-30 22:32:27 +0300

    New translations customer_subscription.php (Russian)

---
+++ upload/extension/opencart/admin/language/ru-ru/report/customer_subscription.php
@@ -0,0 +1,27 @@
+<?php
+// Heading
+$_['heading_title']         = 'Customer Subscription Report';
+
+// Text
+$_['text_extension']        = 'Расширения';
+$_['text_edit']             = 'Edit Customer Subscription Report';
+$_['text_success']          = 'Success: You have modified customer subscription report!';
+$_['text_filter']           = 'Фильтр';
+
+// Column
+$_['column_customer']       = 'Имя покупателя';
+$_['column_email']          = 'Эл. почта';
+$_['column_customer_group'] = 'Группа покупателя';
+$_['column_status']         = 'Статус';
+$_['column_total']          = 'Всего';
+$_['column_action']         = 'Действие';
+
+// Entry
+$_['entry_date_start']      = 'Дата начала';
+$_['entry_date_end']        = 'Дата окончания';
+$_['entry_customer']        = 'Покупатель';
+$_['entry_status']          = 'Статус';
+$_['entry_sort_order']      = 'Порядок сортировки';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify customer subscription report!';
