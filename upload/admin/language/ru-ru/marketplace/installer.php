2b514616fb968020c989d461b7746ef8882e8188
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-06-15 17:37:24 +0300

    New translations installer.php (Russian)

--- upload/admin/language/ru-ru/marketplace/installer.php
+++ upload/admin/language/ru-ru/marketplace/installer.php
@@ -1,34 +1,38 @@
 <?php
 // Heading
-$_['heading_title']     = 'Установщик расширений';
+$_['heading_title']          = 'Установщик расширений';
 
 // Text
-$_['text_upload']       = 'Успех: Расширение было загружено!';
-$_['text_install']      = 'Успех: Расширение было установлено!';
-$_['text_uninstall']    = 'Успех: Расширение было деинсталлировано!';
-$_['text_delete']       = 'Успех: Расширение было удалено!';
-$_['text_progress']     = 'Прогресс установки';
-$_['text_installed']    = 'Установленные расширения';
-$_['text_unknown']		= 'Неизвестно';
+$_['text_upload']            = 'Успех: Расширение было загружено!';
+$_['text_success']           = 'Успех: Вы изменили расширения!';
+$_['text_progress']          = 'Прогресс установки';
+$_['text_installed']         = 'Установленные расширения';
 
 // Column
-$_['column_image']      = 'Изображение';
-$_['column_name']       = 'Название расширения';
-$_['column_version']    = 'Версия';
-$_['column_date_added'] = 'Дата добавления';
-$_['column_action']     = 'Действие';
+$_['column_image']           = 'Изображение';
+$_['column_name']            = 'Название расширения';
+$_['column_version']         = 'Версия';
+$_['column_date_added']      = 'Дата добавления';
+$_['column_action']          = 'Действие';
 
 // Entry
-$_['entry_progress']    = 'Прогресс';
+$_['entry_progress']         = 'Прогресс';
 
 // Error
 $_['error_permission']       = 'Внимание: У вас нет прав на изменение расширений!';
-$_['error_install']          = 'Установка не найдена!';
+$_['error_install']          = 'Warning: Could not find install.json!';
+
+$_['error_extension']        = 'Installed extension could not be found!';
 $_['error_installed']        = 'Расширение уже установлено!';
+$_['error_name']             = 'Name must be between 3 and 128 characters!';
+$_['error_version']          = 'Version must be between 3 and 128 characters!';
+$_['error_author']           = 'Author must be between 3 and 128 characters!';
+$_['error_link']             = 'Link must be between 3 and 128 characters!';
+
 $_['error_filename']         = 'Имя файла должно быть от 3 до 128 символов!';
 $_['error_file']             = 'Файл установки %s не найден!';
 $_['error_file_exists']      = 'Файл уже существует!';
-$_['error_file_type']         = 'Недопустимый тип файла!';
+$_['error_file_type']        = 'Недопустимый тип файла!';
 $_['error_directory']        = 'Каталог установки %s не найден!';
 $_['error_directory_exists'] = 'Путь %s уже существует!';
 $_['error_unzip']            = 'Zip-файл не может быть открыт!';
