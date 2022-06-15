5a494dd47333f0ac8d9433c543ff8a2d5f779437
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-06-15 17:52:30 +0300

    New translations pin.php (Russian)

--- upload/admin/language/ru-ru/common/pin.php
+++ upload/admin/language/ru-ru/common/pin.php
@@ -5,19 +5,19 @@ $_['heading_title'] = 'Защитите свою учетную запись';
 // Text
 $_['text_setup']    = 'Установите PIN-код для своей учетной записи';
 $_['text_pin']      = 'Пожалуйста, введите 4-значный PIN-код для защиты вашей учетной записи. Это одноразовый PIN-код, который вы вводите в качестве секретного вопроса всякий раз, когда входите в систему с неизвестного устройства или компьютера.';
-$_['text_security'] = 'PIN Security Check';
+$_['text_security'] = 'Проверка безопасности PIN-кода';
 $_['text_confirm']  = 'Пожалуйста, подтвердите, кто вы!';
-$_['text_protect']  = 'Please enter your 4 digit PIN number. This PIN number is to protect your account. When ever you logon from a new device you will be asked for this PIN.';
+$_['text_protect']  = 'Пожалуйста, введите свой 4-значный PIN-код. Этот PIN-код предназначен для защиты вашей учетной записи. Каждый раз, когда вы входите в систему с нового устройства, вам будет предложено ввести этот PIN-код.';
 $_['text_success']  = 'Успех: ПИН-код вашей учетной записи успешно добавлен!';
-$_['text_link']     = 'Success: An email with a reset link has been sent your email address!';
-$_['text_reset']    = 'Success: Your PIN has been reset!';
+$_['text_link']     = 'Успех: На ваш адрес электронной почты отправлено письмо со ссылкой для сброса!';
+$_['text_reset']    = 'Успех: Ваш PIN-код был сброшен!';
 
 // Entry
 $_['entry_pin']     = 'PIN';
 
 // Error
-$_['error_warning'] = 'You have exceeded the allowed number of PIN attempts!';
+$_['error_warning'] = 'Вы превысили допустимое количество попыток ввода PIN-кода!';
 $_['error_pin']     = 'PIN-код должен содержать 4 цифры!';
-$_['error_match']   = 'PIN does not match!';
-$_['error_reset']   = 'Warning: Could not reset your PIN!';
+$_['error_match']   = 'PIN-код не совпадает!';
+$_['error_reset']   = 'Внимание: Не удалось сбросить PIN-код!';
 
