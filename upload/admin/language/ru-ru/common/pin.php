afc1bbcf090e1d88e4ea80cd6126fa75094629b5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-06-15 17:39:50 +0300

    New translations pin.php (Russian)

--- upload/admin/language/ru-ru/common/pin.php
+++ upload/admin/language/ru-ru/common/pin.php
@@ -3,13 +3,21 @@
 $_['heading_title'] = 'Защитите свою учетную запись';
 
 // Text
-$_['text_success']  = 'Успех: ПИН-код вашей учетной записи успешно добавлен!';
 $_['text_setup']    = 'Установите PIN-код для своей учетной записи';
 $_['text_pin']      = 'Пожалуйста, введите 4-значный PIN-код для защиты вашей учетной записи. Это одноразовый PIN-код, который вы вводите в качестве секретного вопроса всякий раз, когда входите в систему с неизвестного устройства или компьютера.';
+$_['text_security'] = 'PIN Security Check';
+$_['text_confirm']  = 'Пожалуйста, подтвердите, кто вы!';
+$_['text_protect']  = 'Please enter your 4 digit PIN number. This PIN number is to protect your account. When ever you logon from a new device you will be asked for this PIN.';
+$_['text_success']  = 'Успех: ПИН-код вашей учетной записи успешно добавлен!';
+$_['text_link']     = 'Success: An email with a reset link has been sent your email address!';
+$_['text_reset']    = 'Success: Your PIN has been reset!';
 
 // Entry
 $_['entry_pin']     = 'PIN';
 
 // Error
+$_['error_warning'] = 'You have exceeded the allowed number of PIN attempts!';
 $_['error_pin']     = 'PIN-код должен содержать 4 цифры!';
-$_['error_hacker']  = 'Вам не разрешается использовать PIN-код, который легко подобрать хакерам!';
+$_['error_match']   = 'PIN does not match!';
+$_['error_reset']   = 'Warning: Could not reset your PIN!';
+
