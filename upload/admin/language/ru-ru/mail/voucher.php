dbb22fa5548a8a379b4b8d9a5efef19ccae10324
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-09-29 22:33:20 +0300

    New translations voucher.php (Russian)

--- upload/admin/language/ru-ru/mail/voucher.php
+++ upload/admin/language/ru-ru/mail/voucher.php
@@ -5,6 +5,6 @@ $_['text_subject']  = 'Вы получили подарочный сертифи
 $_['text_greeting'] = 'Поздравляем, вы получили подарочный сертификат на сумму %s';
 $_['text_from']     = 'Этот подарочный сертификат отправлен вам от %s';
 $_['text_message']  = 'С сообщением';
-$_['text_redeem']   = 'Чтобы воспользоваться данным Подарочным Сертификатом, сохраните код <b>%s</b>, затем перейдите в магазин и закажите товары, которые Вам понравятся. Вы можете ввести код подарочного сертификата на странице просмотра корзины, перед тем как начнете оформление заказа.';
+$_['text_redeem']   = 'In the first place, to redeem this Gift Certificate, write down the redemption code which is <b>%s</b>. Then, click on the link below and purchase the product you wish to use this gift certificate. Lastly, you can enter the gift certificate code on the <b>Shopping Cart</b> page before you click: <b>Checkout</b>.';
 $_['text_footer']   = 'Пожалуйста, ответьте на это письмо, если у вас есть какие-либо вопросы.';
 $_['text_sent']     = 'Успех: Подарочный сертификат успешно отправлен на указанный e-mail адрес!';
