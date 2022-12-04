d8a3887d4ff8627439168889417cbc5481909288
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2022-12-04 19:01:16 +0300

    New translations subscription.php (Russian)

--- upload/admin/language/ru-ru/mail/subscription.php
+++ upload/admin/language/ru-ru/mail/subscription.php
@@ -1,10 +1,10 @@
 <?php
 // Text
-$_['text_subject']             = '%s - Subscription';
+$_['text_subject']             = '%s - Подписка';
 $_['text_subscription_id']     = 'Идентификатор подписки';
-$_['text_date_added']          = 'Subscription Date:';
-$_['text_subscription_status'] = 'Your subscription has been added to the following status:';
-$_['text_comment']             = 'The comments for your subscription are:';
+$_['text_date_added']          = 'Дата подписки:';
+$_['text_subscription_status'] = 'Ваша подписка добавлена в следующий статус:';
+$_['text_comment']             = 'Комментарии к вашей подписке:';
 $_['text_payment_method']      = 'Метод оплаты';
-$_['text_payment_code']        = 'Payment Code';
+$_['text_payment_code']        = 'Код оплаты';
 $_['text_footer']              = 'Пожалуйста, ответьте на это письмо, если у вас есть какие-либо вопросы.';
