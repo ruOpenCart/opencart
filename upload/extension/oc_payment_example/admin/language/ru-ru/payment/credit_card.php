6aeb1319d82bcccac66372baec2ba8681a345991
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-01-10 20:04:25 +0300

    New translations credit_card.php (Russian)

---
+++ upload/extension/oc_payment_example/admin/language/ru-ru/payment/credit_card.php
@@ -0,0 +1,36 @@
+<?php
+// Heading
+$_['heading_title']         = 'Credit Card';
+
+// Text
+$_['text_description']      = 'This payment method is a dummy one and will not store credit card information nor should it be used on a live site. This extension is here for developers that need to create a payment gateway integration.';
+$_['text_extension']        = 'Расширения';
+$_['text_success']          = 'Success: You have modified credit card payment module!';
+$_['text_edit']             = 'Edit Credit Card';
+$_['text_approve']          = 'Одобрить';
+$_['text_deny']             = 'Отклонить';
+$_['text_report']           = 'Отчет';
+
+// Column
+$_['column_order']          = 'Номер заказа';
+$_['column_card']           = 'Credit Card';
+$_['column_amount']         = 'Сумма';
+$_['column_response']       = 'Response';
+$_['column_status']         = 'Статус заказа';
+$_['column_date_added']     = 'Дата добавления';
+$_['column_action']         = 'Действие';
+
+// Entry
+$_['entry_response']        = 'Card Processor Response';
+$_['entry_approved_status'] = 'Approved Status';
+$_['entry_failed_status']   = 'Failed Status';
+$_['entry_order_status']    = 'Статус заказа';
+$_['entry_geo_zone']        = 'Гео-зона';
+$_['entry_status']          = 'Статус';
+$_['entry_sort_order']      = 'Порядок сортировки';
+
+// Help
+$_['help_response']         = 'Choose if the credit card should return approved or denied making test orders';
+
+// Error
+$_['error_permission']      = 'Warning: You do not have permission to modify payment credit card!';
