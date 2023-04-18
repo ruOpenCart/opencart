de8febed4b6cf7861739309660b5d22d83cdfa1b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-04-18 09:23:13 +0300

    New translations payment_method.php (Russian)

--- upload/catalog/language/ru-ru/checkout/payment_method.php
+++ upload/catalog/language/ru-ru/checkout/payment_method.php
@@ -1,15 +1,21 @@
 <?php
 // Heading
-$_['heading_title']         = 'Метод оплаты';
+$_['heading_title']          = 'Метод оплаты';
 
 // Text
-$_['text_stored']           = 'Сохраненные способы оплаты';
-$_['text_comments']         = 'Добавление комментария к заказу';
-$_['text_agree']            = 'Я прочитал и согласен с <a href="%s" class="agree"><b>%s</b></a>';
-$_['text_success']          = 'Успех: Вы изменили способ оплаты!';
-$_['text_comment']          = 'Успех: Комментарий добавлен!';
+$_['text_payment_method']    = 'Payment method options';
+$_['text_payment']           = 'Please select the preferred payment method to use for this order.';
+$_['text_comments']          = 'Добавление комментария к заказу';
+$_['text_agree']             = 'Я прочитал и согласен с <a href="%s" class="agree"><b>%s</b></a>';
+$_['text_success']           = 'Успех: Вы изменили способ оплаты!';
+$_['text_comment']           = 'Успех: Комментарий добавлен!';
+
+// Entry
+$_['entry_payment_method']   = 'Choose payment method...';
 
 // Error
-$_['error_payment_address'] = 'Внимание: Платежный адрес обязателен!';
-$_['error_payment_method']  = 'Внимание: Способ оплаты обязателен!';
-$_['error_no_payment']      = 'Внимание: Нет доступных вариантов оплаты. Пожалуйста, <a href="%s">свяжитесь с нами</a> для помощи!';
+$_['error_payment_address']  = 'Payment address required!';
+$_['error_shipping_address'] = 'Shipping address required!';
+$_['error_shipping_method']  = 'Shipping method required!';
+$_['error_payment_method']   = 'Payment method required!';
+$_['error_no_payment']       = 'No Payment options are available. Please <a href="%s">contact us</a> for assistance!';
