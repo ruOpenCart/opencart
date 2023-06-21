462c4c8960ff9bcf57cc4042c15ec7f2e681f3ac
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-06-21 22:40:46 +0300

    New translations order.php (Russian)

--- upload/catalog/language/ru-ru/account/order.php
+++ upload/catalog/language/ru-ru/account/order.php
@@ -1,18 +1,18 @@
 <?php
 // Heading
-$_['heading_title']              = 'История заказа';
+$_['heading_title']              = 'Заказы';
 
 // Text
 $_['text_account']               = 'Аккаунт';
-$_['text_order']                 = 'Информация о заказе';
-$_['text_order_detail']          = 'Детали заказа';
-$_['text_invoice_no']            = 'Счет № :';
-$_['text_order_id']              = 'Номер заказа:';
-$_['text_date_added']            = 'Дата добавления:';
+$_['text_order']                 = 'Order #%s';
+$_['text_invoice_no']            = 'Счет №';
+$_['text_order_id']              = 'Номер заказа';
+$_['text_order_status']          = 'Статус заказа';
+$_['text_shipping_method']       = 'Способ доставки';
+$_['text_payment_method']        = 'Метод оплаты';
+$_['text_date_added']            = 'Дата добавления';
 $_['text_shipping_address']      = 'Адрес доставки';
-$_['text_shipping_method']       = 'Способ доставки:';
 $_['text_payment_address']       = 'Платежный адрес';
-$_['text_payment_method']        = 'Метод оплаты:';
 $_['text_subscription']          = 'Подписка';
 $_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
 $_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
@@ -38,9 +38,9 @@ $_['column_quantity']            = 'Количество';
 $_['column_price']               = 'Цена';
 $_['column_total']               = 'Всего';
 $_['column_action']              = 'Действие';
-$_['column_date_added']          = 'Дата добавления';
 $_['column_status']              = 'Статус';
 $_['column_comment']             = 'Комментарий';
+$_['column_date_added']          = 'Дата добавления';
 
 // Error
 $_['error_reorder']              = '%s в настоящее время недоступен для повторного заказа.';
