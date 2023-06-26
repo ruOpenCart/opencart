39b4bba119ec68d807f8345e0261583ec3376b02
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-06-26 17:26:29 +0300

    Update source file product.php

--- upload/admin/language/en-gb/catalog/product.php
+++ upload/admin/language/en-gb/catalog/product.php
@@ -32,7 +32,7 @@ $_['text_image']              = 'Image';
 $_['text_image_additional']   = 'Additional Images';
 $_['text_reward']             = 'Buy Points';
 $_['text_points']             = 'Reward Points';
-$_['text_report']             = 'Reports';
+$_['text_report']             = 'Product Reports';
 
 // Column
 $_['column_name']             = 'Product Name';
