da9c36eab9e806f743dc993406910f36b3ece407
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-06-27 17:37:02 +0300

    New translations bestseller.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/bestseller.php
+++ upload/extension/opencart/admin/language/ru-ru/module/bestseller.php
@@ -1,19 +1,13 @@
 <?php
 // Heading
-$_['heading_title']    = 'Best Sellers';
+$_['heading_title']    = 'Bestsellers';
 
 // Text
 $_['text_extension']   = 'Расширения';
-$_['text_success']     = 'Success: You have modified best sellers module!';
-$_['text_next']        = 'Success: You have modified %s best sellers out of %s!';
-$_['text_edit']        = 'Edit Best Sellers Module';
+$_['text_success']     = 'Success: You have modified bestsellers module!';
+$_['text_edit']        = 'Edit Bestsellers Module';
 $_['text_horizontal']  = 'Горизонтально';
 $_['text_vertical']    = 'Вертикально';
-$_['text_report']      = 'Отчеты';
-
-// Column
-$_['column_product']   = 'Товар';
-$_['column_total']     = 'Всего';
 
 // Entry
 $_['entry_name']       = 'Название модуля';
@@ -23,11 +17,8 @@ $_['entry_width']      = 'Ширина изображения';
 $_['entry_height']     = 'Высота изображения';
 $_['entry_status']     = 'Статус';
 
-// Button
-$_['button_sync']      = 'Generate Best Sellers List';
-
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify best sellers module!';
+$_['error_permission'] = 'Warning: You do not have permission to modify bestsellers module!';
 $_['error_name']       = 'Название модуля должно быть от 3 до 64 символов!';
 $_['error_width']      = 'Необходимо указать ширину!';
 $_['error_height']     = 'Необходимо указать высоту!';
