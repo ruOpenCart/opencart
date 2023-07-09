eec249d2ae79e0b3b10c7f75de08fffb592fc894
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-07-09 14:02:29 +0300

    New translations review.php (Russian)

--- upload/admin/language/ru-ru/catalog/review.php
+++ upload/admin/language/ru-ru/catalog/review.php
@@ -4,7 +4,7 @@ $_['heading_title']     = 'Отзывы';
 
 // Text
 $_['text_success']      = 'Успех: Вы изменили отзывы!';
-$_['text_rating']       = 'Success: You have modified %s product ratings out of %s!';
+$_['text_rating']       = 'Успех: Вы изменили %s оценок товаров из %s!';
 $_['text_list']         = 'Список отзывов';
 $_['text_add']          = 'Добавление отзыва';
 $_['text_edit']         = 'Редактирование отзыва';
