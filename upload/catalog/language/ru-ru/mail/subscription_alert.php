938e9ef0e3c5fee7c772a1b6bb39692ede4f3a61
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-07-09 12:02:33 +0300

    New translations subscription_alert.php (Russian)

---
+++ upload/catalog/language/ru-ru/mail/subscription_alert.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_subject']              = '%s - Order %s - Canceled Subscription';
+$_['text_received']             = 'You have received a canceled subscription.';
+$_['text_orders_id']            = 'Номер заказа:';
+$_['text_subscription_id']      = 'Идентификатор подписки';
+$_['text_date_added']           = 'Дата добавления:';
+$_['text_subscription_status']  = 'Subscription Status:';
+$_['text_comment']              = 'Комментарии к вашей подписке:';
+$_['text_canceled']             = 'Success: The Subscription profile has been canceled!';
