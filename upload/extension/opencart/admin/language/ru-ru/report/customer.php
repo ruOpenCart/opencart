ad2ab4cec1eedb8fb13b18686d241613f36672ec
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-08-03 22:14:59 +0300

    New translations customer.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/customer.php
+++ upload/extension/opencart/admin/language/ru-ru/report/customer.php
@@ -1,11 +1,11 @@
 <?php
 // Heading
-$_['heading_title']     = 'Customer Report';
+$_['heading_title']     = 'Отчёт о покупателях';
 
 // Text
 $_['text_extension']    = 'Расширения';
-$_['text_edit']         = 'Edit Customer Report';
-$_['text_success']      = 'Success: You have modified customers report!';
+$_['text_edit']         = 'Редактирование отчета о покупателях';
+$_['text_success']      = 'Успех: Вы изменили отчет о покупателях!';
 $_['text_filter']       = 'Фильтр';
 $_['text_year']         = 'Годы';
 $_['text_month']        = 'Месяцы';
@@ -16,7 +16,7 @@ $_['text_all_status']   = 'Все статусы';
 // Column
 $_['column_date_start'] = 'Дата начала';
 $_['column_date_end']   = 'Дата окончания';
-$_['column_total']      = 'No. Customers';
+$_['column_total']      = 'Всего покупателей';
 
 // Entry
 $_['entry_date_start']  = 'Дата начала';
@@ -26,4 +26,4 @@ $_['entry_status']      = 'Статус';
 $_['entry_sort_order']  = 'Порядок сортировки';
 
 // Error
-$_['error_permission']  = 'Warning: You do not have permission to modify customers report!';
+$_['error_permission']  = 'Внимание: У вас нет прав на изменение отчета о покупателях!';
