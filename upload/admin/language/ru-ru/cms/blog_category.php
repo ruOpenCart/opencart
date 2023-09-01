6150044163a7db8bd0a29294fbfb8c6a46b018f5
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-09-01 20:08:55 +0300

    New translations blog_category.php (Russian)

---
+++ upload/admin/language/ru-ru/cms/blog_category.php
@@ -0,0 +1,38 @@
+<?php
+// Heading
+$_['heading_title']           = 'Blog Category';
+
+// Text
+$_['text_success']            = 'Success: You have modified blog categories!';
+$_['text_list']               = 'Blog Category List';
+$_['text_add']                = 'Add Blog Category';
+$_['text_edit']               = 'Edit Blog Category';
+$_['text_default']            = 'По умолчанию';
+$_['text_keyword']            = 'Не используйте пробелы, вместо этого заменяйте пробелы на - и убедитесь, что Семантический URL-адрес является глобально уникальным.';
+
+// Column
+$_['column_name']             = 'Blog Category Name';
+$_['column_sort_order']       = 'Порядок сортировки';
+$_['column_action']           = 'Действие';
+
+// Entry
+$_['entry_image']             = 'Изображение';
+$_['entry_name']              = 'Blog Category Name';
+$_['entry_description']       = 'Описание';
+$_['entry_meta_title']        = 'Мета-тег Title';
+$_['entry_meta_keyword']      = 'Мета-тег Keywords';
+$_['entry_meta_description']  = 'Мета-тег Description';
+$_['entry_store']             = 'Магазины';
+$_['entry_sort_order']        = 'Порядок сортировки';
+$_['entry_status']            = 'Статус';
+$_['entry_keyword']           = 'Ключевое слово';
+
+// Error
+$_['error_warning']           = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
+$_['error_permission']        = 'Warning: You do not have permission to modify blogs!';
+$_['error_name']              = 'Blog Name must be between 1 and 255 characters!';
+$_['error_meta_title']        = 'Мета заголовок должен быть больше 1 и меньше 255 символов!';
+$_['error_category']          = 'Выбранная вами родительская категория является дочерней по отношению к текущей!';
+$_['error_keyword']           = 'SEO URL-адрес должен быть от 1 до 64 символов!';
+$_['error_keyword_exists']    = 'Семантический URL-адрес должен быть уникальным!';
+$_['error_keyword_character'] = 'Ключевое слово может использовать только символы a-z, 0-9, - и _!';
