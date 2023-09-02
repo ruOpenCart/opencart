0a50e9486b8a355319e3c23f378e2b3dea867bd7
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-09-02 20:09:06 +0300

    New translations blog.php (Russian)

--- upload/admin/language/ru-ru/cms/blog.php
+++ upload/admin/language/ru-ru/cms/blog.php
@@ -1,29 +1,29 @@
 <?php
 // Heading
-$_['heading_title']           = 'Blog';
+$_['heading_title']           = 'Блог';
 
 // Text
-$_['text_success']            = 'Success: You have modified blogs!';
-$_['text_list']               = 'Blog List';
-$_['text_add']                = 'Add Blog';
-$_['text_edit']               = 'Edit Blog';
+$_['text_success']            = 'Успех: Вы изменили блоги!';
+$_['text_list']               = 'Список блогов';
+$_['text_add']                = 'Добавить блог';
+$_['text_edit']               = 'Редактировать блог';
 $_['text_default']            = 'По умолчанию';
 $_['text_keyword']            = 'Не используйте пробелы, вместо этого заменяйте пробелы на - и убедитесь, что Семантический URL-адрес является глобально уникальным.';
 
 // Column
-$_['column_name']             = 'Blog Name';
+$_['column_name']             = 'Название блога';
 $_['column_author']           = 'Автор';
 $_['column_date_added']       = 'Дата добавления';
 $_['column_action']           = 'Действие';
 
 // Entry
 $_['entry_image']             = 'Изображение';
-$_['entry_name']              = 'Blog Name';
+$_['entry_name']              = 'Название блога';
 $_['entry_description']       = 'Описание';
 $_['entry_meta_title']        = 'Мета-тег Title';
 $_['entry_meta_keyword']      = 'Мета-тег Keywords';
 $_['entry_meta_description']  = 'Мета-тег Description';
-$_['entry_category']          = 'Blog Category';
+$_['entry_category']          = 'Категория блога';
 $_['entry_author']            = 'Автор';
 $_['entry_store']             = 'Магазины';
 $_['entry_sort_order']        = 'Порядок сортировки';
@@ -33,8 +33,8 @@ $_['entry_layout']            = 'Переопределение макета';
 
 // Error
 $_['error_warning']           = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
-$_['error_permission']        = 'Warning: You do not have permission to modify blogs!';
-$_['error_name']              = 'Blog Name must be between 1 and 255 characters!';
+$_['error_permission']        = 'Внимание: У вас нет прав на изменение блогов!';
+$_['error_name']              = 'Название блога должно быть от 1 до 255 символов!';
 $_['error_meta_title']        = 'Мета заголовок должен быть больше 1 и меньше 255 символов!';
 $_['error_keyword']           = 'SEO URL-адрес должен быть от 1 до 64 символов!';
 $_['error_keyword_exists']    = 'Семантический URL-адрес должен быть уникальным!';
