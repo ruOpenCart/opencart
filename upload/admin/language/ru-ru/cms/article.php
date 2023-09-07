d0124a7f2668591353cb19d864dc99e02e255214
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-09-07 19:50:09 +0300

    New translations article.php (Russian)

---
+++ upload/admin/language/ru-ru/cms/article.php
@@ -0,0 +1,43 @@
+<?php
+// Heading
+$_['heading_title']           = 'Articles';
+
+// Text
+$_['text_success']            = 'Success: You have modified articles!';
+$_['text_list']               = 'Article List';
+$_['text_add']                = 'Add Article';
+$_['text_edit']               = 'Edit Article';
+$_['text_default']            = 'По умолчанию';
+$_['text_keyword']            = 'Не используйте пробелы, вместо этого заменяйте пробелы на - и убедитесь, что Семантический URL-адрес является глобально уникальным.';
+
+// Column
+$_['column_name']             = 'Article Name';
+$_['column_author']           = 'Автор';
+$_['column_date_added']       = 'Дата добавления';
+$_['column_action']           = 'Действие';
+
+// Entry
+$_['entry_image']             = 'Изображение';
+$_['entry_name']              = 'Article Name';
+$_['entry_description']       = 'Описание';
+$_['entry_tag']               = 'Tags';
+$_['entry_meta_title']        = 'Мета-тег Title';
+$_['entry_meta_keyword']      = 'Мета-тег Keywords';
+$_['entry_meta_description']  = 'Мета-тег Description';
+$_['entry_topic']             = 'Topic';
+$_['entry_author']            = 'Автор';
+$_['entry_store']             = 'Магазины';
+$_['entry_sort_order']        = 'Порядок сортировки';
+$_['entry_status']            = 'Статус';
+$_['entry_keyword']           = 'Ключевое слово';
+$_['entry_layout']            = 'Переопределение макета';
+
+// Error
+$_['error_warning']           = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
+$_['error_permission']        = 'Warning: You do not have permission to modify articles!';
+$_['error_name']              = 'Article Name must be between 1 and 255 characters!';
+$_['error_meta_title']        = 'Мета заголовок должен быть больше 1 и меньше 255 символов!';
+$_['error_keyword']           = 'SEO URL-адрес должен быть от 1 до 64 символов!';
+$_['error_keyword_exists']    = 'Семантический URL-адрес должен быть уникальным!';
+$_['error_keyword_character'] = 'Ключевое слово может использовать только символы a-z, 0-9, - и _!';
+$_['error_author']            = 'Автор должен быть от 3 до 64 символов!';
