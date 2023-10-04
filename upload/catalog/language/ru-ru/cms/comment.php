26ec7faf82464d2f358e7ada17741aac990f2db3
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-10-04 21:20:27 +0300

    New translations comment.php (Russian)

---
+++ upload/catalog/language/ru-ru/cms/comment.php
@@ -0,0 +1,28 @@
+<?php
+// Heading
+$_['heading_title']   = 'Блог';
+
+// Text
+$_['text_refine']     = 'Уточнить поиск';
+$_['text_all']        = 'Все';
+$_['text_blog']       = 'Блог';
+$_['text_by']         = 'By';
+$_['text_tags']       = 'Тэги:';
+$_['text_comment']    = 'comments';
+$_['text_write']      = 'Write a comment';
+$_['text_login']      = 'Please <a href="%s">login</a> or <a href="%s">register</a> to comment';
+$_['text_no_results'] = 'Be the first to write a comment for this article.';
+$_['text_queue']      = 'Your comment has been added to our moderation queue!';
+$_['text_success']    = 'Thank you for your comment. It has been submitted to the webmaster for approval.';
+
+// Entry
+$_['entry_search']    = 'Искать';
+$_['entry_author']    = 'Ваше имя';
+$_['entry_comment']   = 'Комментарий';
+
+// Error
+$_['error_article']   = 'Warning: Article could not be found!';
+$_['error_token']     = 'Warning: Comment token invalid!';
+$_['error_comment']   = 'Comment must be between 25 and 1000 characters!';
+$_['error_guest']     = 'You must login to comment on the article!';
+$_['error_status']    = 'Comments are disabled!';
