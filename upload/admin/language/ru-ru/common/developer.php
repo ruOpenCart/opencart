9792a4726ddcb8a54543516e18df784c5250defd
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-10-04 21:20:00 +0300

    New translations developer.php (Russian)

--- upload/admin/language/ru-ru/common/developer.php
+++ upload/admin/language/ru-ru/common/developer.php
@@ -1,25 +1,26 @@
 <?php
 // Heading
-$_['heading_title']    = 'Настройки разработчика';
+$_['heading_title']          = 'Developer Options';
 
 // Text
-$_['text_success']     = 'Успех: Вы изменили настройки разработчика!';
-$_['text_theme']       = 'Тема';
-$_['text_sass']        = 'SASS';
-$_['text_cache']       = 'Успех: Вы очистили кэш %s!';
+$_['text_developer_success'] = 'Success: You have modified developer settings!';
+$_['text_cache_success']     = 'Success: You have cleared the cache!';
+$_['text_theme_success']     = 'Success: You have cleared the theme cache!';
+$_['text_sass_success']      = 'Success: You have cleared the SASS cache!';
+$_['text_theme']             = 'Тема';
+$_['text_sass']              = 'SASS';
+$_['text_cache']             = 'Кэш';
 
 // Column
-$_['column_component'] = 'Компонент';
-$_['column_action']    = 'Действие';
+$_['column_component']       = 'Компонент';
+$_['column_action']          = 'Действие';
 
 // Entry
-$_['entry_theme']      = 'Тема';
-$_['entry_sass']       = 'SASS';
-$_['entry_cache']      = 'Кэш';
+$_['entry_cache']            = 'Кэш';
 
 // Button
-$_['button_on']        = 'Вкл';
-$_['button_off']       = 'Выкл';
+$_['button_on']              = 'Вкл';
+$_['button_off']             = 'Выкл';
 
 // Error
-$_['error_permission'] = 'Внимание: У вас нет прав на изменение настроек разработчика!';
+$_['error_permission']       = 'Внимание: У вас нет прав на изменение настроек разработчика!';
