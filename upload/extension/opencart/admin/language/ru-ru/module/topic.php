df30f287e1b9a584bdddec2a4134485752478dae
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-10-05 21:24:51 +0300

    New translations topic.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/module/topic.php
+++ upload/extension/opencart/admin/language/ru-ru/module/topic.php
@@ -4,11 +4,11 @@ $_['heading_title']    = 'Тема';
 
 // Text
 $_['text_extension']   = 'Расширения';
-$_['text_success']     = 'Success: You have modified topic module!';
-$_['text_edit']        = 'Edit Topic Module';
+$_['text_success']     = 'Успех: Вы изменили модуль топика!';
+$_['text_edit']        = 'Редактирование модуля топика';
 
 // Entry
 $_['entry_status']     = 'Статус';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify topic module!';
+$_['error_permission'] = 'Внимание: У вас нет прав для изменения модуля топика!';
