a0d5cbdad2945f7c37a3702e2a9bd879575e5d2b
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-11-06 23:48:46 +0300

    Update source file column_left.php

--- upload/admin/language/en-gb/common/column_left.php
+++ upload/admin/language/en-gb/common/column_left.php
@@ -46,7 +46,6 @@ $_['text_manufacturer']        = 'Manufacturers';
 $_['text_marketing']           = 'Marketing';
 $_['text_marketplace']         = 'Marketplace';
 $_['text_menu']                = 'Menu Manager';
-$_['text_modification']        = 'Modification';
 $_['text_navigation']          = 'Navigation';
 $_['text_online']              = 'Who\'s Online';
 $_['text_option']              = 'Options';
