0b81057d615c1bf8ee0e3b77ecea878e4d2d75b1
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-11-06 23:58:31 +0300

    New translations cron.php (Russian)

--- upload/admin/language/ru-ru/marketplace/cron.php
+++ upload/admin/language/ru-ru/marketplace/cron.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']        = 'Задания планировщика';
+$_['heading_title']        = 'CRON Jobs';
 
 // Text
-$_['text_success']         = 'Успех: Вы изменили задания крона!';
+$_['text_success']         = 'Success: You have modified CRON jobs!';
 $_['text_instruction']     = 'Инструкции по Крону';
-$_['text_list']            = 'Список задач';
-$_['text_cron_1']          = 'Cron Job — это запланированные задачи, которые запускаются периодически. Чтобы настроить серверы для использования задания cron, вы можете прочитать страницу <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">документации опенкарт</a>.';
-$_['text_cron_2']          = 'Вам нужно установить задание планировщика для запуска каждый час.';
+$_['text_list']            = 'CRON List';
+$_['text_cron_1']          = 'CRON Job\'s are scheduled tasks that are run periodically. To setup your servers to use cron job you can read the <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">opencart documentation</a> page.';
+$_['text_cron_2']          = 'You need to set your CRON task to run every hour.';
 $_['text_info']            = 'Информация по Крону';
 $_['text_hour']            = 'Час';
 $_['text_day']             = 'День';
 $_['text_month']           = 'Месяц';
 
 // Column
-$_['column_code']          = 'Код крона';
+$_['column_code']          = 'CRON Code';
 $_['column_cycle']         = 'Цикл';
 $_['column_date_added']    = 'Дата добавления';
 $_['column_date_modified'] = 'Дата изменения';
 $_['column_action']        = 'Действие';
 
-// entry
-$_['entry_cron']           = 'URL-адрес крона';
+// Entry
+$_['entry_cron']           = 'CRON URL';
 $_['entry_description']    = 'Описание';
 
 // Error
-$_['error_permission']     = 'Внимание: У вас нет прав на изменение заданий крона!';
+$_['error_permission']     = 'Warning: You do not have permission to modify CRON jobs!';
