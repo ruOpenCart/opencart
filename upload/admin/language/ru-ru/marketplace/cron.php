ad9f61f84c6680b902fa3b0b8587c2e41c48d92f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-11-09 00:36:49 +0300

    New translations cron.php (Russian)

--- upload/admin/language/ru-ru/marketplace/cron.php
+++ upload/admin/language/ru-ru/marketplace/cron.php
@@ -1,28 +1,28 @@
 <?php
 // Heading
-$_['heading_title']        = 'Задания планировщика';
+$_['heading_title']        = 'Задания CRON';
 
 // Text
-$_['text_success']         = 'Success: You have modified cron jobs!';
+$_['text_success']         = 'Успех: Вы изменили задания CRON!';
 $_['text_instruction']     = 'Инструкции по Крону';
-$_['text_list']            = 'Cron List';
-$_['text_cron_1']          = 'Cron Job\'s are scheduled tasks that are run periodically. To setup your servers to use cron job you can read the <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">opencart documentation</a> page.';
-$_['text_cron_2']          = 'You need to set your Cron task to run every hour.';
+$_['text_list']            = 'Список CRON';
+$_['text_cron_1']          = 'Задания CRON — это запланированные задачи, которые запускаются периодически. Чтобы настроить серверы для использования задания cron, вы можете прочитать страницу <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">документации опенкарт</a>.';
+$_['text_cron_2']          = 'Вам нужно настроить задачу CRON для запуска каждый час.';
 $_['text_info']            = 'Информация по Крону';
 $_['text_hour']            = 'Час';
 $_['text_day']             = 'День';
 $_['text_month']           = 'Месяц';
 
 // Column
-$_['column_code']          = 'Cron Code';
+$_['column_code']          = 'Код CRON';
 $_['column_cycle']         = 'Цикл';
 $_['column_date_added']    = 'Дата добавления';
 $_['column_date_modified'] = 'Дата изменения';
 $_['column_action']        = 'Действие';
 
-// entry
-$_['entry_cron']           = 'Cron URL';
+// Entry
+$_['entry_cron']           = 'URL-адрес CRON';
 $_['entry_description']    = 'Описание';
 
 // Error
-$_['error_permission']     = 'Внимание: У вас нет прав на изменение заданий крона!';
+$_['error_permission']     = 'Внимание: У вас нет разрешения на изменение заданий CRON!';
