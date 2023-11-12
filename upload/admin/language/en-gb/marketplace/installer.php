b6e1e9f37b76ddded51391386d75823744ce211a
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-11-12 23:06:19 +0300

    Update source file installer.php

--- upload/admin/language/en-gb/marketplace/installer.php
+++ upload/admin/language/en-gb/marketplace/installer.php
@@ -9,12 +9,6 @@ $_['text_progress']          = 'Install Progress';
 $_['text_installed']         = 'Installed Extensions';
 $_['text_info']              = 'Extension Information';
 
-$_['text_install']           = 'Refresh Vendor Files';
-$_['text_vendor']            = 'Refresh Vendor Files';
-$_['text_xml']               = 'Install OCMOD files';
-
-
-
 // Column
 $_['column_image']           = 'Image';
 $_['column_name']            = 'Extension Name';
@@ -26,7 +20,6 @@ $_['column_action']          = 'Action';
 $_['entry_progress']         = 'Progress';
 $_['entry_name']             = 'Extension Name';
 $_['entry_description']      = 'Description';
-$_['entry_code']             = 'Code';
 
 // Error
 $_['error_permission']       = 'Warning: You do not have permission to modify extensions!';
