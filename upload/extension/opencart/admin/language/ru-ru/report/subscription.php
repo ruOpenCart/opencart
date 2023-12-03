22cb1b673b1ca8cf34b65652295f8985eeae47ae
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-12-03 10:47:22 +0300

    New translations subscription.php (Russian)

--- upload/extension/opencart/admin/language/ru-ru/report/subscription.php
+++ upload/extension/opencart/admin/language/ru-ru/report/subscription.php
@@ -16,7 +16,7 @@ $_['text_all_status']           = 'Все статусы';
 // Column
 $_['column_date_start']         = 'Дата начала';
 $_['column_date_end']           = 'Дата окончания';
-$_['column_subscriptions']      = 'No. Subscriptions';
+$_['column_subscriptions']      = '№ подписки';
 $_['column_products']           = 'Кол-во товаров';
 $_['column_tax']                = 'Налог';
 $_['column_total']              = 'Всего';
