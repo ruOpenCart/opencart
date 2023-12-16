5dd3fe09c30ef05c69e8a165c867e0d7adf8913d
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-12-16 15:31:32 +0300

    New translations blog.php (Russian)

---
+++ upload/extension/opencart/admin/language/ru-ru/module/blog.php
@@ -0,0 +1,25 @@
+<?php
+// Heading
+$_['heading_title']    = 'Блог';
+
+// Text
+$_['text_extension']   = 'Расширения';
+$_['text_success']     = 'Success: You have modified blog module!';
+$_['text_next']        = 'Success: You have modified %s blog out of %s!';
+$_['text_edit']        = 'Edit Blog Module';
+
+// Column
+$_['column_total']     = 'Всего';
+
+// Entry
+$_['entry_name']       = 'Название модуля';
+$_['entry_limit']      = 'Лимит';
+$_['entry_width']      = 'Ширина изображения';
+$_['entry_height']     = 'Высота изображения';
+$_['entry_status']     = 'Статус';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify blog module!';
+$_['error_name']       = 'Название модуля должно быть от 3 до 64 символов!';
+$_['error_width']      = 'Необходимо указать ширину!';
+$_['error_height']     = 'Необходимо указать высоту!';
