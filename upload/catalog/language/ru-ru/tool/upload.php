c8de35a0e3bb9f6d4eb38c3273732bb362ccb9a6
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-12-17 15:28:02 +0300

    New translations upload.php (Russian)

--- upload/catalog/language/ru-ru/tool/upload.php
+++ upload/catalog/language/ru-ru/tool/upload.php
@@ -3,7 +3,7 @@
 $_['text_upload']     = 'Ваш файл был успешно загружен!';
 
 // Error
-$_['error_token']     = 'Warning: upload token invalid!';
+$_['error_token']     = 'Внимание: неверный токен загрузки!';
 $_['error_filename']  = 'Имя файла должно быть от 3 до 64 символов!';
 $_['error_file_type'] = 'Недопустимый тип файла!';
 $_['error_upload']    = 'Загрузка обязательна!';
