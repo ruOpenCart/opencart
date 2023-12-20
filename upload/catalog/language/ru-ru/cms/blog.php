f022965c71c4798c469d7175e68953a84fac669f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2023-12-20 12:21:48 +0300

    New translations blog.php (Russian)

--- upload/catalog/language/ru-ru/cms/blog.php
+++ upload/catalog/language/ru-ru/cms/blog.php
@@ -1,20 +1,23 @@
 <?php
 // Heading
-$_['heading_title']   = 'Блог';
+$_['heading_title'] = 'Блог';
 
 // Text
-$_['text_refine']     = 'Уточнить поиск';
-$_['text_all']        = 'Все';
-$_['text_blog']       = 'Блог';
-$_['text_by']         = 'от';
-$_['text_tags']       = 'Тэги:';
-$_['text_comment']    = 'комментарии';
-
+$_['text_refine'] = 'Уточнить поиск';
+$_['text_all'] = 'Все';
+$_['text_blog'] = 'Блог';
+$_['text_by'] = 'от';
+$_['text_tags'] = 'Тэги:';
+$_['text_comment'] = 'комментарии';
 $_['text_no_results'] = 'Нет статей.';
+$_['text_sort'] = 'Сортировать по';
+$_['text_rating_asc'] = 'Рейтинг (по убыванию)';
+$_['text_rating_desc'] = 'Рейтинг (по возрастанию)';
+$_['text_date_added_asc'] = 'Date Added (Oldest)';
+$_['text_date_added_desc'] = 'Date Added (Latest)';
 
 // Entry
-$_['entry_search']    = 'Искать';
+$_['entry_search'] = 'Искать';
 
 // Button
 $_['button_continue'] = 'Читать далее';
-$_['button_comment']  = 'Оставить комментарий';
