2906944be405fb2c105b6d90847d53535a505269
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-01-20 18:48:14 +0300

    New translations security.php (Russian)

--- upload/admin/language/ru-ru/common/security.php
+++ upload/admin/language/ru-ru/common/security.php
@@ -10,14 +10,18 @@ $_['text_storage']                = 'Каталог хранилища';
 $_['text_storage_description']    = 'Очень важно переместить каталог хранения вне веб-каталога (например, public_html, www или htdocs).';
 $_['text_storage_move']           = 'Перемещение %s в %s из %s файлов хранилища';
 $_['text_storage_success']        = 'Успех: Каталог хранилища был перемещен!';
-$_['text_storage_delete']         = 'Ваш старый каталог хранилища должен быть удален!';
-$_['text_storage_delete_success'] = 'Успех: Старый каталог хранилища был удален!';
 $_['text_admin']                  = 'Каталог администратора';
 $_['text_admin_description']      = 'Пожалуйста, введите новое имя каталога администрирования в поле ниже.';
 $_['text_admin_move']             = 'Перемещение %s в %s из %s файлов администратора';
 $_['text_admin_success']          = 'Успех: Каталог администратора был перемещен!';
-$_['text_admin_delete']           = 'Ваш старый каталог установки должен быть удален!';
-$_['text_admin_delete_success']   = 'Успех: Старый каталог администратора был удален!';
+
+
+$_['text_clear']                  = 'Your old paths need deleting!';
+$_['text_clear_description']      = 'Your old storage directory needs deleting!';
+$_['text_clear_success']          = 'Success: Old Storage directory has been deleted!';
+
+
+
 $_['text_path']                   = 'Путь';
 
 // Entry
@@ -30,14 +34,14 @@ $_['entry_name']                  = 'Название каталога';
 $_['button_move']                 = 'Переместить';
 
 // Help
-$_['help_storage']                = 'Имя каталога хранилища должно начинаться со `storage`';
+$_['help_storage']                = 'Storage directory name must begin with storage. e.g. `storage_`.';
 
 // Error
 $_['error_permission']            = 'Внимание: У вас нет прав для изменений безопасности!';
 $_['error_install']               = 'Внимание: Каталог установки не существует!';
 $_['error_storage']               = 'Внимание: Папка storage не существует!';
-$_['error_storage_name']          = 'Внимание: Наименование каталога хранилища должно начинаться со `storage`!';
+$_['error_storage_name']          = 'Warning: Storage directory name must start with `storage`! e.g. `storage_`.';
 $_['error_admin']                 = 'Внимание: Директория администратора не существует!';
-$_['error_admin_exists']          = 'Внимание: Название папки администратора уже существует!';
 $_['error_admin_name']            = 'Внимание: Имя администратора не может быть `admin`, так как оно доступно для публичного доступа!';
+$_['error_admin_exists']          = 'Внимание: Название папки администратора уже существует!';
 $_['error_writable']              = 'Внимание: config.php и admin/config.php должны быть доступны для записи!';
