335353fc114139201856ed1bc49c8dba9a8619de
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-06-25 22:15:33 +0300

    New translations step_3.php (Russian)

--- upload/install/language/ru-ru/install/step_3.php
+++ upload/install/language/ru-ru/install/step_3.php
@@ -5,34 +5,30 @@ $_['heading_title']          = 'Конфигурация';
 // Text
 $_['text_step_3']            = 'Введите ваши параметры базы данных и администратора';
 $_['text_db_connection']     = '1. Пожалуйста, введите данные для подключения к вашей базе данных.';
+$_['text_db_advanced']       = 'Advanced DB Settings';
 $_['text_db_administration'] = '2. Пожалуйста, введите имя пользователя и пароль для администрирования.';
 $_['text_mysqli']            = 'MySQLi';
 $_['text_pdo']               = 'mPDO';
-$_['text_pgsql']             = 'PostgreSQL';
 $_['text_help']              = 'Информация о настройке баз данных на различных платформах:';
 $_['text_cpanel']            = 'Настройка базы данных в CPanel';
 $_['text_plesk']             = 'Настройка базы данных в Plesk';
+$_['text_db_ssl']            = 'Certificate inputs should be added as a single line like: "MIIDrzCCApegAwIBAgIQ....=="';
 
 // Entry
 $_['entry_db_driver']        = 'Драйвер БД';
-$_['entry_db_driver_select'] = 'Выберите драйвер БД';
 $_['entry_db_hostname']      = 'Хост';
 $_['entry_db_username']      = 'Имя пользователя';
 $_['entry_db_password']      = 'Пароль';
+$_['entry_db_database']      = 'База данных';
+$_['entry_db_port']          = 'Порт';
+$_['entry_db_prefix']        = 'Префикс';
 $_['entry_db_advanced']      = 'Дополнительно';
 $_['entry_db_ssl_key']       = 'SSL-ключ';
 $_['entry_db_ssl_cert']      = 'SSL-сертификат';
 $_['entry_db_ssl_ca']        = 'Корневой SSL-сертификат (CA)';
-
-$_['entry_db_database']      = 'База данных';
-$_['entry_db_port']          = 'Порт';
-$_['entry_db_prefix']        = 'Префикс';
 $_['entry_username']         = 'Имя пользователя';
 $_['entry_password']         = 'Пароль';
 $_['entry_email']            = 'Эл. почта';
-
-// Help
-$_['entry_db_ssl_info']      = 'Входные данные сертификата следует добавлять в виде одной строки, например: "MIIDrzCCApegAwIBAgIQ....=="';
 
 // Error
 $_['error_db_driver']        = 'Драйвер базы данных обязателен!';
