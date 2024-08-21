39d2fbf4a06f8c0b586ecd23cef21933175b74e0
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-08-21 21:49:06 +0300

    New translations filter.php (Russian)

--- upload/admin/language/ru-ru/catalog/filter.php
+++ upload/admin/language/ru-ru/catalog/filter.php
@@ -10,17 +10,17 @@ $_['text_edit']           = 'Редактирование фильтра';
 
 // Column
 $_['column_name']         = 'Название фильтра';
-$_['column_filter_group'] = 'Filter Group';
+$_['column_filter_group'] = 'Группа фильтра';
 $_['column_sort_order']   = 'Порядок сортировки';
 $_['column_action']       = 'Действие';
 
 // Entry
 $_['entry_name']          = 'Название фильтра';
-$_['entry_filter_group']  = 'Filter Group';
+$_['entry_filter_group']  = 'Группа фильтра';
 $_['entry_sort_order']    = 'Порядок сортировки';
 
 // Error
 $_['error_warning']       = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
 $_['error_permission']    = 'Внимание: У вас нет прав на изменение фильтров!';
 $_['error_name']          = 'Название фильтра должно быть от 1 до 64 символов!';
-$_['error_filter_group']  = 'Filter Group required!';
+$_['error_filter_group']  = 'Необходимо указать группу фильтра!';
