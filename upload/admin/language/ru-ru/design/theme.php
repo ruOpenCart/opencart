3339e0e12cdce7e5ca5c05702b218d331877b8fd
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-08-21 21:49:09 +0300

    New translations theme.php (Russian)

--- upload/admin/language/ru-ru/design/theme.php
+++ upload/admin/language/ru-ru/design/theme.php
@@ -4,11 +4,11 @@ $_['heading_title']     = 'Редактор темы';
 
 // Text
 $_['text_success']      = 'Успех: Вы изменили темы!';
-$_['text_add']          = 'Add Theme';
+$_['text_add']          = 'Добавление темы';
 $_['text_edit']         = 'Редактирование темы';
 $_['text_default']      = 'По умолчанию';
 $_['text_extension']    = 'Расширения';
-$_['text_code']         = 'Template Code Editor';
+$_['text_code']         = 'Редактор кода шаблона';
 $_['text_twig']         = 'Редактор тем использует шаблонизатор Twig. Вы можете прочитать о <a href="https://twig.symfony.com/doc/" target="_blank" class="alert-link">синтаксисе Twig здесь</a>.';
 
 // Column
@@ -20,10 +20,10 @@ $_['column_action']     = 'Действие';
 
 // Entry
 $_['entry_store']       = 'Магазин';
-$_['entry_route']       = 'Choose Template';
+$_['entry_route']       = 'Выбор шаблона';
 $_['entry_code']        = 'Код';
 $_['entry_status']      = 'Статус';
 
 // Error
 $_['error_permission']  = 'Внимание: У вас нет прав на изменение редактора тем!';
-$_['error_file']        = 'Warning: Template file does not exist!';
+$_['error_file']        = 'Внимание: Файл шаблона не существует!';
