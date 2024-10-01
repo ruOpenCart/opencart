b3fc65a33be34f18b64f77e931960d16a9fc1c49
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-10-01 21:13:08 +0300

    New translations en-gb.php (Russian)

--- upload/install/language/ru-ru/ru-ru.php
+++ upload/install/language/ru-ru/ru-ru.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_name']       = 'Русский (Russian)';
+$_['text_name']       = 'English (United Kingdom)';
 $_['text_loading']    = 'Загрузка...';
 
 // Button
 $_['button_continue'] = 'Продолжить';
-$_['button_back']     = 'Назад';
+$_['button_back']     = 'Вернуться';
 
 // Error
 $_['error_exception'] = 'Код ошибки(%s): %s в %s строка %s';
