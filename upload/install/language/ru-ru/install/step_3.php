e07ff640e9e71536d9b046495dbac4171579d626
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-10-01 21:13:12 +0300

    New translations step_3.php (Russian)

--- upload/install/language/ru-ru/install/step_3.php
+++ upload/install/language/ru-ru/install/step_3.php
@@ -1,44 +1,44 @@
 <?php
 // Heading
-$_['heading_title']          = 'Конфигурация';
+$_['heading_title']          = 'Configuration';
 
 // Text
-$_['text_step_3']            = 'Введите ваши параметры базы данных и администратора';
-$_['text_db_connection']     = '1. Пожалуйста, введите данные для подключения к вашей базе данных.';
-$_['text_db_advanced']       = 'Расширенные настройки БД';
-$_['text_db_administration'] = '2. Пожалуйста, введите имя пользователя и пароль для администрирования.';
+$_['text_step_3']            = 'Enter your database and administration details';
+$_['text_db_connection']     = '1. Please enter your database connection details.';
+$_['text_db_advanced']       = 'Advanced DB Settings';
+$_['text_db_administration'] = '2. Please enter a username and password for the administration.';
 $_['text_mysqli']            = 'MySQLi';
 $_['text_pdo']               = 'mPDO';
-$_['text_help']              = 'Информация о настройке баз данных на различных платформах:';
-$_['text_cpanel']            = 'Настройка базы данных в CPanel';
-$_['text_plesk']             = 'Настройка базы данных в Plesk';
-$_['text_db_ssl']            = 'Входные данные сертификата следует добавлять в виде одной строки, например: "MIIDrzCCApegAwIBAgIQ....=="';
+$_['text_help']              = 'Information about setting up a databases on different platforms:';
+$_['text_cpanel']            = 'CPanel DB Setup';
+$_['text_plesk']             = 'Plesk DB Setup';
+$_['text_db_ssl']            = 'Certificate inputs should be added as a single line like: "MIIDrzCCApegAwIBAgIQ....=="';
 
 // Entry
-$_['entry_db_driver']        = 'Драйвер БД';
-$_['entry_db_hostname']      = 'Хост';
+$_['entry_db_driver']        = 'DB Driver';
+$_['entry_db_hostname']      = 'Имя хоста';
 $_['entry_db_username']      = 'Имя пользователя';
 $_['entry_db_password']      = 'Пароль';
-$_['entry_db_database']      = 'База данных';
+$_['entry_db_database']      = 'Database';
 $_['entry_db_port']          = 'Порт';
-$_['entry_db_prefix']        = 'Префикс';
-$_['entry_db_advanced']      = 'Дополнительно';
-$_['entry_db_ssl_key']       = 'SSL-ключ';
-$_['entry_db_ssl_cert']      = 'SSL-сертификат';
-$_['entry_db_ssl_ca']        = 'Корневой SSL-сертификат (CA)';
+$_['entry_db_prefix']        = 'Prefix';
+$_['entry_db_advanced']      = 'Advanced';
+$_['entry_db_ssl_key']       = 'SSL Key';
+$_['entry_db_ssl_cert']      = 'SSL Cert';
+$_['entry_db_ssl_ca']        = 'SSL CA';
 $_['entry_username']         = 'Имя пользователя';
 $_['entry_password']         = 'Пароль';
 $_['entry_email']            = 'Эл. почта';
 
 // Error
-$_['error_db_driver']        = 'Драйвер базы данных обязателен!';
-$_['error_db_hostname']      = 'Имя хоста обязательно!';
+$_['error_db_driver']        = 'Database Driver required!';
+$_['error_db_hostname']      = 'Hostname required!';
 $_['error_db_username']      = 'Имя пользователя обязательно!';
-$_['error_db_database']      = 'Имя базы данных обязательно!';
-$_['error_db_port']          = 'Порт базы данных обязателен!';
-$_['error_db_prefix']        = 'Префикс Базы Данных может содержать только строчные символы в диапазоне a-z, 0-9 и подчеркивания';
-$_['error_db_connect']       = 'Ошибка: Не удалось подключиться к Базе Данных, пожалуйста, убедитесь, что сервер базы данных, имя пользователя и пароль верны!';
+$_['error_db_database']      = 'Database Name required!';
+$_['error_db_port']          = 'Database Port required!';
+$_['error_db_prefix']        = 'DB Prefix can only contain lowercase characters in the a-z range, 0-9 and underscores';
+$_['error_db_connect']       = 'Error: Could not connect to the database please make sure the database server, username and password is correct!';
 $_['error_username']         = 'Имя пользователя обязательно!';
-$_['error_password']         = 'Пароль обязателен!';
+$_['error_password']         = 'Password required!';
 $_['error_email']            = 'Адрес электронной почты недействителен!';
-$_['error_config']           = 'Ошибка: Не удалось записать в config.php, убедитесь, что вы установили правильные разрешения для: ';
+$_['error_config']           = 'Error: Could not write to config.php please check you have set the correct permissions on: ';
