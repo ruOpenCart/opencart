92dd92494309df7b4a4655f5d763716e45b90965
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-10-01 21:06:56 +0300

    Update source file default.php

--- upload/admin/language/en-gb/default.php
+++ upload/admin/language/en-gb/default.php
@@ -146,6 +146,7 @@ $_['tab_server']                    = 'Server';
 $_['tab_seo']                       = 'SEO';
 $_['tab_store']                     = 'Store';
 $_['tab_special']                   = 'Special';
+$_['tab_session']                   = 'Session';
 $_['tab_local']                     = 'Local';
 $_['tab_mail']                      = 'Mail';
 $_['tab_payment_method']            = 'Payment Methods';
