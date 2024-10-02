3b521ac1a7feedf4d3beebe9cfd7093487e66caa
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-10-02 21:34:53 +0300

    New translations reward.php (Russian)

--- upload/extension/opencart/catalog/language/ru-ru/api/reward.php
+++ upload/extension/opencart/catalog/language/ru-ru/api/reward.php
@@ -1,11 +1,11 @@
 <?php
 // Text
-$_['text_success']   = 'Success: Your reward points discount has been applied!';
+$_['text_success']   = 'Успех: Ваша скидка за бонусные баллы была применена!';
 
 // Error
 $_['error_customer'] = 'Внимание: Детали о покупателе обязательны!';
 $_['error_product']  = 'Внимание: Товар обязателен!';
-$_['error_points']   = 'Warning: You don\'t have %s reward points!';
-$_['error_maximum']  = 'Warning: The maximum number of points that can be applied is %s!';
-$_['error_status']   = 'Warning: Reward points are not enabled on this store!';
-$_['error_confirm']  = 'Warning: Please check the reward form carefully for errors!';
+$_['error_points']   = 'Внимание: У вас нет %s бонусных баллов!';
+$_['error_maximum']  = 'Внимание: Максимальное количество баллов, которое может быть применено %s!';
+$_['error_status']   = 'Внимание: В этом магазине не действуют бонусные баллы!';
+$_['error_confirm']  = 'Внимание: Пожалуйста, проверьте форму бонусов на наличие ошибок!';
