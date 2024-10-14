1d4be9f440881c7996a1055bec1366a23d8fcf98
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-10-14 22:11:31 +0300

    New translations confirm.php (Russian)

--- upload/catalog/language/ru-ru/checkout/confirm.php
+++ upload/catalog/language/ru-ru/checkout/confirm.php
@@ -2,14 +2,6 @@
 // Text
 $_['text_points']                = 'Бонусные баллы';
 $_['text_subscription']          = 'Подписка';
-$_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
-$_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
-$_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
-$_['text_day']                   = 'день';
-$_['text_week']                  = 'неделя';
-$_['text_semi_month']            = 'полмесяца';
-$_['text_month']                 = 'месяц';
-$_['text_year']                  = 'год';
 
 // Column
 $_['column_name']                = 'Название товара';
