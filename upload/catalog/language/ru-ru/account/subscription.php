2b800c3479e4106a5e22a27c346835c855d69054
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-11-04 18:53:49 +0300

    New translations subscription.php (Russian)

--- upload/catalog/language/ru-ru/account/subscription.php
+++ upload/catalog/language/ru-ru/account/subscription.php
@@ -1,43 +1,51 @@
 <?php
 // Heading
-$_['heading_title']               = 'Подписки';
+$_['heading_title']              = 'Подписки';
 
 // Text
-$_['text_account']                = 'Аккаунт';
-$_['text_subscription']           = 'Подписка #%s';
-$_['text_invoice_no']             = 'Счет №';
-$_['text_subscription_id']        = 'Идентификатор подписки';
-$_['text_order_id']               = 'Номер заказа';
-$_['text_shipping_address']       = 'Адрес доставки';
-$_['text_shipping_method']        = 'Способ доставки';
-$_['text_payment_address']        = 'Платежный адрес';
-$_['text_payment_method']         = 'Метод оплаты';
-$_['text_subscription_trial']     = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
-$_['text_subscription_duration']  = '%s каждые %d %s для %d платеж(а)(ей)';
-$_['text_subscription_cancel']    = '%s каждые %d %s до отмены';
-$_['text_day']                    = 'день';
-$_['text_week']                   = 'неделя';
-$_['text_semi_month']             = 'полмесяца';
-$_['text_month']                  = 'месяц';
-$_['text_year']                   = 'год';
-$_['text_date_added']             = 'Дата добавления';
-$_['text_status']                 = 'Статус подписки';
-$_['text_description']            = 'Описание';
-$_['text_quantity']               = 'Количество';
-$_['text_order']                  = 'История заказа';
-$_['text_no_results']             = 'Подписки не найдены!';
-$_['text_error']                  = 'Запрошенный вами заказ подписки не найден!';
-$_['text_cancelled']              = 'Повторяющийся платеж был отменен';
+$_['text_account']               = 'Аккаунт';
+$_['text_subscription']          = 'Подписка #%s';
+$_['text_subscription_id']       = 'Идентификатор подписки';
+$_['text_order_id']              = 'Номер заказа';
+$_['text_shipping_address']      = 'Адрес доставки';
+$_['text_shipping_method']       = 'Способ доставки';
+$_['text_payment_address']       = 'Платежный адрес';
+$_['text_payment_method']        = 'Метод оплаты';
+$_['text_model']                 = 'Модель';
+$_['text_subscription_plan']     = 'План подписки';
+$_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
+$_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
+$_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
+$_['text_day']                   = 'день';
+$_['text_week']                  = 'неделя';
+$_['text_semi_month']            = 'полмесяца';
+$_['text_month']                 = 'месяц';
+$_['text_year']                  = 'год';
+$_['text_quantity']              = 'Количество';
+$_['text_date_next']             = 'Дате Следующая';
+$_['text_remaining']             = 'Осталось';
+$_['text_history']               = 'Subscription History';
+$_['text_order']                 = 'История заказа';
+$_['text_no_subscription']       = 'No subscriptions found!';
 
 // Column
-$_['column_subscription_id']      = 'Идентификатор подписки';
-$_['column_product']              = 'Товар';
-$_['column_order_id']             = 'Номер заказа';
-$_['column_status']               = 'Статус';
-$_['column_total']                = 'Всего';
-$_['column_comment']              = 'Комментарий';
-$_['column_date_added']           = 'Дата добавления';
+$_['column_subscription_id']     = 'Идентификатор подписки';
+$_['column_product']             = 'Товар';
+$_['column_product_total']       = 'No. of Products';
+$_['column_description']         = 'Описание';
+$_['column_order_id']            = 'Номер заказа';
+$_['column_status']              = 'Статус';
+$_['column_quantity']            = 'Количество';
+$_['column_trial_price']         = 'Пробная цена';
+$_['column_price']               = 'Цена';
+$_['column_total']               = 'Всего';
+$_['column_comment']             = 'Комментарий';
+$_['column_date_added']          = 'Дата добавления';
+
+// Button
+$_['button_cancel']              = 'Отменить подписку';
 
 // Error
-$_['error_not_cancelled']         = 'Ошибка: %s';
-$_['error_not_found']             = 'Невозможно отменить подписку';
+$_['error_subscription']         = 'Subscription could not be found!';
+$_['error_duration']             = 'Subscription can not be canceled until %s more orders!';
+$_['error_cancel']               = 'Could not cancel subscription';
