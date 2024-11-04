5d2ba39af920d718c394223268af3351187227ea
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-11-04 18:50:36 +0300

    Update source file cart.php

--- upload/catalog/language/en-gb/checkout/cart.php
+++ upload/catalog/language/en-gb/checkout/cart.php
@@ -8,7 +8,9 @@ $_['text_edit']                  = 'Success: You have modified your shopping car
 $_['text_remove']                = 'Success: You have removed an item from your shopping cart!';
 $_['text_login']                 = 'Attention: You must <a href="%s">login</a> or <a href="%s">create an account</a> to view prices!';
 $_['text_no_results']            = 'Your shopping cart is empty!';
-$_['text_model']                 = 'Model';
+$_['text_next']                  = 'What would you like to do next?';
+$_['text_next_choice']           = 'Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.';
+$_['text_points']                = 'Reward Points';
 $_['text_subscription']          = 'Subscription';
 $_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
 $_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
@@ -18,13 +20,11 @@ $_['text_week']                  = 'week';
 $_['text_semi_month']            = 'half-month';
 $_['text_month']                 = 'month';
 $_['text_year']                  = 'year';
-$_['text_points']                = 'Reward Points';
-$_['text_next']                  = 'What would you like to do next?';
-$_['text_next_choice']           = 'Choose if you have a discount code or reward points you want to use or would like to estimate your delivery cost.';
 
 // Column
 $_['column_image']               = 'Image';
-$_['column_name']                = 'Product';
+$_['column_name']                = 'Product Name';
+$_['column_model']               = 'Model';
 $_['column_quantity']            = 'Quantity';
 $_['column_price']               = 'Unit Price';
 $_['column_total']               = 'Total';
@@ -34,4 +34,4 @@ $_['error_stock']                = 'Products marked with *** are not available i
 $_['error_minimum']              = 'Minimum order amount is %s!';
 $_['error_required']             = '%s required!';
 $_['error_product']              = 'Warning: Product could not be found!';
-$_['error_subscription']         = 'Subscription Plan is not valid!';
+$_['error_subscription']         = 'Please select a subscription plan!';
