db5c04a0cdde43e234239b638833c8550f190bb2
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-11-05 20:10:55 +0300

    Update source file order.php

--- upload/catalog/language/en-gb/account/order.php
+++ upload/catalog/language/en-gb/account/order.php
@@ -5,14 +5,15 @@ $_['heading_title']              = 'Orders';
 // Text
 $_['text_account']               = 'Account';
 $_['text_order']                 = 'Order #%s';
-$_['text_invoice_no']            = 'Invoice No.';
 $_['text_order_id']              = 'Order ID';
-$_['text_order_status']          = 'Order Status';
-$_['text_shipping_method']       = 'Shipping Method';
-$_['text_payment_method']        = 'Payment Method';
-$_['text_date_added']            = 'Date Added';
+$_['text_invoice_no']            = 'Invoice No.';
+$_['text_tbc']                   = 'TBC';
 $_['text_shipping_address']      = 'Shipping Address';
 $_['text_payment_address']       = 'Payment Address';
+$_['text_shipping_method']       = 'Shipping Method';
+$_['text_payment_method']        = 'Payment Method';
+$_['text_model']                 = 'Model';
+$_['text_points']                = 'Reward Points';
 $_['text_subscription']          = 'Subscription';
 $_['text_subscription_trial']    = '%s every %d %s(s) for %d payment(s) then ';
 $_['text_subscription_duration'] = '%s every %d %s(s) for %d payment(s)';
@@ -22,7 +23,9 @@ $_['text_week']                  = 'week';
 $_['text_semi_month']            = 'half-month';
 $_['text_month']                 = 'month';
 $_['text_year']                  = 'year';
-$_['text_comment']               = 'Order Comments';
+$_['text_date_next']             = 'Date Next';
+$_['text_remaining']             = 'Remaining';
+$_['text_comment']               = 'Note';
 $_['text_history']               = 'Order History';
 $_['text_success']               = 'Success: You have added <a href="%s">%s</a> to your <a href="%s">shopping cart</a>!';
 $_['text_no_results']            = 'You have not made any previous orders!';
@@ -31,16 +34,15 @@ $_['text_error']                 = 'The order you requested could not be found!'
 // Column
 $_['column_order_id']            = 'Order ID';
 $_['column_customer']            = 'Customer';
-$_['column_product']             = 'No. of Products';
-$_['column_name']                = 'Product Name';
-$_['column_model']               = 'Model';
+$_['column_product']             = 'Product';
+$_['column_product_total']       = 'No. of Products';
+$_['column_status']              = 'Status';
+$_['column_total']               = 'Total';
+$_['column_date_added']          = 'Date Added';
 $_['column_quantity']            = 'Quantity';
 $_['column_price']               = 'Price';
-$_['column_total']               = 'Total';
-$_['column_action']              = 'Action';
-$_['column_status']              = 'Status';
 $_['column_comment']             = 'Comment';
-$_['column_date_added']          = 'Date Added';
+$_['column_action']              = 'Action';
 
 // Error
-$_['error_reorder']              = '%s is not currently available to be reordered.';
+$_['error_reorder']              = 'The product you are trying to purchase again has some issues. Please check the error messages shown below.';
