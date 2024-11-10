7f28abefcc180075ca0468d1d50c0a059e0a11d8
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-11-10 21:09:02 +0300

    Update source file subscription.php

--- upload/catalog/language/en-gb/account/subscription.php
+++ upload/catalog/language/en-gb/account/subscription.php
@@ -4,7 +4,6 @@ $_['heading_title']              = 'Subscriptions';
 
 // Text
 $_['text_account']               = 'Account';
-$_['text_success']               = 'Success: Your subscription has successfully been canceled!';
 $_['text_subscription']          = 'Subscription #%s';
 $_['text_subscription_id']       = 'Subscription ID';
 $_['text_order_id']              = 'Order ID';
@@ -49,4 +48,4 @@ $_['button_cancel']              = 'Cancel Subscription';
 // Error
 $_['error_subscription']         = 'Subscription could not be found!';
 $_['error_duration']             = 'Subscription can not be canceled until %s more orders!';
-$_['error_canceled']             = 'Subscription already canceled!';
+$_['error_cancel']               = 'Could not cancel subscription';
