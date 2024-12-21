edb882981c9e6843083b0495f7960b32d5bc07c1
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-12-21 22:13:41 +0300

    New translations subscription.php (Russian)

--- upload/catalog/language/ru-ru/cron/subscription.php
+++ upload/catalog/language/ru-ru/cron/subscription.php
@@ -1,14 +1,18 @@
 <?php
 // Text
 $_['text_success']           = 'Успех: Подписка успешно продлена!';
+$_['text_log']               = 'Check the subscription log for more information.';
 
 // Error
-$_['error_language']         = 'Внимание: Не удалось найти расширение метода оплаты!';
-$_['error_customer']         = 'Внимание: Не удалось найти расширение метода оплаты!';
-$_['error_product']          = 'Внимание: Не удалось найти расширение метода оплаты!';
-$_['error_shipping_address'] = 'Внимание: Не удалось найти расширение метода оплаты!';
-$_['error_shipping_method']  = 'Внимание: Метод доставки %s не найден!';
-$_['error_payment_address']  = 'Внимание: Не удалось найти расширение метода оплаты!';
-$_['error_payment_method']   = 'Внимание: Метод оплаты %s не найден!';
+$_['error_store'] 		     = 'Warning: Store not found!';
+$_['error_language']         = 'Внимание: Язык не найден!';
+$_['error_currency']         = 'Warning: Currency could not be found!';
+$_['error_customer']         = 'Внимание: Покупатель не найден!';
+$_['error_payment_address']  = 'Warning: Payment address could not be found!';
+$_['error_shipping_address'] = 'Warning: Shipping address could not be found!';
+$_['error_product']          = 'Warning: Product %s could not be found!';
+$_['error_option']           = 'Warning: Option %s could not be found!';
+$_['error_shipping_method']  = 'Внимание: Способ доставки обязателен!';
+$_['error_payment_method']   = 'Внимание: Способ оплаты обязателен!';
 $_['error_payment_failed']   = 'Внимание: Невозможно обработать платеж!';
 $_['error_extension']        = 'Внимание: Не удалось найти расширение метода оплаты!';
