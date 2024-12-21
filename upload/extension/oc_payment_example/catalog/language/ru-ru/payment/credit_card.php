98f9a0bc015f46a327284ffb8513fd42b6745f61
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-12-21 22:13:45 +0300

    New translations credit_card.php (Russian)

--- upload/extension/oc_payment_example/catalog/language/ru-ru/payment/credit_card.php
+++ upload/extension/oc_payment_example/catalog/language/ru-ru/payment/credit_card.php
@@ -7,12 +7,15 @@ $_['text_title']         = 'Пожалуйста, введите данные в
 $_['text_description']   = 'Этот способ оплаты является фиктивным и не хранит информацию о кредитной карте, а также не должен использоваться на действующем сайте.';
 $_['text_card_use']      = 'Использовать кредитную карту';
 $_['text_card_new']      = 'Использовать новую кредитную карту';
+$_['text_visa']          = 'Visa';
+$_['text_mastercard']    = 'Mastercard';
 $_['text_month']         = 'Месяцы';
 $_['text_year']          = 'Годы';
 $_['text_store']         = 'Запомнить мою карту';
 
 // Entry
 $_['entry_card_name']    = 'Имя владельца карты';
+$_['entry_card_type']    = 'Card Type';
 $_['entry_card_number']  = 'Номер кредитной карты';
 $_['entry_card_expire']  = 'Дата истечения';
 $_['entry_card_cvv']     = 'CVV-код';
@@ -23,6 +26,7 @@ $_['help_name']          = 'Имя владельца карты';
 // Error
 $_['error_credit_card']  = 'Ошибка: Номер кредитной карты обязателен!';
 $_['error_card_name']    = 'Ошибка: Имя владельца карты обязательно!';
+$_['error_card_type']    = 'Error: Card type needs to be VISA or Mastercard!';
 $_['error_card_number']  = 'Ошибка: Номер кредитной карты обязателен!';
 $_['error_card_expire']  = 'Ошибка: Дата истечения срока действия обязательна!';
 $_['error_card_expired'] = 'Ошибка: Срок действия кредитной карты истек!';
