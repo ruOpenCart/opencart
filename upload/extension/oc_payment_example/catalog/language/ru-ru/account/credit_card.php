b2e7aab77ee87ca9239e3194a29bcceedf0c95ee
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2024-12-22 22:08:07 +0300

    New translations credit_card.php (Russian)

--- upload/extension/oc_payment_example/catalog/language/ru-ru/account/credit_card.php
+++ upload/extension/oc_payment_example/catalog/language/ru-ru/account/credit_card.php
@@ -1,13 +1,13 @@
 <?php
 // Heading
-$_['heading_title']        = 'Credit Cards';
+$_['heading_title']        = 'Кредитные карты';
 
 // Text
-$_['text_success']         = 'Your credit card details have been successfully saved!';
+$_['text_success']         = 'Данные вашей кредитной карты были успешно сохранены!';
 $_['text_description']     = 'Этот способ оплаты является фиктивным и не хранит информацию о кредитной карте, а также не должен использоваться на действующем сайте.';
 $_['text_delete']          = 'Ваш способ оплаты был успешно удален';
-$_['text_no_results']      = 'You have no credit cards stored in your account.';
-$_['text_credit_card_add'] = 'Add Credit Card';
+$_['text_no_results']      = 'У вас нет кредитных карт сохраненных в вашем аккаунте.';
+$_['text_credit_card_add'] = 'Добавить кредитную карту';
 $_['text_visa']            = 'VISA';
 $_['text_mastercard']      = 'Mastercard';
 $_['text_confirm']         = 'Вы уверены?';
@@ -20,19 +20,19 @@ $_['column_action']        = 'Действие';
 
 // Entry
 $_['entry_card_name']      = 'Имя владельца карты';
-$_['entry_card_type']      = 'Card Type';
+$_['entry_card_type']      = 'Тип карты';
 $_['entry_card_number']    = 'Номер кредитной карты';
 $_['entry_card_expire']    = 'Дата истечения';
 $_['entry_card_cvv']       = 'CVV-код';
 
 // Button
-$_['button_add'] 		   = 'Add Credit Card';
+$_['button_add'] 		   = 'Добавить кредитную карту';
 $_['button_save'] 		   = 'Сохранить';
 
 // Error
-$_['error_credit_card']    = 'Warning: Credit Card could not be found!';
+$_['error_credit_card']    = 'Внимание: Кредитная карта не найдена!';
 $_['error_card_name']      = 'Ошибка: Имя владельца карты обязательно!';
-$_['error_card_type']      = 'Error: Card type needs to be VISA or Mastercard!';
+$_['error_card_type']      = 'Ошибка: Тип карты должен быть VISA или Mastercard!';
 $_['error_card_number']    = 'Ошибка: Номер кредитной карты обязателен!';
 $_['error_card_expire']    = 'Ошибка: Дата истечения срока действия обязательна!';
 $_['error_card_expired']   = 'Ошибка: Срок действия кредитной карты истек!';
