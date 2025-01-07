a569177ef3d518778eb11d6592cbe64b3f123eac
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-01-07 12:39:01 +0300

    New translations returns.php (Russian)

--- upload/admin/language/ru-ru/sale/returns.php
+++ upload/admin/language/ru-ru/sale/returns.php
@@ -1,65 +1,72 @@
 <?php
 // Heading
-$_['heading_title']        = 'Возврат товара';
+$_['heading_title']      = 'Возвраты';
 
 // Text
-$_['text_success']         = 'Успех: Вы изменили возвраты!';
-$_['text_list']            = 'Список возвратов товара';
-$_['text_add']             = 'Добавление возврата товара';
-$_['text_edit']            = 'Редактирование возврата товара';
-$_['text_opened']          = 'Открыто';
-$_['text_unopened']        = 'Не открыто';
-$_['text_order']           = 'Информация о заказе';
-$_['text_product']         = 'Информация о товаре и причина возврата';
-$_['text_history']         = 'История';
-$_['text_history_add']     = 'Добавление истории';
-$_['text_filter']          = 'Фильтр';
+$_['text_success']       = 'Успех: Вы изменили возвраты!';
+$_['text_list']          = 'Return List';
+$_['text_add']           = 'Add Return';
+$_['text_edit']          = 'Return (#%s)';
+$_['text_order_id']      = 'Номер заказа';
+$_['text_filter']        = 'Фильтр';
+$_['text_customer']      = 'Покупатель';
+$_['text_date_added']    = 'Дата добавления';
+$_['text_return']        = 'Вернуть товар';
+$_['text_product']       = 'Choose Product to return';
+$_['text_opened']        = 'Открыто';
+$_['text_unopened']      = 'Не открыто';
+$_['text_history']       = 'История';
+$_['text_history_add']   = 'Добавление истории';
 
 // Column
-$_['column_return_id']     = 'Идентификатор возврата';
-$_['column_order_id']      = 'Номер заказа';
-$_['column_customer']      = 'Покупатель';
-$_['column_product']       = 'Товар';
-$_['column_model']         = 'Модель';
-$_['column_status']        = 'Статус';
-$_['column_date_added']    = 'Дата добавления';
-$_['column_date_modified'] = 'Дата изменения';
-$_['column_comment']       = 'Комментарий';
-$_['column_notify']        = 'Покупатель уведомлен';
-$_['column_action']        = 'Действие';
+$_['column_return_id']   = 'Идентификатор возврата';
+$_['column_order_id']    = 'Номер заказа';
+$_['column_customer']    = 'Покупатель';
+$_['column_product']     = 'Товар';
+$_['column_model']       = 'Модель';
+$_['column_quantity']    = 'Количество';
+$_['column_status']      = 'Статус';
+$_['column_date_added']  = 'Дата добавления';
+$_['column_comment']     = 'Комментарий';
+$_['column_notify']      = 'Покупатель уведомлен';
+$_['column_action']      = 'Действие';
 
 // Entry
-$_['entry_customer']       = 'Покупатель';
-$_['entry_order_id']       = 'Номер заказа';
-$_['entry_date_ordered']   = 'Дата заказа';
-$_['entry_firstname']      = 'Имя';
-$_['entry_lastname']       = 'Фамилия';
-$_['entry_email']          = 'Эл. почта';
-$_['entry_telephone']      = 'Телефон';
-$_['entry_product']        = 'Товар';
-$_['entry_model']          = 'Модель';
-$_['entry_quantity']       = 'Количество';
-$_['entry_opened']         = 'Открыто';
-$_['entry_comment']        = 'Комментарий';
-$_['entry_return_reason']  = 'Причина возврата';
-$_['entry_return_action']  = 'Действие при возврате';
-$_['entry_return_status']  = 'Статус возврата';
-$_['entry_notify']         = 'Уведомить покупателя';
-$_['entry_return_id']      = 'Идентификатор возврата';
-$_['entry_date_from']      = 'Дата от';
-$_['entry_date_to']        = 'Дата до';
+$_['entry_customer']     = 'Покупатель';
+$_['entry_order_id']     = 'Номер заказа';
+$_['entry_date_ordered'] = 'Дата заказа';
+$_['entry_firstname']    = 'Имя';
+$_['entry_lastname']     = 'Фамилия';
+$_['entry_email']        = 'Эл. почта';
+$_['entry_telephone']    = 'Телефон';
+$_['entry_product']      = 'Товар';
+$_['entry_model']        = 'Модель';
+$_['entry_quantity']     = 'Количество';
+$_['entry_opened']       = 'Открыто';
+$_['entry_comment']      = 'Комментарий';
+$_['entry_reason']       = 'Return Reason';
+$_['entry_action']       = 'Return Action';
+$_['entry_status']       = 'Return Status';
+$_['entry_notify']       = 'Уведомить покупателя';
+$_['entry_return_id']    = 'Идентификатор возврата';
+$_['entry_date_from']    = 'Дата от';
+$_['entry_date_to']      = 'Дата до';
 
 // Help
-$_['help_product']         = '(Автодополнение)';
+$_['help_product']       = '(Автодополнение)';
 
 // Error
-$_['error_warning']        = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
-$_['error_permission']     = 'Внимание: У вас нет прав на изменение возвратов!';
-$_['error_return']         = 'Внимание: Не удалось найти выбранный возврат!';
-$_['error_order_id']       = 'Необходимо указать номер заказа!';
-$_['error_firstname']      = 'Имя должно быть от 1 до 32-х символов!';
-$_['error_lastname']       = 'Фамилия должна быть от 1 до 32 символов!';
-$_['error_email']          = 'Адрес электронной почты недействителен!';
-$_['error_telephone']      = 'Телефон должен быть от 3 до 32 символов!';
-$_['error_product']        = 'Название товара должно быть больше 1 и меньше 255 символов!';
-$_['error_model']          = 'Модель продукта должна быть больше 1 и меньше, чем 64 символов!';
+$_['error_warning']      = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
+$_['error_permission']   = 'Внимание: У вас нет прав на изменение возвратов!';
+$_['error_order']        = 'Order not found!';
+$_['error_customer']     = 'Customer not found!';
+$_['error_firstname']    = 'Имя должно быть от 1 до 32-х символов!';
+$_['error_lastname']     = 'Фамилия должна быть от 1 до 32 символов!';
+$_['error_email']        = 'Адрес электронной почты недействителен!';
+$_['error_telephone']    = 'Телефон должен быть от 3 до 32 символов!';
+$_['error_product']      = 'Товар не найден!';
+$_['error_name']         = 'Название товара должно быть больше 1 и меньше 255 символов!';
+$_['error_model']        = 'Модель продукта должна быть больше 1 и меньше, чем 64 символов!';
+$_['error_quantity']     = 'Return quantity must be at least 1!';
+$_['error_reason']       = 'You must select a return reason!';
+$_['error_action']       = 'You must select a return action!';
