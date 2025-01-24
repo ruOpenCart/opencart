1e91fdf66af2acbea68c69da6cb1c8ea4ce86bc2
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-01-24 14:33:42 +0300

    New translations password.php (Russian)

--- upload/catalog/language/ru-ru/account/password.php
+++ upload/catalog/language/ru-ru/account/password.php
@@ -1,17 +1,22 @@
 <?php
 // Heading
-$_['heading_title']  = 'Изменение пароля';
+$_['heading_title']            = 'Изменение пароля';
 
 // Text
-$_['text_account']   = 'Аккаунт';
-$_['text_password']  = 'Ваш пароль';
-$_['text_success']   = 'Успех: Ваш пароль был успешно обновлен!';
+$_['text_account']             = 'Аккаунт';
+$_['text_password']            = 'Ваш пароль';
+$_['text_success']             = 'Успех: Ваш пароль был успешно обновлен!';
 
 // Entry
-$_['entry_password'] = 'Пароль';
-$_['entry_confirm']  = 'Подтверждение пароля';
+$_['entry_password']           = 'Пароль';
+$_['entry_confirm']            = 'Подтверждение пароля';
 
 // Error
-$_['error_token']    = 'Внимание: Токен пароля недействителен!';
-$_['error_password'] = 'Пароль должен быть от 6 до 20 символов!';
-$_['error_confirm']  = 'Подтверждение пароля не совпадает с паролем!';
+$_['error_token']              = 'Внимание: Токен пароля недействителен!';
+$_['error_password']           = 'Password must contain a %s and be between %d and 20 characters!';
+$_['error_password_uppercase'] = 'uppercase';
+$_['error_password_lowercase'] = 'lowercase';
+$_['error_password_number']    = 'number';
+$_['error_password_symbol']    = 'symbol';
+$_['error_password_length']    = 'Password must be between %d and 20 characters!';
+$_['error_confirm']            = 'Подтверждение пароля не совпадает с паролем!';
