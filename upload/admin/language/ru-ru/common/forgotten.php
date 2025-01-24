87e4850d8a16b277fe9b6674dfe3d21472626bf6
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-01-24 14:33:33 +0300

    New translations forgotten.php (Russian)

--- upload/admin/language/ru-ru/common/forgotten.php
+++ upload/admin/language/ru-ru/common/forgotten.php
@@ -1,22 +1,28 @@
 <?php
 // Heading
-$_['heading_title']  = 'Забыли пароль?';
-$_['heading_reset']  = 'Сброс пароля';
+$_['heading_title']            = 'Забыли пароль?';
+$_['heading_reset']            = 'Сброс пароля';
 
 // Text
-$_['text_email']     = 'Введите адрес электронной почты вашей учетной записи. Нажмите кнопку Продолжить, чтобы получить ссылку для сброса пароля по электронной почте.';
-$_['text_success']   = 'Success: Your password has been successfully updated.';
-$_['text_password']  = 'Введите новый пароль, который вы хотите использовать.';
-$_['text_reset']     = 'Успех: Ваш пароль был успешно обновлен!';
+$_['text_email']               = 'Введите адрес электронной почты вашей учетной записи. Нажмите кнопку Продолжить, чтобы получить ссылку для сброса пароля по электронной почте.';
+$_['text_success']             = 'Success: Your password has been successfully updated.';
+$_['text_password']            = 'Введите новый пароль, который вы хотите использовать.';
+$_['text_reset']               = 'Успех: Ваш пароль был успешно обновлен!';
 
 // Entry
-$_['entry_password'] = 'Пароль';
-$_['entry_confirm']  = 'Подтвердить';
-$_['entry_email']    = 'Адрес электронной почты';
+$_['entry_password']           = 'Пароль';
+$_['entry_confirm']            = 'Подтвердить';
+$_['entry_email']              = 'Адрес электронной почты';
 
 // Error
-$_['error_email']    = 'Внимание: Адрес электронной почты не найден в наших записях!';
-$_['error_code']     = 'Код сброса не совпадает!';
-$_['error_disabled'] = 'Сброс пароля был отключен!';
-$_['error_password'] = 'Пароль должен быть от 6 до 20 символов!';
-$_['error_confirm']  = 'Пароль и подтверждение пароля не совпадают!';
+$_['error_email']              = 'Внимание: Адрес электронной почты не найден в наших записях!';
+$_['error_code']               = 'Код сброса не совпадает!';
+$_['error_disabled']           = 'Сброс пароля был отключен!';
+$_['error_password']           = 'Password must contain %s and be between %d and 20 characters!';
+$_['error_password_uppercase'] = 'uppercase';
+$_['error_password_lowercase'] = 'lowercase';
+$_['error_password_number']    = 'number';
+$_['error_password_symbol']    = 'symbol';
+$_['error_password_length']    = 'Password must be between %d and 40 characters!';
+$_['error_confirm']            = 'Пароль и подтверждение пароля не совпадают!';
+
