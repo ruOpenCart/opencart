f8d3ce9342069fdce531b5a2efedfe51e116a89c
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-01-24 14:33:39 +0300

    New translations user.php (Russian)

--- upload/admin/language/ru-ru/user/user.php
+++ upload/admin/language/ru-ru/user/user.php
@@ -1,58 +1,63 @@
 <?php
 // Heading
-$_['heading_title']         = 'Пользователи';
+$_['heading_title']            = 'Пользователи';
 
 // Text
-$_['text_success']          = 'Успех: Вы изменили пользователей!';
-$_['text_list']             = 'Список пользователей';
-$_['text_add']              = 'Добавление пользователя';
-$_['text_edit']             = 'Редактирование пользователя';
-$_['text_user']             = 'Сведения о пользователях';
-$_['text_password']         = 'Пароль';
-$_['text_other']            = 'Другие';
-$_['text_login']            = 'История входов в систему';
-$_['text_authorize']        = 'История авторизации';
-$_['text_filter']           = 'Фильтр';
+$_['text_success']             = 'Успех: Вы изменили пользователей!';
+$_['text_list']                = 'Список пользователей';
+$_['text_add']                 = 'Добавление пользователя';
+$_['text_edit']                = 'Редактирование пользователя';
+$_['text_user']                = 'Сведения о пользователях';
+$_['text_password']            = 'Пароль';
+$_['text_other']               = 'Другие';
+$_['text_login']               = 'История входов в систему';
+$_['text_authorize']           = 'История авторизации';
+$_['text_filter']              = 'Фильтр';
 
 // Column
-$_['column_username']       = 'Имя пользователя';
-$_['column_name']           = 'Название';
-$_['column_email']          = 'Эл. почта';
-$_['column_user_group']     = 'Группа пользователя';
-$_['column_status']         = 'Статус';
-$_['column_ip']             = 'IP-адрес';
-$_['column_user_agent']     = 'Пользовательский Агент';
-$_['column_date_added']     = 'Дата добавления';
-$_['column_action']         = 'Действие';
+$_['column_username']          = 'Имя пользователя';
+$_['column_name']              = 'Название';
+$_['column_email']             = 'Эл. почта';
+$_['column_user_group']        = 'Группа пользователя';
+$_['column_status']            = 'Статус';
+$_['column_ip']                = 'IP-адрес';
+$_['column_user_agent']        = 'Пользовательский Агент';
+$_['column_date_added']        = 'Дата добавления';
+$_['column_action']            = 'Действие';
 
 // Entry
-$_['entry_username']        = 'Имя пользователя';
-$_['entry_user_group']      = 'Группа пользователя';
-$_['entry_password']        = 'Пароль';
-$_['entry_confirm']         = 'Подтвердить';
-$_['entry_firstname']       = 'Имя';
-$_['entry_lastname']        = 'Фамилия';
-$_['entry_email']           = 'Эл. почта';
-$_['entry_image']           = 'Изображение';
-$_['entry_status']          = 'Статус';
-$_['entry_name']            = 'Название';
-$_['entry_ip']              = 'IP-адрес';
-$_['entry_date_from']       = 'Дата от';
-$_['entry_date_to']         = 'Дата до';
+$_['entry_username']           = 'Имя пользователя';
+$_['entry_user_group']         = 'Группа пользователя';
+$_['entry_password']           = 'Пароль';
+$_['entry_confirm']            = 'Подтвердить';
+$_['entry_firstname']          = 'Имя';
+$_['entry_lastname']           = 'Фамилия';
+$_['entry_email']              = 'Эл. почта';
+$_['entry_image']              = 'Изображение';
+$_['entry_status']             = 'Статус';
+$_['entry_name']               = 'Название';
+$_['entry_ip']                 = 'IP-адрес';
+$_['entry_date_from']          = 'Дата от';
+$_['entry_date_to']            = 'Дата до';
 
 // Tabs
-$_['tab_authorize']         = 'Авторизация';
-$_['tab_login']             = 'Войти';
+$_['tab_authorize']            = 'Авторизация';
+$_['tab_login']                = 'Войти';
 
 // Error
-$_['error_permission']      = 'Внимание: У вас нет разрешения на изменение пользователей!';
-$_['error_account']         = 'Внимание: Вы не можете удалить свой собственный аккаунт!';
-$_['error_login']           = 'Внимание: Логин пользователя не существует!';
-$_['error_username']        = 'Имя пользователя должно быть от 3 до 20 знаков!';
-$_['error_username_exists'] = 'Внимание: Имя пользователя уже используется!';
-$_['error_firstname']       = 'Имя должно быть от 1 до 32-х символов!';
-$_['error_lastname']        = 'Фамилия должна быть от 1 до 32 символов!';
-$_['error_email']           = 'Адрес электронной почты недействителен!';
-$_['error_email_exists']    = 'Внимание: Адрес эл. почты уже зарегистрирован!';
-$_['error_password']        = 'Пароль должен быть от 6 до 20 символов!';
-$_['error_confirm']         = 'Пароль и подтверждение пароля не совпадают!';
+$_['error_permission']         = 'Внимание: У вас нет разрешения на изменение пользователей!';
+$_['error_account']            = 'Внимание: Вы не можете удалить свой собственный аккаунт!';
+$_['error_login']              = 'Внимание: Логин пользователя не существует!';
+$_['error_username']           = 'Имя пользователя должно быть от 3 до 20 знаков!';
+$_['error_username_exists']    = 'Внимание: Имя пользователя уже используется!';
+$_['error_firstname']          = 'Имя должно быть от 1 до 32-х символов!';
+$_['error_lastname']           = 'Фамилия должна быть от 1 до 32 символов!';
+$_['error_email']              = 'Адрес электронной почты недействителен!';
+$_['error_email_exists']       = 'Внимание: Адрес эл. почты уже зарегистрирован!';
+$_['error_password']           = 'Password must contain a %s and be between %d and 20 characters!';
+$_['error_password_uppercase'] = 'uppercase';
+$_['error_password_lowercase'] = 'lowercase';
+$_['error_password_number']    = 'number';
+$_['error_password_symbol']    = 'symbol';
+$_['error_password_length']    = 'Password must be between %d and 20 characters!';
+$_['error_confirm']            = 'Пароль и подтверждение пароля не совпадают!';
