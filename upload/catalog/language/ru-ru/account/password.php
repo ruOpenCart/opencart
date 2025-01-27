9748a057a1b6afdc48b48c99d67f14d1807d2055
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-01-27 22:57:06 +0300

    New translations password.php (Russian)

--- upload/catalog/language/ru-ru/account/password.php
+++ upload/catalog/language/ru-ru/account/password.php
@@ -13,10 +13,10 @@ $_['entry_confirm']            = 'Подтверждение пароля';
 
 // Error
 $_['error_token']              = 'Внимание: Токен пароля недействителен!';
-$_['error_password']           = 'Password must contain a %s and be between %d and 20 characters!';
-$_['error_password_uppercase'] = 'uppercase';
-$_['error_password_lowercase'] = 'lowercase';
-$_['error_password_number']    = 'number';
-$_['error_password_symbol']    = 'symbol';
-$_['error_password_length']    = 'Password must be between %d and 20 characters!';
+$_['error_password']           = 'Password must contain a %s and be between %d and 40 characters!';
+$_['error_password_uppercase'] = 'верхний регистр';
+$_['error_password_lowercase'] = 'нижний регистр';
+$_['error_password_number']    = 'число';
+$_['error_password_symbol']    = 'символ';
+$_['error_password_length']    = 'Пароль должен содержать от %d до 40 символов!';
 $_['error_confirm']            = 'Подтверждение пароля не совпадает с паролем!';
