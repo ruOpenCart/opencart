44e4b186f23ee7e9fa6c09c56ff37ebb89156a29
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-03-02 10:52:57 +0300

    New translations order.php (Russian)

---
+++ ru/catalog/account/order.php
@@ -0,0 +1,47 @@
+<?php
+// Heading
+$_['heading_title']              = 'Заказы';
+
+// Text
+$_['text_account']               = 'Аккаунт';
+$_['text_order']                 = 'Заказ #%s';
+$_['text_order_id']              = 'Номер заказа';
+$_['text_invoice_no']            = 'Счет №';
+$_['text_tbc']                   = 'TBC';
+$_['text_shipping_address']      = 'Адрес доставки';
+$_['text_payment_address']       = 'Платежный адрес';
+$_['text_shipping_method']       = 'Способ доставки';
+$_['text_payment_method']        = 'Метод оплаты';
+$_['text_model']                 = 'Модель';
+$_['text_points']                = 'Бонусные баллы';
+$_['text_subscription']          = 'Подписка';
+$_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
+$_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
+$_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
+$_['text_day']                   = 'день';
+$_['text_week']                  = 'неделя';
+$_['text_semi_month']            = 'полмесяца';
+$_['text_month']                 = 'месяц';
+$_['text_year']                  = 'год';
+$_['text_date_next']             = 'Дате Следующая';
+$_['text_remaining']             = 'Осталось';
+$_['text_comment']               = 'Заметка';
+$_['text_history']               = 'История заказа';
+$_['text_success']               = 'Успех: Вы добавили <a href="%s">%s</a> в вашу <a href="%s">корзину покупок</a>!';
+$_['text_no_results']            = 'Вы не делали ранее заказов!';
+
+// Column
+$_['column_order_id']            = 'Номер заказа';
+$_['column_customer']            = 'Покупатель';
+$_['column_product']             = 'Товар';
+$_['column_product_total']       = 'Кол-во товаров';
+$_['column_status']              = 'Статус';
+$_['column_total']               = 'Всего';
+$_['column_date_added']          = 'Дата добавления';
+$_['column_quantity']            = 'Количество';
+$_['column_price']               = 'Цена';
+$_['column_comment']             = 'Комментарий';
+$_['column_action']              = 'Действие';
+
+// Error
+$_['error_reorder']              = 'У продукта, который вы пытаетесь купить снова, есть некоторые проблемы. Пожалуйста, проверьте сообщения об ошибках, показанные ниже.';
