6b82657634388a07255cde6884d687d02892320f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-03-16 11:34:25 +0300

    New translations returns.php (Russian)

--- ru/admin/sale/returns.php
+++ ru/admin/sale/returns.php
@@ -1,72 +1,72 @@
 <?php
 // Heading
-$_['heading_title']      = 'Возвраты';
+$_['heading_title']       = 'Возвраты';
 
 // Text
-$_['text_success']       = 'Успех: Вы изменили возвраты!';
-$_['text_list']          = 'Список возвратов';
-$_['text_add']           = 'Добавить возврат';
-$_['text_edit']          = 'Возврат (№%s)';
-$_['text_order_id']      = 'Номер заказа';
-$_['text_filter']        = 'Фильтр';
-$_['text_customer']      = 'Покупатель';
-$_['text_date_added']    = 'Дата добавления';
-$_['text_return']        = 'Вернуть товар';
-$_['text_product']       = 'Выберите товар для возврата';
-$_['text_opened']        = 'Открыто';
-$_['text_unopened']      = 'Не открыто';
-$_['text_history']       = 'История';
-$_['text_history_add']   = 'Добавление истории';
+$_['text_success']        = 'Успех: Вы изменили возвраты!';
+$_['text_list']           = 'Список возвратов';
+$_['text_add']            = 'Добавить возврат';
+$_['text_edit']           = 'Возврат (№%s)';
+$_['text_order_id']       = 'Номер заказа';
+$_['text_filter']         = 'Фильтр';
+$_['text_customer']       = 'Покупатель';
+$_['text_date_added']     = 'Дата добавления';
+$_['text_return']         = 'Вернуть товар';
+$_['text_product']        = 'Выберите товар для возврата';
+$_['text_opened']         = 'Открыто';
+$_['text_unopened']       = 'Не открыто';
+$_['text_history']        = 'История';
+$_['text_history_add']    = 'Добавление истории';
 
 // Column
-$_['column_return_id']   = 'Идентификатор возврата';
-$_['column_order_id']    = 'Номер заказа';
-$_['column_customer']    = 'Покупатель';
-$_['column_product']     = 'Товар';
-$_['column_model']       = 'Модель';
-$_['column_quantity']    = 'Количество';
-$_['column_status']      = 'Статус';
-$_['column_date_added']  = 'Дата добавления';
-$_['column_comment']     = 'Комментарий';
-$_['column_notify']      = 'Покупатель уведомлен';
-$_['column_action']      = 'Действие';
+$_['column_return_id']    = 'Идентификатор возврата';
+$_['column_order_id']     = 'Номер заказа';
+$_['column_customer']     = 'Покупатель';
+$_['column_product']      = 'Товар';
+$_['column_model']        = 'Модель';
+$_['column_quantity']     = 'Количество';
+$_['column_status']       = 'Статус';
+$_['column_date_added']   = 'Дата добавления';
+$_['column_comment']      = 'Комментарий';
+$_['column_notify']       = 'Покупатель уведомлен';
+$_['column_action']       = 'Действие';
 
 // Entry
-$_['entry_customer']     = 'Покупатель';
-$_['entry_order_id']     = 'Номер заказа';
-$_['entry_date_ordered'] = 'Дата заказа';
-$_['entry_firstname']    = 'Имя';
-$_['entry_lastname']     = 'Фамилия';
-$_['entry_email']        = 'Эл. почта';
-$_['entry_telephone']    = 'Телефон';
-$_['entry_product']      = 'Товар';
-$_['entry_model']        = 'Модель';
-$_['entry_quantity']     = 'Количество';
-$_['entry_opened']       = 'Открыто';
-$_['entry_comment']      = 'Комментарий';
-$_['entry_reason']       = 'Причина возврата';
-$_['entry_action']       = 'Действие при возврате';
-$_['entry_status']       = 'Статус возврата';
-$_['entry_notify']       = 'Уведомить покупателя';
-$_['entry_return_id']    = 'Идентификатор возврата';
-$_['entry_date_from']    = 'Дата от';
-$_['entry_date_to']      = 'Дата до';
+$_['entry_customer']      = 'Покупатель';
+$_['entry_order_id']      = 'Номер заказа';
+$_['entry_date_ordered']  = 'Дата заказа';
+$_['entry_firstname']     = 'Имя';
+$_['entry_lastname']      = 'Фамилия';
+$_['entry_email']         = 'Эл. почта';
+$_['entry_telephone']     = 'Телефон';
+$_['entry_product']       = 'Товар';
+$_['entry_model']         = 'Модель';
+$_['entry_quantity']      = 'Количество';
+$_['entry_opened']        = 'Открыто';
+$_['entry_comment']       = 'Комментарий';
+$_['entry_return_reason'] = 'Return Reason';
+$_['entry_return_action'] = 'Return Action';
+$_['entry_return_status'] = 'Return Status';
+$_['entry_return_id']     = 'Идентификатор возврата';
+$_['entry_notify']        = 'Уведомить покупателя';
+$_['entry_date_from']     = 'Дата от';
+$_['entry_date_to']       = 'Дата до';
 
 // Help
-$_['help_product']       = '(Автодополнение)';
+$_['help_product']        = '(Автодополнение)';
 
 // Error
-$_['error_warning']      = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
-$_['error_permission']   = 'Внимание: У вас нет прав на изменение возвратов!';
-$_['error_order']        = 'Заказ не найден!';
-$_['error_customer']     = 'Покупатель не найден!';
-$_['error_firstname']    = 'Имя должно быть от 1 до 32-х символов!';
-$_['error_lastname']     = 'Фамилия должна быть от 1 до 32 символов!';
-$_['error_email']        = 'Адрес электронной почты недействителен!';
-$_['error_telephone']    = 'Телефон должен быть от 3 до 32 символов!';
-$_['error_product']      = 'Товар не найден!';
-$_['error_name']         = 'Название товара должно быть больше 1 и меньше 255 символов!';
-$_['error_model']        = 'Модель продукта должна быть больше 1 и меньше, чем 64 символов!';
-$_['error_quantity']     = 'Количество возврата должно быть не менее 1!';
-$_['error_reason']       = 'Вы должны выбрать причину возврата!';
-$_['error_action']       = 'Вы должны выбрать действие по возврату!';
+$_['error_warning']       = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
+$_['error_permission']    = 'Внимание: У вас нет прав на изменение возвратов!';
+$_['error_order']         = 'Заказ не найден!';
+$_['error_customer']      = 'Покупатель не найден!';
+$_['error_firstname']     = 'Имя должно быть от 1 до 32-х символов!';
+$_['error_lastname']      = 'Фамилия должна быть от 1 до 32 символов!';
+$_['error_email']         = 'Адрес электронной почты недействителен!';
+$_['error_telephone']     = 'Телефон должен быть от 3 до 32 символов!';
+$_['error_product']       = 'Товар не найден!';
+$_['error_name']          = 'Название товара должно быть больше 1 и меньше 255 символов!';
+$_['error_model']         = 'Модель продукта должна быть больше 1 и меньше, чем 64 символов!';
+$_['error_quantity']      = 'Количество возврата должно быть не менее 1!';
+$_['error_reason']        = 'Вы должны выбрать причину возврата!';
+$_['error_action']        = 'Вы должны выбрать действие по возврату!';
