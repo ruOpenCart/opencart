a04f43dd5bdbb6faa6a78e3ee0a73d7f0829687a
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-03-16 11:36:29 +0300

    New translations identifier.php (Russian)

---
+++ ru/admin/catalog/identifier.php
@@ -0,0 +1,30 @@
+<?php
+// Heading
+$_['heading_title']    = 'Identifiers';
+
+// Text
+$_['text_success']     = 'Success: You have modified Identifiers!';
+$_['text_list']        = 'Identifier List';
+$_['text_add']         = 'Add Identifier';
+$_['text_edit']        = 'Edit Identifier';
+
+// Column
+$_['column_name']      = 'Identifier Name';
+$_['column_code']      = 'Код';
+$_['column_action']    = 'Действие';
+
+// Entry
+$_['entry_name']       = 'Identifier Name';
+$_['entry_code']       = 'Код';
+$_['entry_status']     = 'Статус';
+
+// Help
+$_['help_name']        = 'Example: Stock Keeping Unit, Universal Product Code, European Article Number, Japanese Article Number, International Standard Book Number or Manufacturer Part Number';
+$_['help_code']        = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
+$_['help_status']      = 'Display product identifier on product info page';
+
+// Error
+$_['error_warning']    = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
+$_['error_permission'] = 'Warning: You do not have permission to modify identifiers!';
+$_['error_name']       = 'Identifier Name must be between 1 and 64 characters!';
+$_['error_code']       = 'Identifier Code must be between 3 and 48 characters!';
