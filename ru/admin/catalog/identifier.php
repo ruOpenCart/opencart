c1fd8dedcb0e6ada5244fa50d70b6acc02a8a470
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-03-19 20:34:30 +0300

    New translations identifier.php (Russian)

--- ru/admin/catalog/identifier.php
+++ ru/admin/catalog/identifier.php
@@ -1,30 +1,33 @@
 <?php
 // Heading
-$_['heading_title']    = 'Identifiers';
+$_['heading_title']    = 'Идентификаторы';
 
 // Text
-$_['text_success']     = 'Success: You have modified Identifiers!';
-$_['text_list']        = 'Identifier List';
-$_['text_add']         = 'Add Identifier';
-$_['text_edit']        = 'Edit Identifier';
+$_['text_success']     = 'Успех: Вы изменили идентификаторы!';
+$_['text_list']        = 'Список идентификаторов';
+$_['text_add']         = 'Добавить идентификатор';
+$_['text_edit']        = 'Изменить идентификатор';
+$_['text_regex']       = 'Регулярное выражение';
 
 // Column
-$_['column_name']      = 'Identifier Name';
+$_['column_name']      = 'Имя идентификатора';
 $_['column_code']      = 'Код';
 $_['column_action']    = 'Действие';
 
 // Entry
-$_['entry_name']       = 'Identifier Name';
+$_['entry_name']       = 'Имя идентификатора';
 $_['entry_code']       = 'Код';
+$_['entry_validation'] = 'Валидация';
 $_['entry_status']     = 'Статус';
 
 // Help
-$_['help_name']        = 'Example: Stock Keeping Unit, Universal Product Code, European Article Number, Japanese Article Number, International Standard Book Number or Manufacturer Part Number';
-$_['help_code']        = 'Example: SKU, UPC, EAN, JAN, ISBN or MPN';
-$_['help_status']      = 'Display product identifier on product info page';
+$_['help_name']        = 'Пример: Stock Keeping Unit, Universal Product Code, European Article Number, Japanese Article Number, International Standard Book Number или Manufacturer Part Number';
+$_['help_code']        = 'Пример: SKU, UPC, EAN, JAN, ISBN или MPN';
+$_['help_regex']       = 'Используйте регулярное выражение. Например: /[^a-zA-Z0-9_-]/';
+$_['help_status']      = 'Показывать идентификатор товара на странице с информацией о товаре';
 
 // Error
 $_['error_warning']    = 'Внимание: Пожалуйста, проверьте форму на наличие ошибок!';
-$_['error_permission'] = 'Warning: You do not have permission to modify identifiers!';
-$_['error_name']       = 'Identifier Name must be between 1 and 64 characters!';
-$_['error_code']       = 'Identifier Code must be between 3 and 48 characters!';
+$_['error_permission'] = 'Внимание: У вас нет прав на изменение идентификаторов!';
+$_['error_name']       = 'Имя идентификатора должно быть от 1 до 64 символов!';
+$_['error_code']       = 'Код идентификации должен быть от 3 до 48 символов!';
