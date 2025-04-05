decd66c45cfec2b7dc58b4d4368916024516dc32
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-04-05 10:33:54 +0300

    New translations country.php (Russian)

---
+++ ru/admin/ssr/country.php
@@ -0,0 +1,4 @@
+<?php
+$_['text_country'] = 'You have rendered %s to %s of %s countries';
+$_['text_country'] = 'You have rendered %s to %s of %s countries';
+$_['text_success'] = 'Успех: Вы изменили страны!';
