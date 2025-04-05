1b43a41cde7bcf6a0530fe1ec3dc83fe802a5f8d
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-04-05 10:33:52 +0300

    New translations ssr.php (Russian)

---
+++ ru/admin/marketplace/ssr.php
@@ -0,0 +1,18 @@
+<?php
+// Heading
+$_['heading_title']        = 'Static Site Rendering';
+
+// Text
+$_['text_success']         = 'Success: You have modified static site rendering!';
+$_['text_list']            = 'SSR List';
+$_['text_event']           = 'Static Site Rendering is used to pre render templates and static data. If you have issues you can disable or enable the SSR here.';
+
+// Column
+$_['column_code']          = 'SSR Code';
+$_['column_progress']      = 'Прогресс';
+$_['column_sort_order']    = 'Порядок сортировки';
+$_['column_date_modified'] = 'Дата изменения';
+$_['column_action']        = 'Действие';
+
+// Error
+$_['error_permission']     = 'Warning: You do not have permission to modify static site rendering!';
