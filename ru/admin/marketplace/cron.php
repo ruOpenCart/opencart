3192b05734bc9b583fc367ff29ad296afa9bcab2
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-04-05 10:33:46 +0300

    New translations cron.php (Russian)

--- ru/admin/marketplace/cron.php
+++ ru/admin/marketplace/cron.php
@@ -6,7 +6,7 @@ $_['heading_title']        = 'Задания CRON';
 $_['text_success']         = 'Успех: Вы изменили задания CRON!';
 $_['text_instruction']     = 'Инструкции по Крону';
 $_['text_list']            = 'Список CRON';
-$_['text_cron_1']          = 'Задания CRON — это запланированные задачи, которые запускаются периодически. Чтобы настроить серверы для использования задания cron, вы можете прочитать страницу <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">документации опенкарт</a>.';
+$_['text_cron_1']          = 'CRON Job\'s are scheduled tasks that are run periodically. To set up your servers to use cron job you can read the <a href="http://docs.opencart.com/extension/cron/" target="_blank" class="alert-link">opencart documentation</a> page.';
 $_['text_cron_2']          = 'Вам нужно настроить задачу CRON для запуска каждый час.';
 $_['text_info']            = 'Информация по Крону';
 $_['text_hour']            = 'Час';
@@ -22,7 +22,6 @@ $_['column_action']        = 'Действие';
 
 // Entry
 $_['entry_cron']           = 'URL-адрес CRON';
-$_['entry_description']    = 'Описание';
 
 // Error
 $_['error_permission']     = 'Внимание: У вас нет разрешения на изменение заданий CRON!';
