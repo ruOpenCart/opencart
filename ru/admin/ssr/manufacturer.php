be51d8d2f5c6ae2391385725fa68991e76fa1eeb
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-04-08 11:23:18 +0300

    New translations manufacturer.php (Russian)

---
+++ ru/admin/ssr/manufacturer.php
@@ -0,0 +1,10 @@
+<?php
+// Text
+$_['text_list']        = 'You have rendered the manufacturer list';
+$_['text_next']        = 'You have rendered %s to %s of %s manufacturers';
+$_['text_success']     = 'Успех: Вы изменили производителей!';
+
+// Error
+$_['error_permission'] = 'Внимание: У вас нет прав на изменение производителей!';
+$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
+$_['error_file']       = 'Warning: File %s could not be written!';
