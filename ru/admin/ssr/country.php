8e4b570bcdaf7cb9fd26adc2fb35a5283f16b683
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-04-12 12:11:45 +0300

    New translations country.php (Russian)

--- ru/admin/ssr/country.php
+++ ru/admin/ssr/country.php
@@ -1,10 +1,10 @@
 <?php
 // Text
-$_['text_list']        = 'You have rendered the country list';
-$_['text_next']        = 'You have rendered %s to %s of %s countries';
+$_['text_list']        = 'Вы предоставили список стран для рендеринга';
+$_['text_next']        = 'Вы предоставили %s для %s из %s стран';
 $_['text_success']     = 'Успех: Вы изменили страны!';
 
 // Error
 $_['error_permission'] = 'Внимание: У вас нет прав на изменение стран!';
-$_['error_directory']  = 'Warning: Directory %s does not exist or is not writable!';
-$_['error_file']       = 'Warning: File %s could not be written!';
+$_['error_directory']  = 'Внимание: Директория %s не существует или недоступна для записи!';
+$_['error_file']       = 'Внимание: Файл %s не может быть записан!';
