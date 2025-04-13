dc832af6da4ba4eeba8802fcc68abeef6d50cc26
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2025-04-13 12:38:56 +0300

    New translations currency.php (Russian)

--- ru/admin/ssr/currency.php
+++ ru/admin/ssr/currency.php
@@ -1,6 +1,6 @@
 <?php
 // Text
-$_['text_list']        = 'You have rendered the currency list';
+$_['text_list']        = 'Вы предоставили список валют для рендеринга';
 $_['text_next']        = 'You have rendered %s to %s of %s currencies';
 $_['text_success']     = 'Успех: Вы изменили валюты!';
 
