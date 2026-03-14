3510c303510468b0e9594b9e624d5b2c414e6f3f
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2026-03-14 21:52:27 +0300

    New translations subscription.php (Russian)

---
+++ ru/admin/language/ru-ru/sale/subscription.php
@@ -0,0 +1,82 @@
+<?php
+// Heading
+$_['heading_title']              = 'Подписки';
+
+// Text
+$_['text_success']               = 'Успех: Вы изменили подписки!';
+$_['text_list']                  = 'Список подписок';
+$_['text_add']                   = 'Добавить подписку';
+$_['text_edit']                  = 'Подписка (#%s)';
+$_['text_filter']                = 'Фильтр';
+$_['text_date_added']            = 'Дата добавления';
+$_['text_order_id']              = 'Номер заказа';
+$_['text_order']                 = 'Заказы';
+$_['text_product_add']           = 'Добавление товара';
+$_['text_model'] 			     = 'Модель';
+$_['text_subscription']          = 'Подписка';
+$_['text_date_next']             = 'Дате Следующая';
+$_['text_remaining']             = 'Осталось';
+$_['text_subscription_trial']    = '%s каждые %d %s для %d платеж(а)(ей) тогда ';
+$_['text_subscription_duration'] = '%s каждые %d %s для %d платеж(а)(ей)';
+$_['text_subscription_cancel']   = '%s каждые %d %s до отмены';
+$_['text_cancel']                = 'До отмены';
+$_['text_day']                   = 'День';
+$_['text_week']                  = 'Неделя';
+$_['text_semi_month']            = 'Полмесяца';
+$_['text_month']                 = 'Месяц';
+$_['text_year']                  = 'Год';
+$_['text_payment_method']        = 'Метод оплаты';
+$_['text_payment']               = 'Пожалуйста, выберите предпочтительный способ оплаты для этой подписки.';
+$_['text_shipping_method']       = 'Способ доставки';
+$_['text_shipping']              = 'Пожалуйста, выберите предпочтительный способ доставки для этой подписки.';
+$_['text_history']               = 'История';
+$_['text_history_add']           = 'Добавление истории';
+$_['text_log']                   = 'Логи';
+
+// Column
+$_['column_subscription_id']     = 'Идентификатор подписки';
+$_['column_order_id']            = 'Номер заказа';
+$_['column_customer']            = 'Покупатель';
+$_['column_comment']             = 'Комментарий';
+$_['column_notify']              = 'Покупатель уведомлен';
+$_['column_status']              = 'Статус';
+$_['column_date_added']          = 'Дата добавления';
+$_['column_product']             = 'Товар';
+$_['column_model']               = 'Модель';
+$_['column_quantity']            = 'Количество';
+$_['column_trial_price']         = 'Пробная цена';
+$_['column_price']               = 'Цена';
+$_['column_total']               = 'Всего';
+$_['column_amount']              = 'Сумма';
+$_['column_code']                = 'Код';
+$_['column_description']         = 'Описание';
+$_['column_action']              = 'Действие';
+
+// Entry
+$_['entry_subscription_id']      = 'Идентификатор подписки';
+$_['entry_order_id']             = 'Номер заказа';
+$_['entry_customer']             = 'Покупатель';
+$_['entry_store']                = 'Магазин';
+$_['entry_language']             = 'Язык';
+$_['entry_currency']             = 'Валюта';
+$_['entry_subscription_plan']    = 'План подписки';
+$_['entry_date_next']            = 'Дате Следующая';
+$_['entry_comment']              = 'Комментарий';
+$_['entry_notify']               = 'Уведомить покупателя';
+$_['entry_date_from']            = 'Дата от';
+$_['entry_date_to']              = 'Дата до';
+$_['entry_subscription_status']  = 'Статус подписки';
+$_['entry_product']              = 'Выберите товар';
+$_['entry_option']               = 'Выбрать опци(ю)(и)';
+$_['entry_quantity']             = 'Количество';
+$_['entry_payment_address']      = 'Платежный адрес';
+$_['entry_shipping_address']     = 'Адрес доставки';
+
+// Tab
+$_['tab_order']                  = 'Заказы';
+
+// Error
+$_['error_permission']           = 'Внимание: У вас нет разрешения на изменение подписки!';
+$_['error_subscription_status']  = 'Внимание: Необходимо выбрать статус подписки!';
+$_['error_payment_method']       = 'Внимание: Способ оплаты не существует!';
+$_['error_subscription']         = 'Внимание: Подписка еще не добавлена!';
