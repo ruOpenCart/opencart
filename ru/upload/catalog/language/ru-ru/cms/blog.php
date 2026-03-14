242542f3ef03e6b3384882a4a4a90cc6b1a4250a
Aleksandr Gorbatov <hkrtrk@yandex.ru>
2026-03-14 17:19:56 +0300

    New translations blog.php (Russian)

---
+++ ru/upload/catalog/language/ru-ru/cms/blog.php
@@ -0,0 +1,23 @@
+<?php
+// Heading
+$_['heading_title'] = 'Блог';
+
+// Text
+$_['text_refine'] = 'Уточнить поиск';
+$_['text_all'] = 'Все';
+$_['text_blog'] = 'Блог';
+$_['text_by'] = 'от';
+$_['text_tags'] = 'Тэги:';
+$_['text_comment'] = 'комментарии';
+$_['text_no_results'] = 'Нет статей.';
+$_['text_sort'] = 'Сортировать по';
+$_['text_rating_asc'] = 'Рейтинг (по убыванию)';
+$_['text_rating_desc'] = 'Рейтинг (по возрастанию)';
+$_['text_date_added_asc'] = 'Дата добавления (старее)';
+$_['text_date_added_desc'] = 'Дата добавления (новее)';
+
+// Entry
+$_['entry_search'] = 'Искать';
+
+// Button
+$_['button_continue'] = 'Читать далее';
